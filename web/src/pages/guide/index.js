425d179ecf5ada5177d6c00773e3bebe264d08e1
yeshtml5 <yeshtml5@gmail.com>
2019-12-30 14:41:46 +0900

    jsconfig.json 업데이트

--- web/src/pages/guide/index.js
+++ web/src/pages/guide/index.js
@@ -1,4 +1,7 @@
-import React from 'react'
+/**
+ * @title 가이드페이지
+ */
+import React, {useEffect} from 'react'
 
 export default () => {
   return (
--- web/src/pages/main/index.js
+++ web/src/pages/main/index.js
@@ -7,6 +7,7 @@ export default () => {
   return (
     <h1>
       <a href="/guide">메인페이지</a>
+      <span></span>
     </h1>
   )
 }
