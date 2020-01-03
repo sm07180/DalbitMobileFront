2e94e3bdce2e8a15e889cd93a97804a6831598db
kimhogyeom <hgkim1118@inforex.co.kr>
2020-01-03 09:48:31 +0900

    eslint-prettier 충돌해결

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react'
-import {Route, Switch} from 'react-router-dom'
+import React, {Component} from 'react';
+import {Route, Switch} from 'react-router-dom';
 //pages
 
 /*-common-*/
-import Main from 'Pages/main' //메인
-import Guide from 'Pages/guide/' //가이드
+import Main from 'Pages/main'; //메인
+import Guide from 'Pages/guide/'; //가이드
 
 export default () => {
   return (
@@ -12,5 +12,5 @@ export default () => {
       <Route exact path="/" component={Main} />
       <Route exact path="/guide/" component={Guide} />
     </Switch>
-  )
-}
+  );
+};
--- web/src/index.js
+++ web/src/index.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {BrowserRouter} from 'react-router-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import App from './App';
 //style
-import './styles/common.scss'
-import './styles/layout.scss'
+import './styles/common.scss';
+import './styles/layout.scss';
 //context
-import {GlobalProvider} from './context'
+import {GlobalProvider} from './context';
+
 //----------------------------------------------------------------
 ReactDOM.render(
   <BrowserRouter>
@@ -15,4 +16,4 @@ ReactDOM.render(
     </GlobalProvider>
   </BrowserRouter>,
   document.getElementById('root')
-)
+);
