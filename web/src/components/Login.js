2ab99872ed6728123efe90e648c0bc50a40c7a43
kimhogyeom <hgkim1118@inforex.co.kr>
2020-01-09 11:23:26 +0900

    hooks test

--- web/src/components/Login.js
+++ web/src/components/Login.js
@@ -4,5 +4,6 @@ import KakaoLogin from 'react-kakao-login'
 import styled from 'styled-components'
 
 export default () => {
-  return <GoogleLogin clienrId={process.env.REACT_APP_Goolgle} buttonText="Google" onSuccess={'성공'} onFailure={'실패'} />
+  return
+  ;<GoogleLogin clienrId={process.env.REACT_APP_Goolgle} buttonText="Google" onSuccess={'성공'} onFailure={'실패'} />
 }
