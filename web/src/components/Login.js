cd79a8a481d0e4ab6dd183d0e14f3c16da89225b
kimhogyeom <hgkim1118@inforex.co.kr>
2020-01-09 13:51:11 +0900

    login

--- web/src/components/Login.js
+++
@@ -1,8 +0,0 @@
-import React, {useEffect, useState, Component} from 'react'
-import {GoogleLogin} from 'react-google-login'
-import KakaoLogin from 'react-kakao-login'
-import styled from 'styled-components'
-
-export default () => {
-  return <GoogleLogin clienrId={process.env.REACT_APP_Goolgle} buttonText="Google" onSuccess={'성공1'} onFailure={'실패'} />
-}
--- web/src/pages/common/auth/index.js
+++ web/src/pages/common/auth/index.js
@@ -6,10 +6,35 @@
 import React, {useEffect} from 'react'
 import styled from 'styled-components'
 //components
+import GoogleLogin from 'react-google-login'
+import KakaoLogin from 'react-kakao-login'
+import NaverLogin from 'react-naver-login'
+import FacebookLogin from 'react-facebook-login'
+
+const responseFacebook = () => {
+  console.log('성공')
+}
+
+const ComponentClicked = () => {
+  console.log('clicked')
+}
 
 export default () => {
   //---------------------------------------------------------------------
-  return <Login>TEST</Login>
+  return (
+    <Login>
+      <GoogleLogin clienrId={process.env.REACT_APP_Goolgle} buttonText="Google" onSuccess={'성공1'} onFailure={'실패'} />
+      <KakaoLogin />
+      <NaverLogin />
+      <FacebookLogin
+        appId="2418533275143361"
+        autoLoad={true} //실행과 동시에 자동으로 로그인 팝업창이 뜸
+        fields="name,email,picture" //어떤 정보를 받아올지 입력하는 필드
+        onClick={ComponentClicked}
+        callback={responseFacebook}
+      />
+    </Login>
+  )
 }
 //---------------------------------------------------------------------
 const Login = styled.div`
