fe7defa20c121d06c117ca533b22ede87d0636ed
kimhogyeom <hgkim1118@inforex.co.kr>
2020-01-09 11:24:49 +0900

    test

--- web/src/components/Login.js
+++ web/src/components/Login.js
@@ -4,6 +4,5 @@ import KakaoLogin from 'react-kakao-login'
 import styled from 'styled-components'
 
 export default () => {
-  return
-  ;<GoogleLogin clienrId={process.env.REACT_APP_Goolgle} buttonText="Google" onSuccess={'성공'} onFailure={'실패'} />
+  return <GoogleLogin clienrId={process.env.REACT_APP_Goolgle} buttonText="Google" onSuccess={'성공'} onFailure={'실패'} />
 }
