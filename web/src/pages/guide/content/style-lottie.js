b8bfe8ecfec79cede4bc763f89ce0d2be20d3a2e
황상한 <nmc2711@inforex.co.kr>
2020-01-13 11:22:40 +0900

    로티 갯수 추가

--- web/src/pages/guide/content/style-lottie.js
+++ web/src/pages/guide/content/style-lottie.js
@@ -8,6 +8,28 @@ import l1 from '../content/lottie/l1.json'
 import l2 from '../content/lottie/l2.json'
 import l3 from '../content/lottie/l3.json'
 import l4 from '../content/lottie/l4.json'
+import l5 from '../content/lottie/l5.json'
+import l6 from '../content/lottie/l6.json'
+import l7 from '../content/lottie/l7.json'
+import l8 from '../content/lottie/l8.json'
+import l9 from '../content/lottie/l9.json'
+import l10 from '../content/lottie/l10.json'
+import l11 from '../content/lottie/l11.json'
+import l12 from '../content/lottie/l12.json'
+import l13 from '../content/lottie/l13.json'
+import l14 from '../content/lottie/l14.json'
+import l15 from '../content/lottie/l15.json'
+import l16 from '../content/lottie/l16.json'
+import l17 from '../content/lottie/l17.json'
+import l18 from '../content/lottie/l18.json'
+import l19 from '../content/lottie/l19.json'
+import l20 from '../content/lottie/l20.json'
+import l21 from '../content/lottie/l21.json'
+import l22 from '../content/lottie/l22.json'
+import l23 from '../content/lottie/l23.json'
+import l24 from '../content/lottie/l24.json'
+import l25 from '../content/lottie/l25.json'
+
 export default () => {
   return (
     <>
@@ -16,25 +38,156 @@ export default () => {
           options={{
             animationData: l1
           }}></Lottie>
-
+      </Wrap>
+      <Wrap>
         <Lottie
           options={{
             animationData: l2
           }}></Lottie>
+      </Wrap>
+      <Wrap>
         <Lottie
           options={{
             animationData: l3
           }}></Lottie>
+      </Wrap>
+      <Wrap>
         <Lottie
           options={{
             animationData: l4
+          }}></Lottie>
+      </Wrap>
+      <Wrap>
+        <Lottie
+          options={{
+            animationData: l5
+          }}></Lottie>
+      </Wrap>
+      <Wrap>
+        <Lottie
+          options={{
+            animationData: l6
+          }}></Lottie>
+      </Wrap>
+      <Wrap>
+        <Lottie
+          options={{
+            animationData: l7
+          }}></Lottie>
+      </Wrap>
+      <Wrap>
+        <Lottie
+          options={{
+            animationData: l8
+          }}></Lottie>
+      </Wrap>
+      <Wrap>
+        <Lottie
+          options={{
+            animationData: l9
+          }}></Lottie>
+      </Wrap>
+      <Wrap>
+        <Lottie
+          options={{
+            animationData: l10
+          }}></Lottie>
+      </Wrap>
+      <Wrap>
+        <Lottie
+          options={{
+            animationData: l11
+          }}></Lottie>
+      </Wrap>
+      <Wrap>
+        <Lottie
+          options={{
+            animationData: l12
+          }}></Lottie>
+      </Wrap>
+      <Wrap>
+        <Lottie
+          options={{
+            animationData: l13
+          }}></Lottie>
+      </Wrap>
+      <Wrap>
+        <Lottie
+          options={{
+            animationData: l14
+          }}></Lottie>
+      </Wrap>
+      <Wrap>
+        <Lottie
+          options={{
+            animationData: l15
+          }}></Lottie>
+      </Wrap>
+      <Wrap>
+        <Lottie
+          options={{
+            animationData: l16
+          }}></Lottie>
+      </Wrap>
+      <Wrap>
+        <Lottie
+          options={{
+            animationData: l17
+          }}></Lottie>
+      </Wrap>
+      <Wrap>
+        <Lottie
+          options={{
+            animationData: l18
+          }}></Lottie>
+      </Wrap>
+      <Wrap>
+        <Lottie
+          options={{
+            animationData: l19
+          }}></Lottie>
+      </Wrap>
+      <Wrap>
+        <Lottie
+          options={{
+            animationData: l20
+          }}></Lottie>
+      </Wrap>
+      <Wrap>
+        <Lottie
+          options={{
+            animationData: l21
+          }}></Lottie>
+      </Wrap>
+      <Wrap>
+        <Lottie
+          options={{
+            animationData: l22
+          }}></Lottie>
+      </Wrap>
+      <Wrap>
+        <Lottie
+          options={{
+            animationData: l23
+          }}></Lottie>
+      </Wrap>
+      <Wrap>
+        <Lottie
+          options={{
+            animationData: l24
+          }}></Lottie>
+      </Wrap>
+      <Wrap>
+        <Lottie
+          options={{
+            animationData: l25
           }}></Lottie>
       </Wrap>
     </>
   )
 }
 const Wrap = styled.div`
-  width: 50vw;
-  height: 50vh;
-  margin: 0 auto;
+  width: 20%;
+  height: 20%;
+  display: inline-block;
 `
