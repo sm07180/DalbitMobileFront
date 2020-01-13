996e90dd656b8995d2b5289574bffd5e43296752
yeshtml5 <yeshtml5@gmail.com>
2020-01-13 19:17:31 +0900

    라우팅 /app/ 추가

---
+++ web/src/pages/app/index.js
@@ -0,0 +1,26 @@
+/**
+ * @file /app/index.js
+ * @brief Native-App WEBVIEW
+ */
+import React, {useEffect, useState} from 'react'
+//layout
+import Layout from 'Pages/common/layout'
+//context
+//components
+import Api from 'Context/api'
+//
+const User = () => {
+  //---------------------------------------------------------------------
+
+  useEffect(() => {}, [])
+  //---------------------------------------------------------------------
+  return (
+    <Layout>
+      <section>
+        <div>app</div>
+        <p>app</p>
+      </section>
+    </Layout>
+  )
+}
+export default User
