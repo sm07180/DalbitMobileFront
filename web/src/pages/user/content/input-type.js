fd6d2e794f52eecafc185da9f8d6665092401e19
leeeunbee <herbione@inforex.co.kr>
2020-01-14 09:06:59 +0900

    input component 수정

--- web/src/pages/user/content/input-type.js
+++ web/src/pages/user/content/input-type.js
@@ -5,6 +5,8 @@
  * @props type 종류
  *        nickname : 닉네임
  *        name : 이름(실명)
+ *        password : 비밀번호
+ *        phone : 휴대전화
  * @state
  */
 
@@ -22,10 +24,29 @@ const makeInput = props => {
     isNameValid: 0
   })
 
+  const [pwdState, setPwdState] = useState({
+    pwdEntered: '',
+    isPwdValid: 0
+  })
+
+  const [pwdCheckState, setPwdCheckState] = useState({
+    pwdCheckEntered: '',
+    isPwdCheckValid: 0
+  })
+
+  const [phoneState, setPhoneState] = useState({
+    phoneEntered: '',
+    isPhoneValid: 0
+  })
+
   const validateNickName = nameEntered => {
     if (nameEntered.length > 1 && nameEntered.length < 21) {
       setNickNameState({
         isNameValid: 1
+      })
+    } else if (nameEntered.length == 0) {
+      setNickNameState({
+        isNameValid: 0
       })
     } else {
       setNickNameState({
@@ -50,6 +71,37 @@ const makeInput = props => {
     }
   }
 
+  const validatePwd = pwdEntered => {
+    if (pwdEntered.length > 7 && pwdEntered.length < 21) {
+      setPwdState({
+        isPwdValid: 1
+      })
+    } else if (pwdEntered.length == 0) {
+      setPwdState({
+        isPwdValid: 0
+      })
+    } else {
+      setPwdState({
+        isPwdValid: 2
+      })
+    }
+  }
+
+  const validatePwdCheck = pwdCheckEntered => {}
+
+  const validatePhone = phoneEntered => {
+    const phoneNumberRegExp = /^\d{3}-\d{3,4}-\d{4}$/
+    if (phoneEntered.match(phoneNumberRegExp)) {
+      setPhoneState({
+        isPhoneValid: 1
+      })
+    } else {
+      setPhoneState({
+        isPhoneValid: 2
+      })
+    }
+  }
+
   // input 아래 도움말 텍스트 컬러
   const textColor = vaildState => {
     if (vaildState > 1) {
@@ -61,6 +113,24 @@ const makeInput = props => {
     }
   }
 
+  const addPwdCheck = () => {
+    const text = ['', '*비밀번호가 일치합니다.', '*비밀번호가 일치하지 않습니다.']
+    if (props.type === 'password') {
+      return (
+        <>
+          <Input
+            id={props.type}
+            type={selectInputType().type}
+            placeholder="비밀번호 확인"
+            onChange={e => {
+              selectInputType().validate(e.target.value)
+            }}></Input>
+          <HelpText className={textColor(selectInputType().isValied)}>{selectInputType().text[selectInputType().isValied]}</HelpText>
+        </>
+      )
+    }
+  }
+
   const selectInputType = () => {
     switch (props.type) {
       case 'nickname':
@@ -69,7 +139,7 @@ const makeInput = props => {
           title: '닉네임', // label 태그에 들어갈 text
           required: true, // 폼에 필수 값인지 아닌지 여부
           placeholder: '닉네임을 입력해주세요', // input 태그 placeholder
-          text: '2~20자 한글/영문/숫자를 포함할 수 있습니다.', // input 박스 아래에 표시될 안내 텍스트 초기값
+          text: ['*2~20자 한글/영문/숫자를 포함할 수 있습니다.', '*사용 가능한 닉네임입니다.', '실패1'], // input 박스 아래에 표시될 안내 텍스트
           validate: function(targetValue) {
             // 유효성 검사 function
             validateNickName(targetValue)
@@ -82,15 +152,44 @@ const makeInput = props => {
           title: '이름(실명)',
           required: true,
           placeholder: '이름을 입력해주세요',
-          // text: '2~10자 한글만 사용이 가능 합니다.',
-          text: ['기본', '성공', '실패1'],
+          text: ['*2~10자 한글만 사용이 가능 합니다.', '*사용이 가능한 이름 입니다.', '실패1'],
           validate: function(targetValue) {
             validateName(targetValue)
           },
           isValied: nameState.isNameValid
         }
+      case 'password':
+        return {
+          type: 'password',
+          title: '비밀번호',
+          required: true,
+          placeholder: '비밀번호를 입력해주세요',
+          text: ['* 8~20자로 영문/숫자/특수문자 중 2가지 이상 조합으로 입력하세요.(대소문자 구분)', '*사용이 가능한 비밀번호입니다.', '실패1'],
+          validate: function(targetValue) {
+            validatePwd(targetValue)
+          },
+          isValied: pwdState.isPwdValid
+        }
+      case 'phone':
+        return {
+          type: 'text',
+          title: '휴대전화',
+          required: true,
+          placeholder: '휴대폰 번호를 입력해주세요',
+          text: ['기본', '성공', '숫자만 입력이 가능합니다'],
+          validate: function(targetValue) {
+            validatePhone(targetValue)
+          },
+          isValied: phoneState.isPhoneValid
+        }
       default:
-        break
+        return {
+          type: 'text',
+          title: '타이틀',
+          required: true,
+          placeholder: 'placeholder',
+          text: ['', '', '']
+        }
     }
   }
 
@@ -102,10 +201,12 @@ const makeInput = props => {
       <Input
         id={props.type}
         type={selectInputType().type}
+        placeholder={selectInputType().placeholder}
         onChange={e => {
           selectInputType().validate(e.target.value)
         }}></Input>
-      <HelpText className={textColor(selectInputType().isValied)}>{selectInputType().text[nameState.isNameValid]}</HelpText>
+      <HelpText className={textColor(selectInputType().isValied)}>{selectInputType().text[selectInputType().isValied]}</HelpText>
+      {addPwdCheck()}
     </Inputwrap>
   )
 }
--- web/src/pages/user/content/join-auth.js
+++ web/src/pages/user/content/join-auth.js
@@ -9,22 +9,8 @@ import Button from './style-button'
 const JoinAuth = props => {
   return (
     <>
-      {/* <JoinText>
-        입력하신 번호는 안전하게 보관됩니다. <br />
-        휴대폰 번호 인증 후 새로운 비밀번호로 재등록 하셔야 합니다.
-      </JoinText>
-
-      <FormWrap>
-        <Label text="휴대전화" htmlfor="phone" />
-        <Input type="text" placeholder="휴대폰 번호를 입력해주세요" id="phone" />
-        <Button text="인증번호 받기" joinState={props.joinState} update={props.update} />
-      </FormWrap> */}
-
       <Content>
-        <Input type="nickname" />
-        <Input type="name" />
-
-        {/* <Input type="password" /> */}
+        <Input type="phone" />
 
         <Button text="인증번호 받기" joinState={props.joinState} update={props.update} />
       </Content>
