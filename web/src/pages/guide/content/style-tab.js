5899bd2f84c67552dd6fbda07636f58fe25c0396
황상한 <nmc2711@inforex.co.kr>
2020-01-16 12:40:30 +0900

    방송설정

--- web/src/pages/guide/content/style-tab.js
+++ web/src/pages/guide/content/style-tab.js
@@ -1,14 +1,48 @@
 /**
  * @title
  */
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
-
+import Player from './player'
+import Broad from './broad'
 export default () => {
+  const [show, setShow] = useState(false)
+  const [show2, setShow2] = useState(false)
   //---------------------------------------------------------------------
-
+  const operation = () => {
+    if (show === false) {
+      setShow(true)
+      setShow2(false)
+    } else {
+      setShow(false)
+    }
+  }
+  const operation2 = () => {
+    if (show2 === false) {
+      setShow2(true)
+      setShow(false)
+    } else {
+      setShow2(false)
+    }
+  }
   //---------------------------------------------------------------------
-  return <Content>스타일가이드-탭</Content>
+  return (
+    <>
+      <Content>
+        <Btn onClick={() => operation()}>방송듣기</Btn>
+        <Btn onClick={() => operation2()}>방송하기</Btn>
+        {show ? <Player /> : null}
+        {show2 ? <Broad /> : null}
+      </Content>
+    </>
+  )
 }
 //---------------------------------------------------------------------
-const Content = styled.div``
+const Btn = styled.button`
+  width: 100px;
+  height: 40px;
+  color: white;
+  background-color: skyblue;
+  margin-right: 10px;
+`
+const Content = styled.section``
