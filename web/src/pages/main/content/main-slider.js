c95d2a4faf7af631c2ccc1124b2b433fc0fa9942
leeeunbee <herbione@inforex.co.kr>
2020-01-21 16:29:37 +0900

    메인 작업

---
+++ web/src/pages/main/content/main-slider.js
@@ -0,0 +1,42 @@
+/**
+ * @file /content/context-list.js
+ * @brief 메인 라이브, 캐스트 리스트 component
+ */
+import React from 'react'
+import styled from 'styled-components'
+
+//context
+import {COLOR_MAIN, COLOR_POINT_Y, COLOR_POINT_P} from 'Context/color'
+
+//components
+
+export default props => {
+  return (
+    <Content>
+      <MainSlider>
+        <h5>오후 잠을 깨워줄 상큼한 목소리 들어요</h5>
+      </MainSlider>
+    </Content>
+  )
+}
+
+//---------------------------------------------------------------------
+//styled
+
+const Content = styled.section``
+
+const MainSlider = styled.div`
+  overflow: hidden;
+  position: relative;
+  width: 100%;
+  height: 658px;
+
+  &::before {
+    position: absolute;
+    width: 920px;
+    height: 920px;
+    background-color: ${COLOR_MAIN};
+    border-radius: 50%;
+    content: '';
+  }
+`
---
+++ web/src/pages/main/content/popular.js
@@ -0,0 +1,15 @@
+/**
+ * @file /content/context-list.js
+ * @brief 메인 라이브, 캐스트 리스트 component
+ */
+import React from 'react'
+import styled from 'styled-components'
+
+//context
+import {WIDTH_TABLET} from 'Context/config'
+
+//components
+
+export default props => {
+  return <p>인기 DJ</p>
+}
