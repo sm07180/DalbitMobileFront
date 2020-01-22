0de1c9cb92533452c857032d7056d972a80fb48e
yeshtml5 <yeshtml5@gmail.com>
2020-01-23 08:55:27 +0900

    Hybrid 주석추가

--- web/src/context/hybrid.js
+++ web/src/context/hybrid.js
@@ -1,24 +1,28 @@
 /**
- * @title React ---> iOS,Android 로 function전달
- * @example 사용법
- * @ios webkit.messageHandlers.GetLoginToken.postMessage(res.data)
- * @android window.android.GetLoginToken(JSON.stringify(res.data))
+ * @title     : React ---> iOS,Android 로 function전달
+ * @example   : 사용법
+ * @ios       : webkit.messageHandlers.GetLoginToken.postMessage(res.data) -> Hybrid('GetLoginToken', res.data)
+ * @android   : window.android.GetLoginToken(JSON.stringify(res.data)) -> Hybrid('GetLoginToken', res.data)
  *
  */
 import React, {useEffect, useState} from 'react'
 import {osName, browserName} from 'react-device-detect'
 //---------------------------------------------------------------------
-
+/**
+ *
+ * @param string  func          //*function 이름 (Method)
+ * @param any     info          //*data (string or object )
+ *
+ */
 export const Hybrid = (func, info) => {
   switch (osName) {
     case 'Windows':
-      alert('windows')
+      console.log('Windows버젼입니다')
       break
     case 'iOS':
       webkit.messageHandlers[func].postMessage(info)
       break
     case 'Android':
-      console.log(info)
       window.android[func](JSON.stringify(info))
       break
     default:
