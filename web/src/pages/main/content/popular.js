f79c05ddc57c947fbc42c86f28142d898eac4fec
황상한 <nmc2711@inforex.co.kr>
2020-01-22 13:51:12 +0900

    popular 임시 커밋

--- web/src/pages/main/content/popular.js
+++ web/src/pages/main/content/popular.js
@@ -2,14 +2,135 @@
  * @file /content/context-list.js
  * @brief 메인 라이브, 캐스트 리스트 component
  */
-import React from 'react'
+import React, {useState} from 'react'
+import Swiper from 'react-id-swiper'
 import styled from 'styled-components'
-
-//context
+import ContentBox from './contentBox'
+import {DEVICE_MOBILE} from 'Context/config'
 import {WIDTH_TABLET} from 'Context/config'
-
-//components
-
+import {WIDTH_PC} from 'Context/config'
+import {COLOR_WHITE} from 'Context/color'
 export default props => {
-  return <p>인기 DJ</p>
+  const [slideInfo, setSlideInfo] = useState(props.Info)
+  const params = {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    slidesPerColumn: 2,
+    slidesPerColumnFill: 'row',
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        slidesPerColumn: 2,
+        spaceBetween: 0
+      },
+      //0까지
+      601: {
+        slidesPerView: 2,
+        slidesPerColumn: 2,
+        spaceBetween: 14
+      },
+      //601까지
+      1281: {
+        slidesPerView: 3,
+        slidesPerColumn: 2,
+        spaceBetween: 14
+      }
+      //1280까지
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  }
+  const arraySlide = slideInfo.map((item, index) => {
+    console.log(item)
+    const {id, title, url, name, people, category} = item
+    console.log(url)
+    return (
+      <Slide key={index}>
+        <ContentBox {...item}>
+          <ImgBox bg={url}></ImgBox>
+          <Info>
+            <Category>{category}</Category>
+            <Title>{title}</Title>
+            <Name>{name}</Name>
+          </Info>
+        </ContentBox>
+      </Slide>
+    )
+  })
+  return (
+    <>
+      <SwiperWrap>
+        <h1>인기 DJ</h1>
+        <Swiper {...params}>{arraySlide}</Swiper>
+      </SwiperWrap>
+    </>
+  )
 }
+const SwiperWrap = styled.div`
+  width: 1210px;
+  margin: 0 auto;
+  @media (max-width: ${WIDTH_PC}) {
+    width: 100%;
+  }
+  @media (max-width: ${WIDTH_TABLET}) {
+    width: 100%;
+  }
+  @media (max-width: ${DEVICE_MOBILE}) {
+    width: 100%;
+  }
+`
+const Slide = styled.div`
+  width: 32.56%;
+`
+const Category = styled.span`
+  display: block;
+  font-size: 14px;
+  font-weight: normal;
+  letter-spacing: -0.35px;
+  color: #bdbdbd;
+`
+const Title = styled.h2`
+  font-size: 18px;
+  letter-spacing: -0.45px;
+  margin: 20px 0 8px 0;
+  box-sizing: border-box;
+`
+
+const Name = styled.strong`
+  display: block;
+  font-size: 14px;
+  font-weight: normal;
+  letter-spacing: -0.35px;
+  padding-right: 22px;
+  box-sizing: border-box;
+  color: #8556f6;
+`
+
+const ImgBox = styled.div`
+  float: left;
+  width: 48.22%;
+  height: 100%;
+  background: url(${props => props.bg}) no-repeat center center / cover;
+`
+
+const Info = styled.div`
+  width: 51.77%;
+  padding: 8px 20px 11px 20px;
+  box-sizing: border-box;
+  float: left;
+`
+const Imgbox = styled.div``
+const InfoBox = styled.div``
+const InfoTop = styled.div``
+const InfoTitle = styled.div``
+const InfoBj = styled.div``
+const InfoBottom = styled.div``
+const InfoPeople = styled.div``
+const InfoLike = styled.div``
--- web/src/pages/main/index.js
+++ web/src/pages/main/index.js
@@ -21,7 +21,108 @@ const Main = props => {
   //---------------------------------------------------------------------
   //context
   //state
-
+  const slideInfo = [
+    {
+      id: '1',
+      category: '노래/연주',
+      title: '이 밤을 신나게 보내요',
+      url: 'http://img.khan.co.kr/news/2017/07/11/l_2017071201001381500111321.jpg',
+      name: '끝날 때까지 끝난게 아닌인연을 위해',
+      people: '1,860',
+      like: '5,200',
+      icon: 'BEST'
+    },
+    {
+      id: '2',
+      category: '책/스토리',
+      title: '오후를 밝혀줄 빛',
+      url: 'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F99A2AC3E5D15A5F629',
+      name: '누운별',
+      people: '1,230',
+      like: '4,110',
+      icon: 'HOT'
+    },
+    {
+      id: '3',
+      category: '건강/스포츠',
+      title: '아침을 위한 활기차게 반려견과 함께하는 방',
+      url: 'http://image.cine21.com/resize/cine21/person/2019/0729/11_42_45__5d3e5d25ef4fb[H800-].jpg',
+      name: '견주 모여라',
+      people: '1,092',
+      like: '8,999',
+      icon: 'NEW'
+    },
+    {
+      id: '4',
+      category: '노래/연주',
+      title: '저녁엔 냥이와 함께 라이프 스타일',
+      url: 'http://image.cine21.com/resize/cine21/person/2018/0205/10_31_41__5a77b3fdde0a6[W680-].jpg',
+      name: '별빛이 내린다~',
+      people: '1,013',
+      like: '1,818',
+      icon: 'NEW'
+    },
+    {
+      id: '5',
+      category: '책/스토리',
+      title: '감성 재즈의 1인자',
+      url: 'http://image.cine21.com/resize/cine21/person/2019/0311/15_07_55__5c85fb3b335df[W680-].jpg',
+      name: '속삭이는 달빛',
+      people: '1,002',
+      like: '8,212',
+      icon: 'STAR'
+    },
+    {
+      id: '6',
+      title: '째즈와 함께 시간 여행을 떠나요',
+      category: '건강/스포츠',
+      url: 'https://mblogthumb-phinf.pstatic.net/20150707_79/icccaccc_1436274069809nV3TH_PNG/2015-07-07-21-49-13.png?type=w420',
+      name: '감성 재즈의 1인자',
+      people: '772',
+      like: '1,212',
+      icon: 'HOT'
+    },
+    {
+      id: '7',
+      category: '건강/스포츠',
+      title: '기분좋은 아침, 달콤 신청곡 라디오',
+      url: 'http://iflv14.afreecatv.com/save/afreeca/station/2019/1230/11/thumb/1577672974671669_L_8.jpg',
+      name: '볼빵°.°',
+      people: '615',
+      like: '5,409',
+      icon: 'VIP'
+    },
+    {
+      id: '8',
+      category: '건강/스포츠',
+      title: '가야금 , ㅎㅎㅎㅎㅎㅎㅎㅎ 💌',
+      url: 'http://admin.img.afreecatv.com/thema_group_vod/2020/01/05/24985e11c9b1a63ce.jpg',
+      name: '비익연리',
+      people: '599',
+      like: '5,111',
+      icon: 'NEW'
+    },
+    {
+      id: '9',
+      category: '건강/스포츠',
+      title: '존예보스 💿 Music Radio 📻',
+      url: 'http://admin.img.afreecatv.com/thema_group_vod/2020/01/07/23745e144e84e8f72.jpg',
+      name: '라온제나☆°',
+      people: '329',
+      like: '9,212',
+      icon: 'BEST'
+    },
+    {
+      id: '10',
+      category: '건강/스포츠',
+      title: '제목없는 방입니다.',
+      url: 'http://admin.img.afreecatv.com/thema_group_vod/2020/01/02/94445e0dbacda8bec.jpg',
+      name: '공신:강성태',
+      people: '222',
+      like: '3,212',
+      icon: 'NEW'
+    }
+  ]
   //---------------------------------------------------------------------
   return (
     <Layout {...props}>
@@ -33,7 +134,7 @@ const Main = props => {
       </RangkingWrap>
       {/* 인기 DJ 영역 */}
       <PopularWrap>
-        <PopularDJ />
+        <PopularDJ Info={slideInfo} />
       </PopularWrap>
       {/* 라이브 list 영역, 캐스트 list 영역 */}
       <ContentListWrap>
