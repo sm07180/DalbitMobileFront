fee1ce2e67b906654e350e3f570a8a868ad3b0f8
kimhogyeom <hgkim1118@inforex.co.kr>
2020-01-23 14:27:53 +0900

    webfont 작업

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -53,12 +53,12 @@ module.exports = {
           //name: '[name].[ext]?[hash]',
           publicPath: './dist',
           name: 'images/[name].[ext]',
-          limit: 10000, // 10kb
+          //limit: 10000, // 10kb
           fallback: 'file-loader' // 파일사이즈가 10k보다 큰 경우, file-loader 이용하여 파일 복사
         }
       },
       {
-        test: /\.(jpe?g|png|gif|svg|ico)$/i, // 이미지 로더
+        test: /\.(jpe?g|png|gif|svg|ico|woff|woff2|ttf|eot)$/i, // 이미지 로더
         use: [
           {
             loader: 'file-loader',
