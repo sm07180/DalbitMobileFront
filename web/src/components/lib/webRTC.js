6211f42a7aac5ac36d76e852b4e91d31be31b99f
Park Songwon <songwon.bak@gmail.com>
2020-01-30 17:52:11 +0900

    코드 정리

--- web/src/components/lib/webRTC.js
+++ web/src/components/lib/webRTC.js
@@ -7,30 +7,8 @@ export const checkMic = async () => {
   const constraint = {audio: true, video: false}
   let mediaStream = null
 
-  const streamHandler = () => {
-    ;() => {
-      // check if mic is provided
-      navigator.mediaDevices
-        .enumerateDevices()
-        .then(devices => {
-          devices.forEach(device => {
-            if (device.kind === 'audioinput') {
-              console.log('mic', device)
-            }
-          })
-        })
-        .catch(err => {
-          console.log(err.name + ': ' + error.message)
-        })
-    }
-  }
-
-  const errorHandler = () => {
-    alert('Mic is not connected.')
-  }
-
   await navigator.mediaDevices
-    .getUserMedia(constraint, streamHandler, errorHandler)
+    .getUserMedia(constraint)
     .then(stream => {
       mediaStream = stream
     })
