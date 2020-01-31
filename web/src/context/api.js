76c0e097ee39929b1f25dc2c4845abafa83b26e2
kimhogyeom <hgkim1118@inforex.co.kr>
2020-01-31 11:21:14 +0900

    방송관련 api 추가

--- web/src/context/api.js
+++ web/src/context/api.js
@@ -27,19 +27,251 @@ import {API_SERVER} from 'context/config'
 export default class API {
   //---------------------------------------------------------------------방송관련
   /**
-   * @brief 방송방 리스트
+   * @brief 방송방 생성
+   * @method "POST"
+   * @todo
+   * @param int roomType                //*방송종류
+   * @param string title                //*제목
+   * @param string bgImg                //*백그라운드 이미지 경로
+   * @param int bgImgRacy               //백그라운드 구글 선정성
+   * @param string welcomMsg            //*환영 메시지
+   * @param string notice               //공지사항
+   * @param int entryType               //*entry 타입 (0:전체,1:팬,20세이상)
+   * @param int os                      //*OS 구분(1:Android,2:IOS,3:PC)
+   * @param string deviceId             //디바이스 고유아이디
+   * @param string deviceToken          //디바이스 토큰
+   * @param string appVer               //앱 버전
+   * @create 김호겸 2020.01.31
+   */
+  static broad_create = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/broad/create`, method: method || 'POST', data: data})
+  }
+
+  /**
+   * @brief 방송방 참여하기 (익명 로그인일때 프로시저 memLogin:0 추가)
+   * @method "POST"
+   * @todo
+   * @param int roomNo                //*방송방번호
+   * @create 김호겸 2020.01.31
+   */
+  static broad_join = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/broad/join`, method: method || 'POST', data: data})
+  }
+
+  /**
+   * @brief 방송방 정보수정
+   * @method "POST"
+   * @todo
+   * @param string roomNo               //*방송방번호
+   * @param int roomType                //*방송종류
+   * @param string title                //*제목
+   * @param string bgImg                //백그라운드 이미지 경로
+   * @param string bgImgDel             //삭제할 백그라운드 이미지 경로
+   * @param int bgImgRacy               //백그라운드 구글 선정성
+   * @param string welcomMsg            //환영 메시지
+   * @param string notice               //공지사항
+   * @param int os                      //*OS 구분(1:Android,2:IOS,3:PC)
+   * @param string deviceId             //디바이스 고유아이디
+   * @param string deviceToken          //디바이스 토큰
+   * @param string appVer               //앱 버전
+   * @create 김호겸 2020.01.31
+   */
+  static broad_edit = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/broad/edit`, method: method || 'POST', data: data})
+  }
+  /**
+   * @brief 방송방 리스트 (익명 로그인일때 프로시저 memLogin:0 추가)
    * @method "GET"
-   * @todo 마지막페이지인줄 어떻게 알수있나? last?
-   * @param string roomType            //*회원구분
-   * @param string page               //*아이디,소셜아이디
-   * @param string records             //비밀번호
+   * @todo
+   * @param int roomType                //*방주제
+   * @param int page                    //*페이지번호
+   * @param int records                 //*페이지당 리스트 개수
+   * @create 김호겸 2020.01.31
    */
   static broad_list = async obj => {
     const {url, method, data} = obj || {}
-    return await ajax({...obj, url: url || `/member/login`, method: method || 'POST', data: data})
+    return await ajax({...obj, url: url || `/broad/list`, method: method || 'GET', data: data})
   }
 
-  //---------------------------------------------------------------------
+  /**
+   * @brief 방송방 참여자 리스트
+   * @method "GET"
+   * @todo
+   * @param string roomNo               //*방송방번호
+   * @param int page                    //페이지번호
+   * @param int records                 //페이지당 리스트 개수
+   * @create 김호겸 2020.01.31
+   */
+  static broad_listeners = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/broad/listeners`, method: method || 'GET', data: data})
+  }
+  /**
+   * @brief 방송방 좋아요 추가
+   * @method "POST"
+   * @todo
+   * @param string roomNo                 //*방송방번호
+   * @create 김호겸 2020.01.31
+   */
+  static broad_likes = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/broad/likes`, method: method || 'POST', data: data})
+  }
+
+  /**
+   * @brief 게스트 지정하기
+   * @method "POST"
+   * @todo
+   * @param string roomNo                 //*방송방번호
+   * @param string memNo                  //*게스트 회원번호
+   * @param string title                  //*본방제목으로
+   * @create 김호겸 2020.01.31
+   */
+  static broad_guest = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/broad/guest`, method: method || 'POST', data: data})
+  }
+
+  /**
+   * @brief 게스트 취소하기
+   * @method "DELETE"
+   * @todo
+   * @param string roomNo                 //*방송방번호
+   * @param string memNo                  //*게스트 회원번호
+   * @create 김호겸 2020.01.31
+   */
+  static broad_guest = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/broad/guest`, method: method || 'DELETE', data: data})
+  }
+
+  /**
+   * @brief 방송방 공지사항 조회
+   * @method "GET"
+   * @todo
+   * @param string roomNo                 //*방송방번호
+   * @create 김호겸 2020.01.31
+   */
+  static broad_notice = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/broad/notice`, method: method || 'GET', data: data})
+  }
+
+  /**
+   * @brief 방송방 공지사항 입력/수정
+   * @method "POST"
+   * @todo
+   * @param string roomNo                 //*방송방번호
+   * @param string notice                 //*공지사항
+   * @create 김호겸 2020.01.31
+   */
+  static broad_notice = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/broad/notice`, method: method || 'POST', data: data})
+  }
+  /**
+   * @brief 방송방 공지사항 삭제
+   * @method "DELETE"
+   * @todo
+   * @param string roomNo                 //*방송방번호
+   * @create 김호겸 2020.01.31
+   */
+  static broad_notice = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/broad/notice`, method: method || 'DELETE', data: data})
+  }
+  /**
+   * @brief 방송방 사연등록
+   * @method "POST"
+   * @todo
+   * @param string roomNo                 //*방송방번호
+   * @param string contents                 //*사연등록
+   * @create 김호겸 2020.01.31
+   */
+  static broad_stoty = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/broad/stoty`, method: method || 'POST', data: data})
+  }
+  /**
+   * @brief 방송방 사연 목록 조회
+   * @method "GET"
+   * @todo
+   * @param string roomNo                 //*방송방번호
+   * @param string contents                 //*사연등록
+   * @create 김호겸 2020.01.31
+   */
+  static broad_stoty = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/broad/stoty`, method: method || 'GET', data: data})
+  }
+  /**
+   * @brief 방송방 사연 목록 삭제
+   * @method "DELETE"
+   * @todo
+   * @param string roomNo                 //*방송방번호
+   * @param int storyIdx                 //*사연 인덱스 번호
+   * @create 김호겸 2020.01.31
+   */
+  static broad_stoty = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/broad/stoty`, method: method || 'DELETE', data: data})
+  }
+  /**
+   * @brief 방송방 공유링크 확인 (익명 로그인일때 프로시저 memLogin:0 추가)
+   * @method "GET"
+   * @todo
+   * @param string link                //*링크코드
+   * @create 김호겸 2020.01.31
+   */
+  static broad_link = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/broad/link`, method: method || 'GET', data: data})
+  }
+
+  /**
+   * @brief 방송방 강퇴하기
+   * @method "POST"
+   * @todo
+   * @param string roomNo                 //*방송방번호
+   * @param string blockNo                 //*강퇴회원번호
+   * @create 김호겸 2020.01.31
+   */
+  static broad_kickout = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/broad/link`, method: method || 'POST', data: data})
+  }
+
+  /**
+   * @brief 방송방 정보조회 (익명 로그인일때 프로시저 memLogin:0 추가)
+   * @method "GET"
+   * @todo
+   * @param string roomNo                 //*방송방번호
+   * @create 김호겸 2020.01.31
+   */
+  static broad_info = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/broad/info`, method: method || 'GET', data: data})
+  }
+  //--------------------------------------------------------------------- 회원 관련
+  /**
+   * @brief 토큰조회
+   * @method "GET"
+   * @param int    os                 //*OS구분
+   * @param string deviceid           //*디바이스 고유아이디
+   * @param string deviceToken        //디바이스 토큰
+   * @param string appVer             //앱 버전
+   * @param string appAdId            //광고 아이디
+   * @create 김호겸 2020.01.15
+   */
+
+  static getToken = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/token`, method: method || 'GET', data: data})
+  }
+
   /**
    * @brief 회원로그인
    * @method "POST"
@@ -143,7 +375,7 @@ export default class API {
    */
   static pan_register = async obj => {
     const {url, method, data} = obj || {}
-    return await ajax({...obj, url: url || `/mypage/pan/{스타회원번호}}`, method: method || 'POST', data: data})
+    return await ajax({...obj, url: url || `/mypage/pan`, method: method || 'POST', data: data})
   }
 
   /**
@@ -154,7 +386,7 @@ export default class API {
    */
   static pan_delete = async obj => {
     const {url, method, data} = obj || {}
-    return await ajax({...obj, url: url || `/mypage/pan/{스타회원번호}}`, method: method || 'DELETE', data: data})
+    return await ajax({...obj, url: url || `/mypage/pan`, method: method || 'DELETE', data: data})
   }
 
   /**
@@ -165,7 +397,7 @@ export default class API {
    */
   static member_info = async obj => {
     const {url, method, data} = obj || {}
-    return await ajax({...obj, url: url || `/profile/{조회회원번호}}`, method: method || 'GET', params: params})
+    return await ajax({...obj, url: url || `/profile}`, method: method || 'GET', params: params})
   }
 
   /**
@@ -181,11 +413,19 @@ export default class API {
   /**
    * @brief 회원 방송방 기본설정 수정하기
    * @method "GET"
+   * @param int roomType          //*방송종류
+   * @param string title          //*제목
+   * @param string bgImg          //백그라운드 이미지 경로
+   * @param string bgImgDel       //삭제할 백그라운드 이미지
+   * @param int bgImgRacy         //백그라운드 구글 선정성
+   * @param string wecomMsg       //*환영 메세지
+   * @param string notice         //공지사항
+   * @param int entryType         //*입장 (0:전체, 1:팬 ,2:20세이상)
    * @create 김호겸 2020.01.15
    */
   static member_broadcast_basic_Edit = async obj => {
     const {url, method, data} = obj || {}
-    return await ajax({...obj, url: url || `/mypage/broad`, method: method || 'GET', params: params})
+    return await ajax({...obj, url: url || `/mypage/broad`, method: method || 'POST', data: data})
   }
 
   /**
@@ -213,7 +453,7 @@ export default class API {
   }
 
   /**
-   * @brief 회원 해제하기
+   * @brief 회원 차단 해제하기
    * @method "DELETE"
    * @param string    memNo             //*차단회원번호
    * @create 김호겸 2020.01.15
@@ -221,6 +461,125 @@ export default class API {
   static member_block_delete = async obj => {
     const {url, method, data} = obj || {}
     return await ajax({...obj, url: url || `/mypage/block`, method: method || 'DELETE', data: data})
+  }
+
+  /**
+   * @brief 회원 알림설정 조회하기
+   * @method "GET"
+   * @create 김호겸 2020.01.31
+   */
+  static member_notify_request = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/mypage/notify`, method: method || 'GET', data: data})
+  }
+
+  /**
+   * @brief 회원 알림설정 수정하기
+   * @method "POST"
+   * @param int isAll                 //*전체알림
+   * @param int isFanReg              //*팬등록알림
+   * @param int isBoard               //*팬보드알림
+   * @param int isStarCast            //*스타방송알림
+   * @param int isStarNoti            //*스타공지사항알림
+   * @param int isEvtNoti             //*이벤트공지알림
+   * @param int isSearch              //*검색전용
+   * @create 김호겸 2020.01.31
+   */
+  static member_notify_edit = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/mypage/notify`, method: method || 'POST', data: data})
+  }
+
+  /**
+   * @brief 회원 팬보드 팬보드 댓글 달기
+   * @method "POST"
+   * @param string memNo              //*팬보드 회원번호
+   * @param int depth                 //* (1:댓글,2:대댓글)
+   * @param int boardNo               //대댓글일 경우 부모 댓글번호
+   * @param int content               //*내용
+   * @create 김호겸 2020.01.31
+   */
+  static member_fanboard_add = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/profile/board`, method: method || 'POST', data: data})
+  }
+  /**
+   * @brief 회원 팬보드 목록조회
+   * @method "GET"
+   * @param string memNo              //*팬보드 회원번호
+   * @param int page                  //페이지번호
+   * @param int records               //페이지당 리스트 수
+   * @create 김호겸 2020.01.31
+   */
+  static member_fanboard_list = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/profile/board`, method: method || 'GET', data: data})
+  }
+
+  /**
+   * @brief 회원 팬보드 삭제하기
+   * @method "DELETE""
+   * @param string memNo                  //*팬보드 회원번호
+   * @param int boardIdx                  //*페이지번호
+   * @create 김호겸 2020.01.31
+   */
+  static member_fanboard_delete = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/profile/board`, method: method || 'DELETE', data: data})
+  }
+  /**
+   * @brief 회원 팬보드 대댓글 조회하기
+   * @method "GET"
+   * @param string memNo              //*팬보드 회원번호
+   * @param int boardNo                  //*댓글 그룹번호
+   * @create 김호겸 2020.01.31
+   */
+  static member_fanboard_reply = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/profile/board/reply`, method: method || 'GET', data: data})
+  }
+  /**
+   * @brief 회원 방송방 빠른말 가져오기 (없을 경우 기획에서 정의된 기본 데이터 노출되어야함)
+   * @method "GET"
+   * @create 김호겸 2020.01.31
+   */
+  static member_broadcast_shortcut = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/mypage/shortcut`, method: method || 'GET', data: data})
+  }
+
+  /**
+   * @brief 회원 방송방 빠른말 저장하기
+   * @method "POST"
+   * @param string order_1                  //*첫번째 명령
+   * @param string text_1                   //*첫번째 내용
+   * @param string onOff_1                  //*첫번째 사용여부(on/off)
+   * @param string order_2                  //*두번째 명령
+   * @param string text_2                   //*두번째 내용
+   * @param string onOff_2                  //*두번째 사용여부(on/off)
+   * @param string order_3                  //*세번째 명령
+   * @param string text_3                   //*세번째 내용
+   * @param string onOff_3                  //*세번째 사용여부(on/off)
+   * @create 김호겸 2020.01.31
+   */
+  static member_broadcast_shortcut = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/mypage/shortcut`, method: method || 'POST', data: data})
+  }
+
+  //-------------------------------------------------------------------- 포토 관련
+
+  /**
+   * @brief 이미지 업로드
+   * @method "POST"
+   * @param string file                      //file 객체
+   * @param string dataUrl                   //data URL
+   * @param string imageUrl                  //image URL
+   * @create 김호겸 2020.01.31
+   */
+  static image_upload = async obj => {
+    const {url, method, data} = obj || {}
+    return await ajax({...obj, url: url || `/upload`, method: method || 'POST', data: data})
   }
 }
 //---------------------------------------------------------------------
