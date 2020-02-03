178461e5a6eaf86de49bd514a934e4dec4cb343f
kimhogyeom <hgkim1118@inforex.co.kr>
2020-02-03 10:13:31 +0900

    header test

--- web/src/pages/app/index.js
+++ web/src/pages/app/index.js
@@ -77,14 +77,14 @@ export default () => {
       if (this.readyState == this.HEADERS_RECEIVED) {
         console.log('2')
         var contentType = client.getResponseHeader('customHeader')
-        var contentType2 = client.getResponseHeader('authToken')
+        var contentType2 = client.getResponseHeader('authtoken')
         var contentType3 = client.getAllResponseHeaders()
         var contentType4 = client.getResponseHeader('Content-Type')
 
-        console.log('contentType = ' + contentType)
-        console.log('contentType2 = ' + contentType2)
-        console.log('contentType3 = ' + contentType3)
-        console.log('contentType4 = ' + contentType4)
+        console.log('customHeader = ' + contentType)
+        console.log('authtoken = ' + contentType2)
+        console.log('All-context = ' + contentType3)
+        console.log('contentType = ' + contentType4)
         // if (contentType != my_expected_type) {
         //   client.abort();
         // }
