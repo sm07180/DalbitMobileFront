2e9ff73eecd4a07d17b1bb43fa206f0d139febd3
yeshtml5 <yeshtml5@gmail.com>
2020-02-05 08:56:44 +0900

    App.js 서버에서 내려오는값테스트

--- web/src/App.js
+++ web/src/App.js
@@ -3,7 +3,7 @@
  * @brief React실행시토큰검증및 필수작업
  * @notice
  */
-import React, {useContext} from 'react'
+import React, {useMemo, useContext} from 'react'
 
 //context
 import {Context} from 'context'
@@ -14,15 +14,12 @@ export default () => {
   //context
   const context = useContext(Context)
 
-  const obj = {
-    locale: 'KR',
-    deviceId: '5E5D8EE5-FC84-40BE-84D7-73598ABC9FEB',
-    os: '2',
-    language: 'ko',
-    deviceToken: 'd6ZW5ZHyEUI9qVeLhXyItO:APA91bH4hFV0H4pWv6CIvT2LAUVwfiHKeVos0rbSZE8x4DTmkbxns9XwP6f3t_qXEkaHWxvJtpO6hTAaNzFtY59l5pG1Rv_Rt0zBD5C1paVnrgFavJOGBgeUJ2rDsElEPI9deFVsj2WV'
-  }
-  console.log(JSON.stringify(obj))
-  //---------------------------------------------------------------------방송관련
+  //SERVER & APP -> REACT
+  const customHeader = useMemo(() => {
+    const ele = document.getElementById('customHeader').value
+    console.log(ele)
+  })
 
+  //---------------------------------------------------------------------
   return <Route />
 }
