22a31d462882cb64285142ee485104255bec05d6
황상한 <nmc2711@inforex.co.kr>
2020-02-04 16:29:25 +0900

    gnb1차작업완료

--- web/src/pages/common/gnb/menu.js
+++ web/src/pages/common/gnb/menu.js
@@ -1,16 +1,59 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useContext} from 'react'
 import styled from 'styled-components'
-
+import {Context} from 'context'
 //component
 import Gnb from './gnb-layout'
-
+import {Link, NavLink} from 'react-router-dom'
 export default props => {
   //---------------------------------------------------------------------
+  const context = useContext(Context)
+  //data
+  const info = [
+    {title: '라이브', url: '/live'},
+    {title: '캐스트', url: '/store?캐스트'},
+    {title: '스토어', url: '/store'},
+    {title: '이벤트', url: '/event'},
+    {title: '랭킹', url: '/store?랭킹'},
+    {title: '고객센터', url: '/store?고객센터'},
+    {title: '설정', url: '/store?설정'}
+  ]
+  const makeNavi = () => {
+    return info.map((list, idx) => {
+      const _title = info[idx].title
+      const _url = info[idx].url
+      return (
+        <NavLink title={_title} key={idx} to={_url} exact activeClassName="on">
+          <LinkLi
+            onClick={() => {
+              context.action.updateGnbVisible(false)
+            }}>
+            <span>{_title}</span>
+          </LinkLi>
+        </NavLink>
+      )
+    })
+  }
 
   return (
     <>
       <Gnb>
-        <p>나는 기본 메뉴입니다. 나는 레이아웃의 차일드에요.</p>
+        <NoticeWrap>
+          <Nheader>
+            <ICON></ICON>
+            <Title>전체메뉴</Title>
+          </Nheader>
+
+          <CONTENT className={`${props.type}`}>
+            <LiveStart>
+              <StartBtn></StartBtn>
+              <StartIcon>
+                <span></span>
+                <h2>방송하기</h2>
+              </StartIcon>
+            </LiveStart>
+            {makeNavi()}
+          </CONTENT>
+        </NoticeWrap>
       </Gnb>
     </>
   )
@@ -18,3 +61,107 @@ export default props => {
 
 //---------------------------------------------------------------------
 //styled
+const NoticeWrap = styled.div`
+  width: 100%;
+`
+const Nheader = styled.div`
+  width: 100%;
+  height: 80px;
+  padding: 16px 20px 16px 20px;
+  box-sizing: border-box;
+  &:after {
+    display: block;
+    clear: both;
+    content: '';
+  }
+`
+const ICON = styled.div`
+  float: left;
+  width: 48px;
+  height: 48px;
+  margin-right: 10px;
+  background: url('https://devimage.dalbitcast.com/images/api/ic_menu_normal.png') no-repeat center center / cover;
+`
+const Title = styled.h2`
+  float: left;
+  color: #fff;
+  font-size: 20px;
+  line-height: 48px;
+  letter-spacing: -0.5px;
+  text-align: left;
+`
+const CONTENT = styled.div`
+  width: 100%;
+  height: calc(100vh -80px);
+  padding: 10px 20px 0 20px;
+  box-sizing: border-box;
+  /* background-color: white; */
+`
+const LiveStart = styled.div`
+  position: relative;
+  width: 100%;
+  height: 40px;
+  margin-bottom: 30px;
+  cursor: pointer;
+`
+const StartBtn = styled.button`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 40px;
+  border-radius: 20px;
+  background-color: #fff;
+`
+const StartIcon = styled.div`
+  width: 100px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  &:after {
+    display: block;
+    clear: both;
+    content: '';
+  }
+  & span {
+    float: left;
+    width: 36px;
+    height: 36px;
+    background: url('https://devimage.dalbitcast.com/images/api/ico-cast-w.png') no-repeat center center/ cover;
+  }
+  & h2 {
+    float: left;
+    width: 64px;
+    color: #8556f6;
+    font-size: 18px;
+    font-weight: 600;
+    line-height: 36px;
+    letter-spacing: -0.45px;
+  }
+`
+const LinkLi = styled.div`
+  position: relative;
+  width: 100%;
+  height: 40px;
+  border-bottom: 1px solid #bdbdbd;
+  & span {
+    display: block;
+    color: #fff;
+    font-size: 16px;
+    line-height: 40px;
+    letter-spacing: -0.4px;
+    transform: skew(-0.03deg);
+  }
+  &:after {
+    display: block;
+    position: absolute;
+    top: 50%;
+    right: 0;
+    width: 24px;
+    height: 24px;
+    background: url('http://www.hwangsh.com/img/ic_arrow_right_color_s.png') no-repeat center center / cover;
+    transform: translateY(-50%);
+    content: '';
+  }
+`
--- web/src/pages/common/gnb/search.js
+++ web/src/pages/common/gnb/search.js
@@ -8,6 +8,10 @@ import {COLOR_MAIN, COLOR_POINT_Y, COLOR_POINT_P} from 'context/color'
 //component
 
 export default props => {
+  const [search, setSearch] = useState('')
+  const handleChange = event => {
+    setSearch(event.target.value)
+  }
   //---------------------------------------------------------------------
   //context
   const context = new useContext(Context)
@@ -17,10 +21,10 @@ export default props => {
         <Close
           onClick={() => {
             context.action.updateGnbVisible(false)
-          }}>
-          닫기
-        </Close>
-        <p>나는 찾기입니다. 나는 레이아웃이 달라요 !!</p>
+          }}></Close>
+        <SearchWrap>
+          <input type="text" placeholder="인기 DJ, 꿀보이스, 나긋한 목소리 등 검색어를 입력해 보세요" value={search} onChange={handleChange} />
+        </SearchWrap>
       </Gnb>
     </>
   )
@@ -43,4 +47,52 @@ const Gnb = styled.div`
   }
 `
 
-const Close = styled.button``
+const SearchWrap = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 41.87%;
+  height: 64px;
+  transform: translate(-50%, -50%);
+  & input {
+    display: block;
+    width: 100%;
+    height: 100%;
+    padding-left: 20px;
+    border: 1px solid #fff;
+    box-sizing: border-box;
+    background-color: #8556f6;
+    transform: skew(-0.03deg);
+    position: relative;
+  }
+  & input::placeholder {
+    opacity: 0.3;
+    color: white;
+    font-size: 16px;
+    letter-spacing: -0.4px;
+  }
+  & input:focus {
+    color: white;
+    font-size: 16px;
+    letter-spacing: -0.4px;
+  }
+  &:after {
+    position: absolute;
+    top: 50%;
+    right: 0;
+    width: 48px;
+    height: 48px;
+    background: url('https://devimage.dalbitcast.com/images/api/search6.png') no-repeat center center / cover;
+    transform: translateY(-50%);
+    content: '';
+    cursor: pointer;
+  }
+`
+const Close = styled.button`
+  position: absolute;
+  top: 16px;
+  right: 10px;
+  width: 48px;
+  height: 48px;
+  background: url('https://devimage.dalbitcast.com/images/api/ic_close.png') no-repeat center center / cover;
+`
