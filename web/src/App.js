a2f10eefe81be96165e5d9fe86f7866da441de0c
yeshtml5 <yeshtml5@gmail.com>
2020-02-05 11:34:35 +0900

    App.js 업데이트

--- web/src/App.js
+++ web/src/App.js
@@ -1,28 +1,41 @@
 /**
  * @file App.js
- * @brief React실행시토큰검증및 필수작업
+ * @brief React 최초실행시토큰검증및 필수작업
  * @notice
  */
-import React, {useMemo, useContext} from 'react'
-
+import React, {useMemo, useState, useEffect, useContext} from 'react'
+//components
+import Api from 'context/api'
 //context
 import {Context} from 'context'
 /*-Route-*/
 import Route from './Route'
 
 export default () => {
+  //---------------------------------------------------------------------
   //context
   const context = useContext(Context)
+  //useState
+  const [fetch, setFetch] = useState(null)
 
   //SERVER & APP -> REACT
   const customHeader = useMemo(() => {
-    if (document.getElementById('customHeader') === null) return
-    const ele = document.getElementById('customHeader').value
-    //예외처리
-    if (ele === null || ele === '') return
-    console.log(ele)
+    const element = document.getElementById('customHeader')
+    if (element === null) return
+    if (element.value === null || element.value === '') return
+    return typeof JSON.parse(element.value) === 'object' && JSON.parse(element.value)
   })
-
+  //---------------------------------------------------------------------
+  //fetch
+  async function fetchData(obj) {
+    const res = await Api.getToken({...obj})
+    setFetch(res)
+    console.log(res)
+  }
+  //useEffect
+  useEffect(() => {
+    fetchData({data: customHeader})
+  }, [])
   //---------------------------------------------------------------------
   return <Route />
 }
