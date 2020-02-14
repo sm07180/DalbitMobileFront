f74782fae5c0bb620d3daaaf6b35354f2ecfd4f1
Park Songwon <songwon.bak@gmail.com>
2020-02-14 10:57:29 +0900

    add host feature

--- web/src/pages/broadcast/content/host.js
+++ web/src/pages/broadcast/content/host.js
@@ -3,10 +3,46 @@
  */
 import React, {useEffect} from 'react'
 import styled from 'styled-components'
-//pages
 
+//context
+import {Context} from 'context'
+
+//etc
+import {getAudioStream} from 'components/lib/getStream'
+
+//pages
 // import Guide from ' pages/common/layout/guide.js'
 
 export default () => {
+  const context = new useContext(Context)
+  const {mediaHandler} = context
+  const [publishStatus, setPublishStatus] = useState(false)
+
+  const startPlayer = () => {
+    setPublishStatus(true)
+  }
+  const stopPlayer = () => {
+    setPublishStatus(false)
+  }
+
+  useEffect(() => {
+    if (mediaHandler) {
+      mediaHandler.setLocalStartCallback(startPlayer)
+      mediaHandler.setLocalStopCallback(stopPlayer)
+      mediaHandler.setType('host')
+      mediaHandler.setStreamId(streamId)
+      ;(async () => {
+        const audioStream = await getAudioStream()
+        mediaHandler.setAudioStream(audioStream)
+      })()
+    }
+
+    return () => {
+      if (mediaHandler) {
+        mediaHandler.resetLocalCallback()
+      }
+    }
+  }, [mediaHandler])
+
   return <h1>호스트</h1>
 }
