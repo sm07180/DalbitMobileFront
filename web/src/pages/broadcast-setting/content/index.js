17623c3179c639a516897842c1601a7df89e126b
kimhogyeom <hgkim1118@inforex.co.kr>
2020-02-17 10:45:52 +0900

    방송하기 url 수정

--- web/src/pages/broadcast-setting/content/index.js
+++ web/src/pages/broadcast-setting/content/index.js
@@ -174,7 +174,7 @@ export default props => {
         if (res) {
           if (res.code == 0) {
             console.log(res)
-            props.history.push('/broadcast/' + res.roomNo, res.data)
+            props.history.push('/broadcast/' + res.data.roomNo, res.data)
           } else {
             console.warn(res.message)
           }
