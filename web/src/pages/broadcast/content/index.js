8166f78a3efcbd0f0d402a8653b17ed597606761
yeshtml5 <yeshtml5@gmail.com>
2020-02-18 18:15:14 +0900

    broadcast

--- web/src/pages/broadcast/content/index.js
+++ web/src/pages/broadcast/content/index.js
@@ -8,9 +8,13 @@ import styled from 'styled-components'
 //context
 import {Context} from 'context'
 import {BroadCastStore} from '../store'
+import {IMG_SERVER, WIDTH_PC, WIDTH_PC_S, WIDTH_TABLET, WIDTH_TABLET_S, WIDTH_MOBILE, WIDTH_MOBILE_S} from 'context/config'
 
 //etc
 import {getAudioStream} from 'components/lib/getStream'
+//components
+import ChatUI from './chat-ui'
+import SlideContent from './SlideContent'
 
 //pages
 // import Guide from ' pages/common/layout/guide.js'
@@ -27,6 +31,9 @@ export default props => {
     store.action.updateRoomInfo(state)
     return state
   })
+  //useState
+  const [isSideOn, setIsSideOn] = useState(true)
+
   const hostRole = 3
   const listenerRole = 0
   //---------------------------------------------------------------------
@@ -53,7 +60,13 @@ export default props => {
       setPlayStatus(false)
     }
   }
-
+  useEffect(() => {
+    //방송방 페이지는 header, footer없음.
+    context.action.updateState({isOnCast: true})
+    return () => {
+      context.action.updateState({isOnCast: false})
+    }
+  }, [])
   useEffect(() => {
     if (mediaHandler) {
       if (roomRole === hostRole) {
@@ -89,6 +102,23 @@ export default props => {
 
   return (
     <Content>
+      <Chat>
+        {/* 채팅방 영역 */}
+        <ChatUI />
+      </Chat>
+      <SideBTN
+        onClick={() => {
+          setTimeout(() => {
+            setIsSideOn(!isSideOn)
+          }, 100)
+        }}>
+        사이드 영역 열고 닫기
+      </SideBTN>
+      <Side>
+        {/* side content 영역 */}
+
+        <SlideContent>{/* <Charge /> */}</SlideContent>
+      </Side>
       {roomRole === hostRole ? (
         <>
           <h1>Host BJ</h1>
@@ -177,8 +207,110 @@ export default props => {
   )
 }
 //---------------------------------------------------------------------
-const Content = styled.div`
-  p {
-    font-size: 12px;
+
+const Content = styled.section`
+  position: relative;
+  width: 1210px;
+  height: 100%;
+  margin: 2.5vh auto 0 auto;
+  &:after {
+    clear: both;
+    display: block;
+    content: '';
+  }
+  & > * {
+    height: calc(95vh - 80px);
+
+    @media (max-width: ${WIDTH_TABLET_S}) {
+      height: 100vh;
+    }
+  }
+
+  &.side-off > div:first-child {
+    width: calc(100% - 20px);
+    /* @media (max-width: ${WIDTH_TABLET_S}) {
+      width: 100%;
+    } */
+  }
+  &.side-off > div:last-child {
+    width: 20px;
+  }
+
+  @media (max-width: 1260px) {
+    width: 95%;
+  }
+
+  @media (max-width: ${WIDTH_TABLET_S}) {
+    width: 100%;
+    margin: 0;
+  }
+`
+
+//채팅창 레이아웃
+const Chat = styled.div`
+  float: left;
+  width: calc(100% - 408px);
+  @media (max-width: ${WIDTH_TABLET_S}) {
+    width: 100%;
+  }
+`
+//side영역
+const Side = styled.div`
+  overflow: hidden;
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 388px;
+  padding-left: 20px;
+  background: #fff;
+  /* min-width: 408px; */
+  @media (max-width: ${WIDTH_TABLET_S}) {
+    padding: 20px;
+    .side-off & {
+      padding: 0;
+    }
+  }
+
+  @media (max-width: 440px) {
+    width: calc(100% - 20px);
+  }
+`
+
+const SideBTN = styled.button`
+  position: relative;
+  display: block;
+  width: 20px;
+  background: #f2f2f2;
+  transform: none;
+  text-indent: -9999px;
+  z-index: 11;
+  &:after {
+    display: block;
+    margin: 0 auto;
+    width: 0;
+    height: 0;
+    border-top: 8px solid transparent;
+    border-bottom: 8px solid transparent;
+    border-left: 8px solid #757575;
+    content: '';
+  }
+  .side-off &:after {
+    border-left: 0;
+    border-right: 8px solid #757575;
+  }
+  @media (max-width: ${WIDTH_TABLET_S}) {
+    position: absolute;
+    right: 388px;
+    .side-off & {
+      right: 0;
+    }
+  }
+  @media (max-width: 440px) {
+    right: inherit;
+    left: 0;
+    .side-off & {
+      right: 0;
+      left: inherit;
+    }
   }
 `
