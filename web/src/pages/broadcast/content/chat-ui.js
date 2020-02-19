bc3f42c00720aadbd6d25b8d409c7a6a79229d9c
leeeunbee <herbione@inforex.co.kr>
2020-02-19 15:04:13 +0900

    채팅ui퍼블

--- web/src/pages/broadcast/content/chat-ui.js
+++ web/src/pages/broadcast/content/chat-ui.js
@@ -63,15 +63,47 @@ export default props => {
 
   //---------------------------------------------------------------------
   return (
-    <Content bgImg="">
+    <Content bgImg={`${IMG_SERVER}/images/api/pexels-photo.jpg`}>
       <InfoArea>정보 담는 영역</InfoArea>
       <CommentList className="scroll" onWheel={handleOnWheel} ref={chatArea}>
         <Scrollbars ref={scrollbars} autoHeight autoHeightMax={'100%'} onUpdate={scrollOnUpdate} autoHide>
-          <Message>
+          {/* 입장 */}
+          <Message className="enter-exit">
+            <div>
+              <span>cherry🍒 님이 입장하셨습니다.</span>
+            </div>
+          </Message>
+          {/* 기본 청취자 메시지 */}
+          <Message className="comment" profImg={`${IMG_SERVER}/images/api/ti375a8312.jpg`}>
             <figure></figure>
             <div>
-              <p>닉네임</p>
-              <pre>여러가지 버전 메시지 퍼블 중</pre>
+              <p>cherry🍒</p>
+              <pre>목소리 좋으시네요~ 자주 들으러 올게요!</pre>
+            </div>
+          </Message>
+          {/* 퇴장 */}
+          <Message className="enter-exit">
+            <div>
+              <span>cherry🍒 님이 퇴장하셨습니다.</span>
+            </div>
+          </Message>
+          {/* 기본 청취자 메시지 */}
+          <Message className="comment" profImg={`${IMG_SERVER}/images/api/tica034j16080551.jpg`}>
+            <figure></figure>
+            <div>
+              <p>러브angel~👼</p>
+              <pre>목소리가 스윗하네요 </pre>
+            </div>
+          </Message>
+          {/* 좋아요~ */}
+          <Message className="like" profImg={`${IMG_SERVER}/images/api/tica034j16080551.jpg`}>
+            <div>
+              <span>러브angel~👼 님이 좋아요를 하셨습니다.</span>
+            </div>
+          </Message>
+          <Message className="like" profImg={`${IMG_SERVER}/images/api/tica034j16080551.jpg`}>
+            <div>
+              <span>가장 못생긴 오징어🦑 님이 좋아요를 하셨습니다.</span>
             </div>
           </Message>
           {comments}
@@ -90,12 +122,23 @@ export default props => {
 const Content = styled.section`
   position: relative;
   height: 100%;
-  background: #555;
+  background: #555 url(${props => props.bgImg}) no-repeat center center / cover;
+  &:before {
+    display: block;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.4);
+    content: '';
+  }
 `
 
 const InfoArea = styled.div`
   height: 80px;
-  background: #212121;
+  border-bottom: 1px solid #e0e0e0;
+  background: none;
 `
 
 const CommentList = styled.div`
@@ -110,20 +153,94 @@ const CommentList = styled.div`
     bottom: 0;
     max-height: 100% !important;
     width: 100%;
+    & > div {
+      margin-right: -18px !important;
+    }
   }
 `
 
 const Message = styled.div`
-  border: 1px solid #000;
+  position: relative;
+  margin: 16px;
 
-  & + & {
-    margin-top: 20px;
+  figure {
+    display: inline-block;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+    background: #fff url(${props => props.profImg}) no-repeat center center / cover;
+  }
+
+  div {
+    padding-left: 44px;
+  }
+
+  &.enter-exit div {
+    text-align: center;
+    span {
+      display: flex;
+      width: 100%;
+      justify-content: center;
+      align-items: center;
+      text-align: center;
+      color: #fff;
+      font-size: 14px;
+      font-weight: 600;
+      letter-spacing: -0.35px;
+      transform: skew(-0.03deg);
+
+      &:before,
+      &:after {
+        border-top: 1px solid rgba(255, 255, 255, 0.3);
+        margin: 0 12px 0 0;
+        flex: 1 0 12px;
+        content: '';
+      }
+      &:after {
+        margin: 0 0 0 12px;
+        flex: 1 0 12px;
+      }
+    }
+  }
+
+  &.like span {
+    display: block;
+    padding: 7px;
+    background: rgba(0, 0, 0, 0.5);
+    border-radius: 36px;
+    font-size: 14px;
+    color: #fff;
+    text-align: center;
+    transform: skew(-0.03deg);
+  }
+
+  p {
+    margin: 0 0 8px 4px;
+    color: #fff;
+    font-size: 12px;
+    font-weight: 600;
+    letter-spacing: -0.3px;
+    transform: skew(-0.03deg);
   }
 
   pre {
+    display: inline-block;
+    padding: 9px 14px;
+    border-radius: 10px;
+    background: rgba(0, 0, 0, 0.3);
+    color: #fff;
+    font-family: 'NanumSquare';
+    font-size: 14px;
+    font-weight: 600;
+    line-height: 18px;
     white-space: pre-wrap;
     word-wrap: break-word;
     word-break: break-word;
+    letter-spacing: -0.35px;
+    transform: skew(-0.03deg);
   }
 `
 
