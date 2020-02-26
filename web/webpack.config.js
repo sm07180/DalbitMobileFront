6d08fa4296883d1e9ffe956ae5a973e2be1953b8
yeshtml5 <yeshtml5@gmail.com>
2020-02-26 16:33:15 +0900

    수정

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -12,7 +12,8 @@ module.exports = (env, options) => {
     output: {
       path: path.resolve(__dirname, 'dist'),
       publicPath: '/dist/',
-      filename: 'bundle.js' //2020-02-03 [kimhogyeom] 나중에 배포할때는 hash 적용해서 해야 할듯
+      filename: 'bundle.js', //2020-02-03 [kimhogyeom] 나중에 배포할때는 hash 적용해서 해야 할듯
+      chunkFilename: '[name].[chunkhash].js'
     },
     module: {
       rules: [
@@ -73,16 +74,12 @@ module.exports = (env, options) => {
     resolve: {
       extensions: ['*', '.js', '*.jsx'],
       modules: [path.resolve(__dirname, './src'), 'node_modules']
-      // alias: {
-      //   Context: path.resolve(__dirname, 'src/context/'),
-      //   Pages: path.resolve(__dirname, 'src/pages/'),
-      //   App: path.resolve(__dirname, 'src/pages/app/'),
-      //   Contents: path.resolve(__dirname, 'src/contents/'),
-      //   Styles: path.resolve(__dirname, 'src/styles/'),
-      //   Components: path.resolve(__dirname, 'src/components/'),
-      //   '@': path.resolve(__dirname, 'src/')
-      // }
     }
+    // optimization: {
+    //   splitChunks: {
+    //     chunks: 'all'
+    //   }
+    // }
   }
   if (options.mode === 'development') {
     config.devtool = 'source-map'
@@ -112,9 +109,29 @@ module.exports = (env, options) => {
       })
     ]
   } else {
+    config.devServer = {
+      hot: true,
+      contentBase: path.resolve('./dist'),
+      index: 'index.html',
+      port: 443,
+      host: '0.0.0.0',
+      historyApiFallback: true, // 서버사이드렌더링 문제 해결 코드 express 를 사용할 경우 nodejs 에서 해결
+      disableHostCheck: true,
+      https: {
+        key: fs.readFileSync(path.resolve(__dirname, 'key/privkey.pem')),
+        cert: fs.readFileSync(path.resolve(__dirname, 'key/fullchain.pem'))
+      }
+    }
     config.plugins = [
       new CleanWebpackPlugin({
         cleanAfterEveryBuildPatterns: ['./dist']
+      }),
+      new BundleAnalyzerPlugin(),
+      new HtmlWebPackPlugin({
+        template: './public/index.html', // public/index.html 파일을 읽는다.
+        filename: 'index.html', // output으로 출력할 파일은 index.html 이다.
+        title: 'Production',
+        showErrors: false // 에러 발생시 메세지가 브라우저 화면에 노출 된다.
       })
     ]
   }
