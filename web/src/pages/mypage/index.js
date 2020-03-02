d455797631927c35799c6a56713c54fa9f8066d3
Park Songwon <songwon.bak@gmail.com>
2020-03-02 11:31:24 +0900

    Remove unused code in mypage

--- web/src/pages/mypage/index.js
+++ web/src/pages/mypage/index.js
@@ -2,15 +2,9 @@
 
 * @file /mypage/index.js
  * @brief 마이페이지
- * @props index : 활성화 할 탭 선택
- *        0 : 팬보드
- *        1 : 내지갑
- *        2 : 리포트
- *        3 : 팬/스타
- *        4 : 설정
  */
 import React, {useEffect, useState} from 'react'
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route, Link} from 'react-router-dom'
 import styled from 'styled-components'
 
 //layout
@@ -38,29 +32,6 @@ import Report from './content/report'
 import FanStar from './content/fan-star'
 import Setting from './content/setting'
 
-function TabContentWrap(props) {
-  const {children, value, index, ...other} = props
-
-  return (
-    <TabContent component="div" role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`} aria-labelledby={`simple-tab-${index}`} {...other}>
-      {value === index && <Box p={3}>{children}</Box>}
-    </TabContent>
-  )
-}
-
-TabContentWrap.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired
-}
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`
-  }
-}
-
 //
 const User = props => {
   //---------------------------------------------------------------------
@@ -76,7 +47,7 @@ const User = props => {
         <h1>
           <a href="/">마이페이지</a>
         </h1>
-        <MypageButton>방송국 관리</MypageButton>
+        {/* <MypageButton>방송국 관리</MypageButton> */}
       </ContentHeader>
       <Content>
         <MyProfile />
@@ -100,53 +71,6 @@ const ContentHeader = styled.div`
   }
 `
 
-const MypageButton = styled.button``
-
 const Content = styled.section`
   margin: 30px 0 100px 0;
-`
-
-const TabsBar = styled(AppBar)`
-  margin-top: 30px;
-  background: #8555f6;
-
-  .MuiTabs-root {
-    width: 600px;
-    margin: 0 auto;
-  }
-  .MuiTab-root {
-    min-width: 16.6666%;
-  }
-  .MuiTabs-indicator {
-    height: 3px;
-    background-color: #5728c5;
-  }
-
-  @media (max-width: ${WIDTH_PC}) {
-    .MuiTabs-root {
-      width: 90%;
-    }
-  }
-
-  @media (max-width: ${WIDTH_TABLET}) {
-    .MuiTabs-root {
-      width: 100%;
-      padding: 0;
-    }
-    .MuiTab-root {
-      padding: 0;
-    }
-  }
-`
-
-const TabContent = styled(Typography)`
-  width: 600px;
-  margin: 30px auto;
-  @media (max-width: ${WIDTH_PC}) {
-    width: 90%;
-  }
-
-  .MuiBox-root {
-    padding: 0;
-  }
 `
