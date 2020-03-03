27af3db78222ecbc358626698df86800bb3c219d
Park Songwon <songwon.bak@gmail.com>
2020-03-03 09:54:30 +0900

    remove unused code

--- web/src/pages/mypage/index.js
+++ web/src/pages/mypage/index.js
@@ -14,15 +14,6 @@ import {WIDTH_PC, WIDTH_TABLET} from 'context/config'
 //context
 import Api from 'context/api'
 
-//material-ui
-import PropTypes from 'prop-types'
-import {StylesProvider} from '@material-ui/core/styles'
-import AppBar from '@material-ui/core/AppBar'
-import Tabs from '@material-ui/core/Tabs'
-import Tab from '@material-ui/core/Tab'
-import Typography from '@material-ui/core/Typography'
-import Box from '@material-ui/core/Box'
-
 //components
 import MyProfile from './content/myProfile.js'
 import Notice from './content/notice.js'
@@ -35,10 +26,6 @@ import MyWallet from './content/myWallet'
 const User = props => {
   //---------------------------------------------------------------------
   // props.index 값 받았을 시 해당되는 탭을 on 시켜줌, 값 없을 시 기본 0
-  const [value, setValue] = React.useState(props.index ? props.index : 0)
-  const handleChange = (event, newValue) => {
-    setValue(newValue)
-  }
   //---------------------------------------------------------------------
   return (
     <Layout {...props}>
