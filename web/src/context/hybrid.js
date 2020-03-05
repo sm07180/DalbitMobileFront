41ed3ea81738fc12971791636829a4112649c7ad
yeshtml5 <yeshtml5@gmail.com>
2020-03-05 09:45:20 +0900

    하이브리드앱 체크여부확인

--- web/src/context/hybrid.js
+++ web/src/context/hybrid.js
@@ -6,8 +6,8 @@
  * @todo      : context.customHeader.os 를 1,2를 체크해서 복합적으로 구현
  *
  */
-import React, {useEffect, useState} from 'react'
 import {osName, browserName} from 'react-device-detect'
+import Api from 'context/api'
 //---------------------------------------------------------------------
 /**
  *
@@ -15,13 +15,32 @@ import {osName, browserName} from 'react-device-detect'
  * @param any     info          //*data (string or object )
  *
  */
+export const isHybrid = () => {
+  const customHeader = Api.customHeader
+  //하이브리드앱 아닐경우 예외처리
+  if (customHeader.os === '1' && customHeader.os === '2') {
+    const element = document.getElementById('customHeader')
+    if (element !== null && element.value.trim() !== '' && element.value !== undefined) {
+      const _temp = JSON.parse(element.value)
+      alert(_temp)
+    }
+    return true
+  }
+  return false
+}
 export const Hybrid = (func, info) => {
-  //console.log('Native post data = ' + JSON.stringify(info))
+  const customHeader = Api.customHeader
+  //하이브리드앱 아닐경우 예외처리
+  if (customHeader.os !== '1' && customHeader.os !== '2') {
+    console.log(`하이브리드앱이 아님!! osName: ${osName} , browserName: ${browserName}`)
+    return
+  }
   switch (osName) {
     case 'Windows':
       //console.log('Windows버젼입니다')
       break
     case 'iOS':
+      if (webkit === null || webkit === undefined) return
       if (info === '' || info === null || info === undefined) {
         //IOS는 string으로라도 넣어주어야함
         webkit.messageHandlers[func].postMessage('')
@@ -30,6 +49,7 @@ export const Hybrid = (func, info) => {
       }
       break
     case 'Android':
+      if (window.android[func] === null || window.android[func] === undefined) return
       if (info === '' || info === null || info === undefined) {
         window.android[func]()
       } else {
