5b36772b64749cb9c43794011dcaf83e0c0d9e6e
yeshtml5 <yeshtml5@gmail.com>
2020-03-11 08:59:30 +0900

    앱테스트

--- web/src/pages/cast/index.js
+++ web/src/pages/cast/index.js
@@ -11,21 +11,31 @@ import {Context} from 'context'
 import Api from 'context/api'
 import {COLOR_MAIN, COLOR_POINT_Y, COLOR_POINT_P} from 'context/color'
 import {IMG_SERVER, WIDTH_PC, WIDTH_PC_S, WIDTH_TABLET, WIDTH_TABLET_S, WIDTH_MOBILE, WIDTH_MOBILE_S} from 'context/config'
+//components
+import Utility from 'components/lib/utility'
 
 export default props => {
   const context = useContext(Context)
   return (
     <Layout {...props}>
       <Content>
-        <p>
+        <nav>
           <button
             onClick={() => {
               window.location.href = '/app/test'
             }}>
             APP/TEST 이동
           </button>
-        </p>
-
+        </nav>
+        <h1>기능단위테스트</h1>
+        <section>
+          <button
+            onClick={() => {
+              alert(Utility.getCookie('native-info'))
+            }}>
+            세션쿠키테스트
+          </button>
+        </section>
         <h1>CustomHeader</h1>
         <section>{JSON.stringify(context.customHeader, null, 1)}</section>
         <h1>token</h1>
@@ -40,6 +50,9 @@ export default props => {
 const Content = styled.section`
   min-height: 300px;
   background: #e1e1e1;
+  nav {
+    margin-bottom: 40px;
+  }
   button {
     display: inline-block;
     padding: 10px;
@@ -47,8 +60,14 @@ const Content = styled.section`
     background: #000;
     color: #fff;
   }
+  h1 {
+    display: block;
+    font-size: 18px;
+  }
   section {
     padding: 20px;
+    font-size: 16px;
     word-break: break-all;
+    border-bottom: 1px solid #ccc;
   }
 `
