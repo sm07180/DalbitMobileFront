23cbfb46ac89583bbb051bf877b40286148764e2
yeshtml5 <yeshtml5@gmail.com>
2020-03-10 13:15:30 +0900

    _active

--- web/src/App.js
+++ web/src/App.js
@@ -94,10 +94,12 @@ export default () => {
       context.action.updateToken(res.data)
       //JWT토큰동일한지유효성확인
       //세션
+
+      const _active = Utility.getCookie('native-active')
       if (isHybrid === 'Y') {
+        alert(_active)
         alert(JSON.stringify(customHeader, null, 1))
       }
-      const _active = Utility.getCookie('native-active')
       if (_active === '' || _active === null || _active === undefined) {
         Utility.setCookie('native-active', 'Y', null)
         Hybrid('GetLoginToken', res.data)
