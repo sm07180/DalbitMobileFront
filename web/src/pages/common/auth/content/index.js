33e2faae34b7391ff79b5602dda48539ed1af173
Park Songwon <songwon.bak@gmail.com>
2020-03-11 10:48:16 +0900

    login bug fix

--- web/src/pages/common/auth/content/index.js
+++ web/src/pages/common/auth/content/index.js
@@ -137,6 +137,11 @@ export default props => {
         const userInfo = await Api.mypage()
         if (userInfo.result === 'success') {
           context.action.updateMypage(userInfo.data)
+          Api.profile({params: {memNo: res.data.memNo}}).then(profileInfo => {
+            if (profileInfo.result === 'success') {
+              context.action.updateProfile(profileInfo.data)
+            }
+          })
         }
       } else {
         context.action.updatePopupVisible(false)
