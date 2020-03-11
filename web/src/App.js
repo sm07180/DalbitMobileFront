0fbd0325fe26fb9ceed8278121d589e50c91d95b
yeshtml5 <yeshtml5@gmail.com>
2020-03-11 18:53:23 +0900

    2.5

--- web/src/App.js
+++ web/src/App.js
@@ -116,6 +116,23 @@ export default () => {
           if (res.data.authToken !== authToken) Hybrid('GetLoginToken', res.data)
         }
       }
+      //-----##TEST
+      console.log('### version 2.5')
+      /**
+       * @새창에서도 실행되므로 주의요망
+       */
+      if (isHybrid === 'Y') {
+        //최초앱구동실행
+        if (customHeader.isFirst === 'Y') {
+          Utility.setCookie('native-player-info', '', -1)
+        } else if (customHeader.isFirst === 'N') {
+          const _cookie = Utility.getCookie('native-player-info')
+          // alert(typeof _cookie + ' _cookie' + _cookie)
+          if (_cookie !== undefined) {
+            alert('창1')
+          }
+        }
+      }
       //모든처리완료
       setReady(true)
     } else {
@@ -132,24 +149,6 @@ export default () => {
     //#2 authToken 토큰업데이트
     Api.setAuthToken(authToken)
     fetchData({data: _customHeader})
-    //-----##TEST
-    console.log('### version 2.4')
-    /**
-     * @새창에서도 실행되므로 주의요망
-     */
-    if (isHybrid === 'Y') {
-      alert('isHybrid_ ' + isHybrid + ' , isFirst_ ' + customHeader.isFirst)
-      //최초앱구동실행
-      if (customHeader.isFirst === 'Y') {
-        Utility.setCookie('native-player-info', '', -1)
-      } else if (customHeader.isFirst === 'N') {
-        const _cookie = Utility.getCookie('native-player-info')
-        alert(typeof _cookie + ' _cookie' + _cookie)
-        if (_cookie !== undefined) {
-          alert('창1')
-        }
-      }
-    }
   }, [])
   //---------------------------------------------------------------------
   /**
