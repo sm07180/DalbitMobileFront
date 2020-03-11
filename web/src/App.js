bc3524665bc5cef521a7a29b9b5a351b15593638
yeshtml5 <yeshtml5@gmail.com>
2020-03-12 08:33:55 +0900

    1.3

--- web/src/App.js
+++ web/src/App.js
@@ -121,7 +121,7 @@ export default () => {
         }
       }
       //-----##TEST
-      console.log('### version 1.2')
+      console.log('### version 1.3')
 
       //모든처리완료
       setReady(true)
