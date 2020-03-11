d90cd69b20d434159d6441aa52cb592b8887ffeb
yeshtml5 <yeshtml5@gmail.com>
2020-03-11 15:03:50 +0900

    native-player-info

--- web/src/pages/common/player/content/index.js
+++ web/src/pages/common/player/content/index.js
@@ -37,7 +37,7 @@ export default props => {
     if (context.nativePlayer !== null && context.nativePlayer !== undefined) {
       const _val = JSON.stringify(context.nativePlayer)
       setInfo(context.nativePlayer)
-      Utility.setCookie('native-info', _val, 100)
+      Utility.setCookie('native-player-info', _val, 100)
     }
     //
   }, [context.nativePlayer])
