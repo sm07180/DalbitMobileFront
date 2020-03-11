32a626d3e6dbdde1a2acd058984c4d82d66025d1
yeshtml5 <yeshtml5@gmail.com>
2020-03-11 12:40:30 +0900

    업데이트

--- web/src/App.js
+++ web/src/App.js
@@ -141,7 +141,7 @@ export default () => {
     fetchData({data: _customHeader})
     //-----##TEST
     if (isHybrid === 'Y') {
-      alert(nativeInfo)
+      //alert(nativeInfo)
     }
   }, [])
   //---------------------------------------------------------------------
