a3e1d9f21f52d436fd005834c5883d2aebc9126c
yeshtml5 <yeshtml5@gmail.com>
2020-03-12 08:18:42 +0900

    업데이트

--- web/src/App.js
+++ web/src/App.js
@@ -121,7 +121,7 @@ export default () => {
         }
       }
       //-----##TEST
-      console.log('### version 3.2')
+      console.log('### version 1.0')
 
       //모든처리완료
       setReady(true)
