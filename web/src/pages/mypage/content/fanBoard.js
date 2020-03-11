4bc0ac8abfc09d7b532c088d842f6816c8b90ab0
Park Songwon <songwon.bak@gmail.com>
2020-03-11 17:17:11 +0900

    notice in mypage working...

--- web/src/pages/mypage/content/fanBoard.js
+++ web/src/pages/mypage/content/fanBoard.js
@@ -19,24 +19,48 @@ export default props => {
   const placeholderText = '팬 보드에 글을 남겨주세요. 타인에게 불쾌감을 주는 욕설 또는 비하글은 이용약관 및 관련 법률에 의해 제재를 받을 수 있습니다.'
 
   return (
-    <FanBoard>
+    <FanBoard className="fanboard">
       <WriteArea>
         <Textarea placeholder={placeholderText} />
+        <WriteAreaBottom>
+          <TextCount>10 / 200</TextCount>
+          <CommentSubmitBtn>등록</CommentSubmitBtn>
+        </WriteAreaBottom>
       </WriteArea>
       <ListArea></ListArea>
     </FanBoard>
   )
 }
 
-const FanBoard = styled.div``
+const ListArea = styled.div`
+  margin: 30px 0;
+`
 
-const WriteArea = styled.form`
+const CommentSubmitBtn = styled.button`
+  width: 92px;
+  text-align: center;
+  font-size: 16px;
+  color: #fff;
+  padding: 16px 0;
+  background-color: #8556f6;
+`
+
+const TextCount = styled.div`
+  width: calc(100% - 92px);
+  font-size: 14px;
+  padding: 17px 20px;
+  height: 50px;
+  box-sizing: border-box;
+  border-top: 1px solid #d0d0d0;
+`
+
+const WriteAreaBottom = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  border: 1px solid #d0d0d0;
+  align-items: center;
+  width: 100%;
 `
-
 const Textarea = styled.textarea`
   display: block;
   width: 100%;
@@ -45,15 +69,21 @@ const Textarea = styled.textarea`
   resize: none;
   font-family: inherit;
   box-sizing: border-box;
-  padding: 0 20px;
+  padding: 18px 20px;
 
   &::placeholder {
     color: #bdbdbd;
     font-size: 14px;
     letter-spacing: -0.35px;
   }
+
+  &:focus {
+    outline: none;
+  }
 `
 
-const ListArea = styled.div`
-  margin: 30px 0;
+const WriteArea = styled.div`
+  border: 1px solid #d0d0d0;
 `
+
+const FanBoard = styled.div``
