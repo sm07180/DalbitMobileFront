455604313357c9700e797f8363c80b04077607f3
yeshtml5 <yeshtml5@gmail.com>
2020-03-13 08:43:06 +0900

    1.4qovh

--- web/src/Interface.js
+++ web/src/Interface.js
@@ -33,7 +33,7 @@ export default () => {
       */
         let _ios = {
           roomNo: event.detail.roomNo,
-          //   bjProfImg: event.detail.bjProfImg.thumb150x150,
+          bjProfImg: event.detail.bjProfImg.thumb150x150,
           title: event.detail.title,
           bjNickNm: event.detail.roomNo
         }
