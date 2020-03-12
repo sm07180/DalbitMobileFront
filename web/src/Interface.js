6e051a6322dfe2fb5ddb4364d5cd525545c1670f
yeshtml5 <yeshtml5@gmail.com>
2020-03-12 14:39:27 +0900

    test1

--- web/src/Interface.js
+++ web/src/Interface.js
@@ -30,10 +30,7 @@ export default () => {
         _ios.roomNo = event.detail.roomNo
         _ios.bjProfImg = event.detail.bjProfImg.thumb150x150
         _ios.title = event.detail.title
-
         _ios = JSON.stringify(_ios)
-
-        alert(_ios)
         Utility.setCookie('native-player-info', _ios, 100)
         context.action.updateMediaPlayerStatus(true)
         context.action.updateNativePlayer(JSON.parse(_ios))
