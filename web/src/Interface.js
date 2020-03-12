24aff334301c07b81a5efe979ab4f59d9d6aec16
yeshtml5 <yeshtml5@gmail.com>
2020-03-12 19:03:11 +0900

    업데이트 배포

--- web/src/Interface.js
+++ web/src/Interface.js
@@ -26,10 +26,10 @@ export default () => {
         break
       case 'native-player-show': //---------------------Native player-show (IOS)
         let _ios = {
-          roomNo: event.detail.roomNo + '',
-          bjProfImg: event.detail.bjProfImg.thumb150x150 + '',
-          title: event.detail.title + '',
-          bjNickNm: event.detail.bjNickNm + ''
+          roomNo: encodeURIComponent(event.detail.roomNo),
+          bjProfImg: encodeURIComponent(event.detail.bjProfImg.thumb150x150),
+          title: encodeURIComponent(event.detail.title),
+          bjNickNm: encodeURIComponent(event.detail.bjNickNm)
         }
         _ios = JSON.stringify(_ios)
         Utility.setCookie('native-player-info', _ios, 100)
