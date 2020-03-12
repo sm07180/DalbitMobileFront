ab4b534a6b560fe12d0b7c42ac979ac32f5aab47
yeshtml5 <yeshtml5@gmail.com>
2020-03-12 19:49:22 +0900

    업데이트

--- web/src/Interface.js
+++ web/src/Interface.js
@@ -31,8 +31,8 @@ export default () => {
           title: event.detail.title,
           bjNickNm: event.detail.bjNickNm
         }
+        alert(JSON.stringify(_ios, null, 1))
         _ios = JSON.stringify(_ios)
-        alert(_ios)
         Utility.setCookie('native-player-info', _ios, 100)
         context.action.updateMediaPlayerStatus(true)
         context.action.updateNativePlayer(JSON.parse(_ios))
