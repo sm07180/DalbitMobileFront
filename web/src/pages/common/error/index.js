666660aec94359f27cd8de0f42fbf15524186732
황상한 <nmc2711@inforex.co.kr>
2020-03-12 09:09:06 +0900

    신고하기푸쉬

--- web/src/pages/common/error/index.js
+++ web/src/pages/common/error/index.js
@@ -2,6 +2,7 @@
  * @title 404페이지
  */
 import React, {useEffect} from 'react'
+import './style.css'
 import styled from 'styled-components'
 //pages
 
@@ -12,11 +13,17 @@ export default () => {
     <>
       <section>
         <ul>
-          <li>로고이미지</li>
-          <li>에러타이틀</li>
-          <li>에러내용</li>
-          <li>버튼</li>
-          <li>하단이미지</li>
+          <li class="logoImg">로고이미지</li>
+          <li class="errorTitle">에러 ERROR</li>
+          <li class="details">
+            죄송합니다.
+            <br /> 서비스 이용에 불편을 드려 죄송합니다.
+            <br /> 잠시 후 다시 시도해 주시기 바랍니다.
+          </li>
+          <li class="btn">
+            <a href="#" onclick=>확인</a>
+          </li>
+          <li class="bottomImg">하단이미지</li>
         </ul>
       </section>
     </>
