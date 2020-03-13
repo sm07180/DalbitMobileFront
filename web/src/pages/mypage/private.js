bea9e5d1019d18e98b974fcfdfb0c637cbeea90b
Park Songwon <songwon.bak@gmail.com>
2020-03-13 15:53:03 +0900

    private page styling

--- web/src/pages/mypage/private.js
+++ web/src/pages/mypage/private.js
@@ -22,13 +22,28 @@ export default props => {
   }, [])
 
   if (profile) {
-    console.log(profile)
     return (
       <PrivatePage>
-        <img src={profile.profImg['thumb80x80']} />
-        <div>{profile.nickNm}</div>
-        <div>fan{profile.fanCnt}</div>
-        <div>star{profile.starCnt}</div>
+        <ProfileImg style={{backgroundImage: `url(${profile.profImg['thumb120x120']})`}}></ProfileImg>
+        <NickName>{profile.nickNm}</NickName>
+        <MemberId>@{profile.memId}</MemberId>
+        <FanAndStarWrap>
+          <span>팬</span>
+          <span style={{color: '#8556f6'}}>{profile.fanCnt}</span>
+          <span style={{margin: '0 10px'}}>|</span>
+          <span>스타</span>
+          <span style={{color: '#8556f6'}}>{profile.starCnt}</span>
+          <span style={{margin: '0 10px'}}>|</span>
+        </FanAndStarWrap>
+        <MsgWrap>
+          {profile.profMsg.split('\n').map((lineText, index) => {
+            return (
+              <div className="line-text" key={index}>
+                {lineText}
+              </div>
+            )
+          })}
+        </MsgWrap>
       </PrivatePage>
     )
   } else {
@@ -36,7 +51,54 @@ export default props => {
   }
 }
 
+const MsgWrap = styled.div`
+  display: block;
+  margin-top: 15px;
+  color: #616161;
+  letter-spacing: -0.35px;
+  font-size: 14px;
+
+  .line-text {
+    text-align: center;
+  }
+`
+
+const FanAndStarWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  color: #e0e0e0;
+  font-size: 14px;
+  justify-content: center;
+  align-items: center;
+  margin-top: 13px;
+`
+
+const MemberId = styled.div`
+  color: #bdbdbd;
+  font-size: 14px;
+  letter-spacing: -0.35px;
+  text-align: center;
+`
+
+const NickName = styled.div`
+  color: #424242;
+  font-size: 24px;
+  letter-spacing: -0.6px;
+  text-align: center;
+  margin-top: 20px;
+`
+
+const ProfileImg = styled.div`
+  width: 100px;
+  height: 100px;
+  margin: 0 auto;
+  border-radius: 50%;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+`
+
 const PrivatePage = styled.div`
-  width: 400px;
+  max-width: 360px;
   margin: 0 auto;
 `
