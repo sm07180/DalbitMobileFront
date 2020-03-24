6ff7df4dd0b2c1ee991c7c77c8166ac1f2ecdc6b
Park Songwon <songwon.bak@gmail.com>
2020-03-24 16:50:30 +0900

    make common ui select box

--- web/src/components/ui/selectBox.js
+++ web/src/components/ui/selectBox.js
@@ -2,5 +2,41 @@ import React from 'react'
 import styled from 'styled-components'
 
 export default props => {
-  return <div></div>
+  const {boxList, onChangeEvent} = props
+
+  const selectBoxList = value => {
+    onChangeEvent(value)
+  }
+
+  if (boxList === undefined) {
+    throw new Error('Need a box list in select box')
+  }
+
+  return (
+    <SelectBox>
+      {boxList.map((value, index) => {
+        return (
+          <div className="box-list" onClick={() => selectBoxList(value)}>
+            {value}
+          </div>
+        )
+      })}
+    </SelectBox>
+  )
 }
+
+const SelectBox = styled.div`
+  position: absolute;
+  width: 136px;
+  padding: 11px;
+  box-sizing: border-box;
+
+  .box-list {
+    color: #757575;
+
+    &:hover {
+      color: #8556f6;
+      background-color: #f8f8f8;
+    }
+  }
+`
