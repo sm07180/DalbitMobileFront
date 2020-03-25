5968d230c3e71d6ae8491ecf32fcfc0f3e228c1d
yeshtml5 <yeshtml5@gmail.com>
2020-03-25 12:49:30 +0900

    debug({title:'타이틀내용',msg:'메시지내용'}) 처리

--- web/public/static/js/reactSampleCode.js
+++ web/public/static/js/reactSampleCode.js
@@ -2,6 +2,25 @@
  * App -> React sampleCode
  * @params "detail" 속성으로 전달
  */
+function debug(obj) {
+  document.dispatchEvent(
+    new CustomEvent('react-debug', {
+      detail: obj
+    })
+
+    /*
+    example) debug({title:'111',msg:'ssss'})
+
+    document.dispatchEvent(
+      new CustomEvent('react-debug', {
+        detail: {
+          title: '타이틀',
+          message: '메시지 내용 에러메시지내용 '
+        }
+      })
+    */
+  )
+}
 function native() {
   // document.dispatchEvent(
   //   new CustomEvent('native-goLogin', {
