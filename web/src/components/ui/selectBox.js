407d5971038ec6270bd85ec1eecb33e18ef2924c
Park Songwon <songwon.bak@gmail.com>
2020-03-26 09:15:18 +0900

    minor fix

--- web/src/components/ui/selectBox.js
+++ web/src/components/ui/selectBox.js
@@ -14,7 +14,7 @@ export default props => {
 
   const selectBoxList = (value, idx) => {
     onChangeEvent(value)
-    setSelectedIdx(value)
+    setSelectedIdx(idx)
   }
 
   const selectBlurEvent = () => {
