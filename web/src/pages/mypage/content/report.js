de0d67acd65a7a7f4db19374854bcc72f25012e2
Park Songwon <songwon.bak@gmail.com>
2020-03-27 17:20:31 +0900

    add report in mypage

--- web/src/pages/mypage/content/report.js
+++ web/src/pages/mypage/content/report.js
@@ -7,7 +7,7 @@ import React, {useEffect, useStet, useContext} from 'react'
 import styled from 'styled-components'
 
 //layout
-import {WIDTH_PC, WIDTH_TABLET} from 'context/config'
+import {IMG_SERVER, WIDTH_MOBILE} from 'context/config'
 
 // context
 import {Context} from 'context'
@@ -27,22 +27,24 @@ const iconInfo = {
 
 export default props => {
   const ctx = useContext(Context)
+  const detialTabList = ['방송일자', '방송시작', '청취종료', '받은별', '좋아요', '최다 청취자', '방송 최고순위']
+
   return (
     <Report>
-      <TitleWrap style={{paddingBottom: '16px'}}>
+      {/* <TitleWrap style={{paddingBottom: '16px'}}>
         <TitleText>리포트</TitleText>
         <div>
           <TypeBtn style={{marginRight: '5px'}}>방송</TypeBtn>
           <TypeBtn style={{marginLeft: '5px'}}>청취</TypeBtn>
         </div>
-      </TitleWrap>
+      </TitleWrap> */}
 
-      <TitleWrap style={{alignItems: 'flex-end'}}>
+      {/* <TitleWrap style={{alignItems: 'flex-end'}}>
         <TitleText>방송요약</TitleText>
         <TitleSubMsg>데이터는 최대 6개월까지 검색 가능합니다.</TitleSubMsg>
-      </TitleWrap>
+      </TitleWrap> */}
 
-      <BroadcastShort>
+      {/* <BroadcastShort>
         {Object.keys(iconInfo).map((section, index) => {
           return (
             <ShortSection key={index}>
@@ -54,7 +56,7 @@ export default props => {
             </ShortSection>
           )
         })}
-      </BroadcastShort>
+      </BroadcastShort> */}
 
       <TitleWrap>
         <TitleText>상세내역</TitleText>
@@ -62,14 +64,68 @@ export default props => {
 
       <DetailTable>
         <DetailTableNav>
-          {['방송일자', '방송시작', '청취종료', '받은별', '좋아요', '최다 청취자', '방송 최고순위'].map((text, index) => {
+          {detialTabList.map((text, index) => {
             return <DetailTableTab key={index}>{text}</DetailTableTab>
           })}
         </DetailTableNav>
+
+        <div>
+          {[
+            {
+              date: '2020-02-14',
+              start: '01:12:23',
+              end: '02:12:23',
+              byeol: 123123,
+              like: 1555,
+              maxListener: 94405,
+              rank: 15
+            },
+            {
+              date: '2020-03-14',
+              start: '01:12:23',
+              end: '02:12:23',
+              byeol: 123123,
+              like: 1555,
+              maxListener: 94405,
+              rank: 15
+            },
+            {
+              date: '2020-03-22',
+              start: '01:12:23',
+              end: '02:12:23',
+              byeol: 123123,
+              like: 1555,
+              maxListener: 94405,
+              rank: 15
+            }
+          ].map((value, idx) => {
+            return (
+              <MobileDetailTab key={idx}>
+                <div>{`방송일자 ${value.date}`}</div>
+                <div>{`방송시작 ${value.start}`}</div>
+                <div>{`방송종료 ${value.end}`}</div>
+                <div>{`받은별 ${value.byeol}`}</div>
+                <div>{`좋아요 ${value.like}`}</div>
+                <div>{`최다 청취자 ${value.maxListener}`}</div>
+                <div>{`방송 최고 순위 ${value.rank}`}</div>
+              </MobileDetailTab>
+            )
+          })}
+        </div>
       </DetailTable>
     </Report>
   )
 }
+
+const MobileDetailTab = styled.div`
+  padding: 16px 10px;
+  border-bottom: 1px solid #e0e0e0;
+  div {
+    color: #424242;
+    font-size: 14px;
+    margin: 8px 0;
+  }
+`
 
 const DetailTableTab = styled.div`
   width: 14.2857%;
@@ -83,6 +139,10 @@ const DetailTableNav = styled.div`
   display: flex;
   flex-direction: row;
   width: 100%;
+
+  @media (max-width: ${WIDTH_MOBILE}) {
+    display: none;
+  }
 `
 const DetailTable = styled.div``
 
