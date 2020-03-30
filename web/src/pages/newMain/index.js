6124e49d5c25ca4072758127bfa7f98ddab031cc
Park Songwon <songwon.bak@gmail.com>
2020-03-30 09:50:07 +0900

    add new main

---
+++ web/src/pages/newMain/index.js
@@ -0,0 +1,10 @@
+/**
+ * @file main.js
+ * @brief 메인페이지
+ */
+import React, {useContext, useEffect} from 'react'
+
+export default props => {
+  useEffect(() => {}, [])
+  return <div>new main</div>
+}
