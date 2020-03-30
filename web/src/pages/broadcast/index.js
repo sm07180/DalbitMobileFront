201e1cda0701e5f1302e3d6be4ee5fc71f41e939
kimhogyeom <hgkim1118@inforex.co.kr>
2020-03-30 13:42:24 +0900

    방송방 로직 수정

--- web/src/pages/broadcast/index.js
+++ web/src/pages/broadcast/index.js
@@ -29,12 +29,12 @@ export default props => {
     console.log('join')
     const resRoomJoin = await Api.broad_join(obj) //방입장
     const {code, result, data, message} = resRoomJoin
-
+    //alert('roomNo = ' + roomNo)
     if (result === 'success') {
       console.log('방입장  = ' + data)
       if (data) {
         if (isHybrid()) {
-          //alert('hybrid')
+          alert('hybrid')
 
           Hybrid('RoomJoin', data)
           setReadyRoom(false)
@@ -43,7 +43,7 @@ export default props => {
             props.history.goBack()
           }
         } else {
-          //alert('no hybrid')
+          alert('no hybrid')
           ctx.action.updateBroadcastTotalInfo(data)
           localStorage.setItem('currentRoomNo', roomNo)
           setReadyRoom(true)
@@ -53,14 +53,26 @@ export default props => {
       //참여 성공(0) ,회원 아닐시(-1),해당방 미존재(-2),종료된 방송(-3),이미 참여(-4),입장 제한(-5),나이 제한(-6)
       if (code !== '-1') {
         if (code === '-4') {
-          //setReadyRoom(true)
+          alert('code = ' + code)
+          if (isHybrid()) {
+            //alert('EnterRoom')
+            setReadyRoom(false)
+            Hybrid('EnterRoom', '')
+            //   //props.history.goBack()
+          } else {
+            //   alert('asdasdasdasd')
+            //   context.action.updateReloadType(3)
+            setReadyRoom(true)
+            props.history.push('/broadcast/' + '?roomNo=' + roomNo)
+          }
+        } else {
+          ctx.action.alert({
+            callback: () => {
+              props.history.goBack()
+            },
+            msg: message
+          })
         }
-        ctx.action.alert({
-          callback: () => {
-            props.history.goBack()
-          },
-          msg: message
-        })
       }
     }
   }
