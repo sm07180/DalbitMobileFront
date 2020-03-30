c4fe82c1192b2ffe474e5f63ac81a9aa205b4011
leeeunbee <herbione@inforex.co.kr>
2020-03-30 16:35:35 +0900

    결제 반응형 및 로직 수정

--- web/src/pages/broadcast/content/tab/charge-success-popup.js
+++ web/src/pages/broadcast/content/tab/charge-success-popup.js
@@ -3,41 +3,63 @@ import styled from 'styled-components'
 import {LButton} from './bot-button'
 import {BroadCastStore} from '../../store'
 import {Context} from 'context'
+import {COLOR_MAIN, COLOR_POINT_Y, COLOR_POINT_P} from 'context/color'
+import {IMG_SERVER, WIDTH_TABLET_S, WIDTH_PC_S, WIDTH_TABLET, WIDTH_MOBILE, WIDTH_MOBILE_S} from 'context/config'
 
 // props 로 가져온 데이터들 바인딩 필요함
 export default props => {
   //---------------------------------------------------------------- declare start
   const context = useContext(Context)
+  const {type, itemName, price, date, cardName, cardNum, phone} = props.detail
   //---------------------------------------------------------------- func start
   //---------------------------------------------------------------- components start
   return (
     <Container>
       <div>
-        <div className="title">달 충전 결제 대기중</div>
+        <div className="title">결제완료</div>
+
         <Wrap>
-          <div className="subTitle">결제대기중</div>
-          <ChargeHistory>
-            <div className="subTitle">
-              <span>입금하실 금액</span>
-              <div>11,000(부가세 포함)</div>
-            </div>
-            <div className="subTitle">
-              <span>예금주</span>
-              <div>(주)인포렉스</div>
-            </div>
-            <div className="subTitle">
-              <span>입금은행</span>
-              <div>KB 국민은행</div>
-            </div>
-            <div className="subTitle">
-              <span>입금계좌</span>
-              <div>197790-71-606640</div>
-            </div>
-            <div className="subTitle">
-              <span>입금자</span>
-              <div>손흥민</div>
-            </div>
-          </ChargeHistory>
+          <div className="subTitle">결제내역</div>
+          {props.detail && (
+            <ChargeHistory>
+              <div className="subTitle">
+                <span>가맹점</span>
+                <div>달빛라이브</div>
+              </div>
+              <div className="subTitle">
+                <span>상품명</span>
+                <div>{itemName}</div>
+              </div>
+              <div className="subTitle">
+                <span>결제 금액</span>
+                <div>{price}</div>
+              </div>
+              <div className="subTitle">
+                <span>결제 일시</span>
+                <div>{date}</div>
+              </div>
+              {type == 'pay_card' && (
+                <>
+                  <div className="subTitle">
+                    <span>카드명</span>
+                    <div>{cardName}</div>
+                  </div>
+                  <div className="subTitle">
+                    <span>카드번호</span>
+                    <div>{cardNum}</div>
+                  </div>
+                </>
+              )}
+              {type == 'pay_phone' && (
+                <>
+                  <div className="subTitle">
+                    <span>주문자 휴대번호</span>
+                    <div>{phone}</div>
+                  </div>
+                </>
+              )}
+            </ChargeHistory>
+          )}
         </Wrap>
       </div>
       <ButtonArea>
@@ -58,25 +80,29 @@ const Container = styled.div`
   & > div {
     display: flex;
     flex-direction: column;
-    width: 90%;
+    width: 100%;
   }
 `
 const Wrap = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100%;
+  width: 90%;
+  margin: 0 auto;
   margin-top: 33px;
+
+  @media (max-width: ${WIDTH_MOBILE}) {
+    margin-top: 76px;
+  }
 `
 
 const ChargeHistory = styled.div`
   .subTitle {
-    border-bottom-color: #e0e0e0;
+    border-bottom-color: #e0e0e0 !important;
 
     & > span {
       color: #616161;
       font-size: 14px;
       font-weight: 400;
-      line-height: 1.43;
       letter-spacing: -0.35px;
     }
 
@@ -84,15 +110,21 @@ const ChargeHistory = styled.div`
       color: #424242;
       font-size: 14px;
       font-weight: 600;
-      line-height: 1.43;
       letter-spacing: -0.35px;
     }
   }
 `
 const ButtonArea = styled.div`
   display: flex;
+
   align-items: flex-end;
-  width: 100%;
+  width: 90% !important;
   height: 48px;
   margin-bottom: 12px;
+  margin-top: 20px;
+
+  @media (max-width: ${WIDTH_MOBILE}) {
+    position: fixed;
+    bottom: 0;
+  }
 `
