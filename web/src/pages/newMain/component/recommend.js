6f59a2a0a0084c91ceea33a9bd91c1b5a79b8749
Park Songwon <songwon.bak@gmail.com>
2020-03-31 17:21:17 +0900

    styling in new main

--- web/src/pages/newMain/component/recommend.js
+++ web/src/pages/newMain/component/recommend.js
@@ -9,12 +9,14 @@ import Swiper from 'react-id-swiper'
 export default props => {
   const {list} = props
 
-  console.log('r list', list)
   return (
     <RecommendWrap>
       <div className="selected-wrap">
         <img className="live-icon" src={LiveIcon} />
-        <div className="counting">{list ? `1 / ${list.length}` : ''}</div>
+        <div className="counting">
+          <span className="bold">{list ? `1` : ''}</span>
+          <span>{list ? `/ ${list.length}` : ''}</span>
+        </div>
       </div>
     </RecommendWrap>
   )
@@ -46,6 +48,12 @@ const RecommendWrap = styled.div`
       border-radius: 10px;
       background-color: rgba(0, 0, 0, 0.5);
       font-size: 10px;
+      color: rgba(255, 255, 255, 0.5);
+
+      .bold {
+        color: #fff;
+        margin-right: 4px;
+      }
     }
   }
 `
