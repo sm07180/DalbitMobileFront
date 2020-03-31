3c792933fe8f3c97f6f069b2e6cc58fe511e662c
Park Songwon <songwon.bak@gmail.com>
2020-03-31 17:10:23 +0900

    add styling in new main

--- web/src/pages/newMain/component/recommend.js
+++ web/src/pages/newMain/component/recommend.js
@@ -7,10 +7,14 @@ import LiveIcon from '../static/ic_live.svg'
 import Swiper from 'react-id-swiper'
 
 export default props => {
+  const {list} = props
+
+  console.log('r list', list)
   return (
     <RecommendWrap>
       <div className="selected-wrap">
-        <img src={LiveIcon} />
+        <img className="live-icon" src={LiveIcon} />
+        <div className="counting">{list ? `1 / ${list.length}` : ''}</div>
       </div>
     </RecommendWrap>
   )
@@ -23,5 +27,25 @@ const RecommendWrap = styled.div`
     border-radius: 10px;
     margin: 0 16px;
     background-color: #eee;
+
+    .live-icon {
+      position: absolute;
+      top: 6px;
+      left: 6px;
+      width: 51px;
+    }
+    .counting {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      position: absolute;
+      width: 46px;
+      height: 16px;
+      right: 6px;
+      bottom: 6px;
+      border-radius: 10px;
+      background-color: rgba(0, 0, 0, 0.5);
+      font-size: 10px;
+    }
   }
 `
