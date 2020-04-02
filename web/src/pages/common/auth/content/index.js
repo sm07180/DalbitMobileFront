5eff5316dbea5d3c94fa0000150ae172b85ac5a8
Park Songwon <songwon.bak@gmail.com>
2020-04-02 14:57:22 +0900

    remove unused code in login.js

--- web/src/pages/common/auth/content/index.js
+++ web/src/pages/common/auth/content/index.js
@@ -157,32 +157,6 @@ export default props => {
           title: res.messageKey !== undefined && res.messageKey,
           msg: res.message !== undefined && res.message
         })
-        // context.action.updatePopupVisible(false)
-        // context.action.updateLogin(false)
-        // let result = confirm(res.message)
-        // if (props.history) {
-        //   switch (ostype) {
-        //     case 'g':
-        //       break
-        //     default:
-        //   }
-        //   if (result) {
-        //     props.history.push('/user/join', loginInfo)
-        //   } else {
-        //     if (ostype === 'n') {
-        //       localStorage.removeItem('com.naver.nid.access_token')
-        //       localStorage.removeItem('com.naver.nid.oauth.state_token')
-
-        //       // Utility.removeCookie('NID_AUT', '', -1)
-        //       // Utility.removeCookie('NID_JKL', '', -1)
-        //       // Utility.removeCookie('NID_SES', '', -1)
-        //       // Utility.removeCookie('NNB', '', -1)
-        //     }
-
-        //     //props.history.push('/')
-        //     //alert('회원가입 실패 메인이동')
-        //   }
-        // }
       }
       //alert(res.message)
     } else {
@@ -202,90 +176,7 @@ export default props => {
         break
     }
   }
-  // Facebook에서 성별,생일 권한은 다시 권한 요청이 있어 버린다.
-  const responseFacebook = response => {
-    // status는 앱 사용자의 로그인 상태를 지정합니다. 상태는 다음 중 하나일 수 있습니다.
-    // connected - 사용자가 Facebook에 로그인하고 앱에 로그인했습니다.
-    // not_authorized - 사용자가 Facebook에는 로그인했지만 앱에는 로그인하지 않았습니다.
-    // unknown - 사용자가 Facebook에 로그인하지 않았으므로 사용자가 앱에 로그인했거나 FB.logout()이 호출되었는지 알 수 없어, Facebook에 연결할 수 없습니다.
-    // connected 상태인 경우 authResponse가 포함되며 다음과 같이 구성되어 있습니다.
-    // accessToken - 앱 사용자의 액세스 토큰이 포함되어 있습니다.
-    // expiresIn - 토큰이 만료되어 갱신해야 하는 UNIX 시간을 표시합니다.
-    // signedRequest - 앱 사용자에 대한 정보를 포함하는 서명된 매개변수입니다.
-    // userID - 앱 사용자의 ID입니다.
-    //fetchData(response, 'f')
-  }
-  const responseFacebookCallback = response => {
-    // status는 앱 사용자의 로그인 상태를 지정합니다. 상태는 다음 중 하나일 수 있습니다.
-    // connected - 사용자가 Facebook에 로그인하고 앱에 로그인했습니다.
-    // not_authorized - 사용자가 Facebook에는 로그인했지만 앱에는 로그인하지 않았습니다.
-    // unknown - 사용자가 Facebook에 로그인하지 않았으므로 사용자가 앱에 로그인했거나 FB.logout()이 호출되었는지 알 수 없어, Facebook에 연결할 수 없습니다.
-    // connected 상태인 경우 authResponse가 포함되며 다음과 같이 구성되어 있습니다.
-    // accessToken - 앱 사용자의 액세스 토큰이 포함되어 있습니다.
-    // expiresIn - 토큰이 만료되어 갱신해야 하는 UNIX 시간을 표시합니다.
-    // signedRequest - 앱 사용자에 대한 정보를 포함하는 서명된 매개변수입니다.
-    // userID - 앱 사용자의 ID입니다.
-    if (response && response.id) {
-      fetchData(response, 'f')
-    }
-    console.log('responseFacebookCallback')
 
-    //fetchData(response, 'f')
-  }
-  const responseNaver = response => {
-    console.log('32123321312321321')()
-    console.log('responseNaver')
-
-    fetchData(response, 'n')
-  }
-  const responseKakao = response => {
-    console.log(response)
-    fetchData(response, 'k')
-  }
-  const responseGoogle = response => {
-    //console.log(response)
-    fetchData(response, 'g')
-  }
-  const responseGoogleFail = err => {
-    //console.error(err)
-  }
-  function responseGooglelogin() {
-    // signInWithGoogle().then(function(result) {
-    //   //console.log(result)
-    //   // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-    //   var token = result.credential.accessToken
-    //   // The signed-in user info.
-    //   var user = result.user
-    //   // console.log('GoogleToken = ' + token)
-    //   // console.log('user = ' + user)
-    //   // auth.onAuthStateChanged(function(user) {
-    //   //   if (user) {
-    //   //     // User is signed in.
-    //   //     console.log('user2= ' + user)
-    //   //   }
-    //   // })
-    //   fetchData(result, 'g')
-    //   //alert(JSON.stringify(result, null, 1))
-    //   // SetloginStatus(true)
-    // }),
-    //   function(error) {
-    //     // The provider's account email, can be used in case of
-    //     // auth/account-exists-with-different-credential to fetch the providers
-    //     // linked to the email:
-    //     var email = error.email
-    //     // The provider's credential:
-    //     var credential = error.credential
-    //     // In case of auth/account-exists-with-different-credential error,
-    //     // you can fetch the providers using this:
-    //     if (error.code === 'auth/account-exists-with-different-credential') {
-    //       auth.fetchSignInMethodsForEmail(email).then(function(providers) {
-    //         // The returned 'providers' is a list of the available providers
-    //         // linked to the email address. Please refer to the guide for a more
-    //         // complete explanation on how to recover from this error.
-    //       })
-    //     }
-    //   }
-  }
   const pwdEnterkeyHandle = e => {
     if (e.keyCode == 13) {
       fetchData({...changes}, 'p')
@@ -301,53 +192,7 @@ export default props => {
       console.log(cookie)
     }
   }, [])
-  useEffect(() => {
-    // var naverLogin = new naver.LoginWithNaverId({
-    //   clientId: 'WK0ohRsfYc9aBhZkyApJ',
-    //   callbackUrl: 'https://devm-hgkim1118.dalbitcast.com/login',
-    //   isPopup: false,
-    //   loginButton: {color: 'green', type: 3, height: 60} /* 로그인 버튼의 타입을 지정 */,
-    //   callbackHandle: false
-    // })
-    // naverLogin.init()
-    // naverLogin.getLoginStatus(function(status) {
-    //   if (status) {
-    //     // var email = naverLogin.user.getEmail()
-    //     // var name = naverLogin.user.getNickName()
-    //     // var profileImage = naverLogin.user.getProfileImage()
-    //     // var birthday = naverLogin.user.getBirthday()
-    //     // var uniqId = naverLogin.user.getId()
-    //     // var age = naverLogin.user.getAge()
-    //     if (!context.token.isLogin) {
-    //       fetchData(naverLogin.user, 'n')
-    //     }
-    //   } else {
-    //     console.log('AccessToken이 올바르지 않습니다.')
-    //   }
-    // })
-  }, [])
-
-  // const loadData = () => {
-  //   var el = document.getElementById('naverIdLogin')
-  //   el.addEventListener('load', function() {
-  //     console.warn('asdasdasd')
-  //     naverLogin.getLoginStatus(function(status) {
-  //       if (status) {
-  //         /* (5) 필수적으로 받아야하는 프로필 정보가 있다면 callback처리 시점에 체크 */
-  //         var email = naverLogin.user.getEmail()
-  //         if (email == undefined || email == null) {
-  //           alert('이메일은 필수정보입니다. 정보제공을 동의해주세요.')
-  //           /* (5-1) 사용자 정보 재동의를 위하여 다시 네아로 동의페이지로 이동함 */
-  //           naverLogin.reprompt()
-  //           return
-  //         }
-  //         window.location.replace('http://' + window.location.hostname + (location.port == '' || location.port == undefined ? '' : ':' + location.port) + '/sample/main.html')
-  //       } else {
-  //         console.log('callback 처리에 실패하였습니다.')
-  //       }
-  //     })
-  //   })
-  // }
+  useEffect(() => {}, [])
 
   const pwdValidateHandle = e => {
     let value = e.target.value.toLowerCase()
@@ -398,15 +243,6 @@ export default props => {
         로그인
       </LoginSubmit>
       <ButtonArea>
-        {/* <input
-          type="checkbox"
-          id="keeplogin"
-          defaultChecked={saveLogin}
-          onClick={() => {
-            props.update({saveLogin: event.target.checked, changes: changes})
-          }}
-        /> */}
-        {/* <label htmlFor="keeplogin">로그인 유지</label> */}
         <div>
           <button
             onClick={() => {
@@ -424,66 +260,6 @@ export default props => {
           </button>
         </div>
       </ButtonArea>
-      {/* 현재 소셜로그인쪽 display:none; */}
-      {/* <SocialLogin>
-        <FacebookLogin
-          appId="2711342585755275"
-          autoLoad={false} //실행과 동시에 자동으로 로그인 팝업창이 뜸
-          fields="name,email,picture" //어떤 정보를 받아올지 입력하는 필드
-          scope="public_profile,email"
-          onClick={responseFacebook}
-          callback={responseFacebookCallback}
-
-          // cssClass="my-facebook-button-class"
-          // icon="fa-facebook"
-        />
-        <KakaoLogin
-          jsKey="275312c2c2715a7d00c6f0d6c1730353"
-          onSuccess={result => responseKakao(result)}
-          render={props => <img src="//mud-kage.kakao.com/14/dn/btqbjxsO6vP/KPiGpdnsubSq3a0PHEGUK1/o.jpg" width="300" height="66" onClick={props.onClick} />}
-          onFailure={responseKakao}
-          useDefaultStyle={true}
-          getProfile={true}
-        />
-        {/* <NaverLogin
-          clientId="WK0ohRsfYc9aBhZkyApJ"
-          render={props => <div onClick={props.onClick}>Naver Login</div>}
-          onSuccess={() => {
-            console.log('assssss')
-          }}
-          onFailure={() => {
-            console.log('asdasdasd')
-          }}
-          callbackUrl="https://devm-hgkim1118.dalbitcast.com"
-        /> */}
-      {/* <div id="naverIdLogin" /> */}
-      {/* </SocialLogin> */}
-      {/* <CustomButton onClick={signInWithGoogle}>SIGN IN WITH GOOGLE</CustomButton> */}
-      {/* <SnsGoogleLogion onClick={() => responseGooglelogin()}>
-        SIGN IN WITH GOOGLE
-      </SnsGoogleLogion> */}
-      {/* <CustomButton onClick={() => signInWithGoogle}>SIGN IN WITH GOOGLE</CustomButton> */}
-      {/* <button onClick={signOut}>Sign out</button> */}
-      {/* {currentUser ? (
-        <div className="option" onClick={() => auth.signOut()}>
-          SIGN OUT
-        </div>
-      ) : (
-        <Link className="option" to="/signin">
-          SIGN IN
-        </Link>
-      )} */}
-      {/* <GoogleLogin
-        clientId="76445230270-03g60q4kooi6qg0qvtjtnqqnn70juulc.apps.googleusercontent.com"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogleFail}
-        render={renderProps => (
-          <button onClick={renderProps.onClick} disabled={renderProps.disabled}>
-            LOGIN WITH GOOGLE
-          </button>
-        )}
-        cookiePolicy={'single_host_origin'}
-      /> */}
     </LoginWrap>
   )
 }
