dd9a75be51a8649403f1d92ce03a4667d3ca768a
leeeunbee <herbione@inforex.co.kr>
2020-04-02 14:20:25 +0900

    마이페이지 방송 설정 모바일 작업 중

---
+++ web/src/pages/mMypage/component/setting/manager.js
@@ -0,0 +1,28 @@
+/**
+ * @file /mypage/component/blacklist.js
+ * @brief 마이페이지 방송설정 - 매니저 설정
+ **/
+import React, {useState, useEffect, useContext, useRef} from 'react'
+import styled from 'styled-components'
+
+//context
+import {Context} from 'context'
+import Api from 'context/api'
+import {COLOR_MAIN, COLOR_POINT_Y, COLOR_POINT_P} from 'context/color'
+import {IMG_SERVER, WIDTH_TABLET_S, WIDTH_PC_S, WIDTH_TABLET, WIDTH_MOBILE, WIDTH_MOBILE_S} from 'context/config'
+
+export default props => {
+  //-----------------------------------------------------------------------------
+  //contenxt
+  const context = useContext(Context)
+
+  //state
+
+  //-----------------------------------------------------------------------------
+  //function
+
+  //-----------------------------------------------------------------------------
+  return <>매니저 설정입니다.</>
+}
+
+const Content = styled.div``
