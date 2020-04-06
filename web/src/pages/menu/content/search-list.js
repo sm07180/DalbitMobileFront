84f697b4bad591024c42646a224ce51096cd7107
yeshtml5 <yeshtml5@gmail.com>
2020-04-06 17:51:26 +0900

    검색영역

---
+++ web/src/pages/menu/content/search-list.js
@@ -0,0 +1,80 @@
+/**
+ * @title 최근 본 달디
+ */
+import React, {useEffect, useState} from 'react'
+import styled from 'styled-components'
+//context
+import API from 'context/api'
+// component
+import NoResult from 'components/ui/noResult'
+//
+export default props => {
+  //---------------------------------------------------------------------
+  //useState
+  //---------------------------------------------------------------------
+
+  //update
+  function update(mode) {
+    switch (true) {
+      case mode.search !== undefined: //-------------------------------검색어
+        const {query} = mode.search
+        fetchData(query)
+        break
+      default:
+        break
+    }
+  }
+  //makeContents
+  const makeContents = () => {
+    if (props.fetch === null || props.fetch === undefined) return
+    const {list} = props.fetch
+    if (list.length === 0) {
+      return <NoResult />
+    } else {
+      return list.map((list, idx) => {
+        const {title, profImg} = list
+        console.log(profImg.thumb150x150)
+        return (
+          <div key={idx} className="list">
+            <button>
+              <img src={profImg.thumb150x150} />
+            </button>
+            <p>{title}</p>
+          </div>
+        )
+      })
+    }
+  }
+  //---------------------------------------------------------------------
+  return <Content>{makeContents()}</Content>
+}
+//---------------------------------------------------------------------
+const Content = styled.div`
+  button {
+    display: inline-block;
+    width: 72px;
+    height: 72px;
+    border-radius: 26px;
+    overflow: hidden;
+    img {
+      width: 100%;
+      height: auto;
+      vertical-align: top;
+    }
+  }
+  p {
+    display: block;
+    margin-top: 6px;
+    width: 72px;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    font-size: 11px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.45;
+    letter-spacing: -0.28px;
+    text-align: center;
+    color: #424242;
+  }
+`
--- web/src/pages/menu/content/search_bar.js
+++ web/src/pages/menu/content/search_bar.js
@@ -1,20 +1,69 @@
-import React, {useEffect, useState} from 'react'
+/**
+ * @title 검색바
+ */
+import React from 'react'
 import styled from 'styled-components'
-//context
-import API from 'context/api'
-// component
-import Header from '../component/header.js'
-
+//hooks
+import useChange from 'components/hooks/useChange'
+//static
+import SearchIco from '../static/search/ic_search_p.svg'
+//
 export default props => {
   //---------------------------------------------------------------------
-
-  //useEffect
-  useEffect(() => {}, [])
+  //hooks
+  const {changes, setChanges, onChange} = useChange(update, {onChange: -1})
+  //update
+  function update(mode) {
+    switch (true) {
+      case mode.onChange !== undefined:
+        break
+    }
+  }
   //---------------------------------------------------------------------
   return (
-    <div>
-      <div>검색영역</div>
-    </div>
+    <Content>
+      <div className="in_wrap">
+        <input type="text" name="query" placeholder="검색어를 입력해 보세요." onChange={onChange} />
+        <button
+          onClick={() => {
+            props.update({search: changes})
+          }}>
+          <img className="ico" src={SearchIco} />
+        </button>
+      </div>
+    </Content>
   )
 }
 //---------------------------------------------------------------------
+const Content = styled.div`
+  display: block;
+  margin-top: 20px;
+  margin-bottom: 36px;
+  padding: 17px 16px 15px;
+  border-radius: 28px;
+  border: solid 2px #8556f6;
+  .in_wrap {
+    position: relative;
+    input {
+      display: block;
+      width: 100%;
+      color: #424242;
+      font-size: 14px;
+      line-height: 1.71;
+      letter-spacing: -0.35px;
+      text-align: left;
+      &:focus + button .ico {
+        right: -10px;
+        transform: rotate(60deg);
+      }
+    }
+    button {
+      position: absolute;
+      top: -7px;
+      right: 0;
+      .ico {
+        transition: 0.3s;
+      }
+    }
+  }
+`
