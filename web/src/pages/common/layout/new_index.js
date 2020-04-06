e176dad8095619fd9721e51e7609dfddaf5b91b6
Park Songwon <songwon.bak@gmail.com>
2020-04-06 10:36:14 +0900

    recover layout file

---
+++ web/src/pages/common/layout/new_index.js
@@ -0,0 +1,33 @@
+/**
+ *
+ */
+import React from 'react'
+//context
+//layout
+import Gnb from 'pages/common/newGnb'
+import NewPlayer from 'pages/common/newPlayer'
+import Popup from 'pages/common/popup'
+import Message from 'pages/common/message'
+import TopScrollBtn from 'pages/main/component/top_scroll_btn.js'
+//
+const Layout = props => {
+  const {children} = props
+  //---------------------------------------------------------------------
+  return (
+    <React.Fragment>
+      {/* GNB */}
+      {props.status !== 'no_gnb' && <Gnb />}
+      {/* 탑버튼 */}
+      <TopScrollBtn />
+      <article>{children}</article>
+      {/* (방송방)Player */}
+      <NewPlayer {...props} />
+      {/* 레이어팝업 */}
+      <Popup {...props} />
+      {/* 메시지팝업 */}
+      <Message {...props} />
+    </React.Fragment>
+  )
+}
+export default Layout
+//---------------------------------------------------------------------
