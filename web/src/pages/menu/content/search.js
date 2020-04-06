fb667bccce217c26447d7dc9d36dd90a39612d5c
yeshtml5 <yeshtml5@gmail.com>
2020-04-06 16:37:31 +0900

    검색영역수정

--- web/src/pages/menu/content/search.js
+++ web/src/pages/menu/content/search.js
@@ -1,17 +1,45 @@
+/**
+ * @title 검색바
+ * @todos 디자인과 기획과 상이한부분이 있음
+ */
 import React, {useEffect, useState} from 'react'
 import styled from 'styled-components'
-
+//context
+import API from 'context/api'
 // component
 import Header from '../component/header.js'
-
+import SearchBar from './search_bar'
+//
 export default props => {
+  //---------------------------------------------------------------------
+  //fetch
+  async function fetchData() {
+    const qs = location.href.split('?')[1] && decodeURIComponent(location.href.split('?')[1].split('=')[1])
+    const res = await API.live_search({
+      params: {
+        search: qs,
+        page: 1,
+        records: 10
+      }
+    })
+    if (res.result === 'success') {
+    }
+    console.log(res)
+  }
+  //useEffect
+  useEffect(() => {
+    fetchData()
+  }, [])
+  //---------------------------------------------------------------------
   return (
     <div>
       <Header>
         <div className="category-text">검색</div>
       </Header>
+      {/* 검색바 */}
+      <SearchBar />
       <div>검색영역</div>
     </div>
   )
 }
-//-------------------------
+//---------------------------------------------------------------------
---
+++ web/src/pages/menu/content/search_bar.js
@@ -0,0 +1,20 @@
+import React, {useEffect, useState} from 'react'
+import styled from 'styled-components'
+//context
+import API from 'context/api'
+// component
+import Header from '../component/header.js'
+
+export default props => {
+  //---------------------------------------------------------------------
+
+  //useEffect
+  useEffect(() => {}, [])
+  //---------------------------------------------------------------------
+  return (
+    <div>
+      <div>검색영역</div>
+    </div>
+  )
+}
+//---------------------------------------------------------------------
