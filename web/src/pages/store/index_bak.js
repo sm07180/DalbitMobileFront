c62cd1144fd3ddae6385adc1979df80d12aec5f7
leeeunbee <herbione@inforex.co.kr>
2020-04-10 17:06:51 +0900

    로그인 푸쉬 수정

--- web/src/pages/store/index_bak.js
+++ web/src/pages/store/index_bak.js
@@ -78,7 +78,7 @@ export default props => {
         price: selected.price
       })
     } else {
-      props.history.push('/login?redirect=/store')
+      props.history.push('/login')
     }
   }
 
