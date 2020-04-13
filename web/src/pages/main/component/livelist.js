aadd4acba4f890cb1be37daf8b0289e16a21f8eb
Park Songwon <songwon.bak@gmail.com>
2020-04-13 17:28:52 +0900

    add icon

--- web/src/pages/main/component/livelist.js
+++ web/src/pages/main/component/livelist.js
@@ -7,6 +7,10 @@ import {broadcastLive} from 'constant/broadcast.js'
 
 import HeartIcon from '../static/ic_heart_s_g.svg'
 import HeadphoneIcon from '../static/ic_headphones_s.svg'
+import audioIcon from '../static/ico_audio.svg'
+import videoIcon from '../static/ico_video.svg'
+import maleIcon from '../static/ico_male.png'
+import femaleIcon from '../static/ico_female.png'
 
 function usePrevious(value) {
   const ref = useRef()
