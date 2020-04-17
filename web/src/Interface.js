65cc58d8f4372cad8de5ddaabff41026d2dad4ea
yeshtml5 <yeshtml5@gmail.com>
2020-04-17 13:47:25 +0900

    push_type

--- web/src/Interface.js
+++ web/src/Interface.js
@@ -46,10 +46,11 @@ native-push-foreground
          */
       case 'native-push-background': //----------------------------native-push-foreground
         let pushMsg = decodeURIComponent(event.detail)
-
         pushMsg = pushMsg.trim()
+        alert(pushMsg)
         pushMsg = JSON.parse(pushMsg)
         const {push_type} = pushMsg
+
         let room_no, mem_no
         //---------------------[분기처리시작]
         switch (push_type) {
