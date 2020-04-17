f24b3fe83e156409b633dd43a5e177e9d7b23b53
Park Songwon <songwon.bak@gmail.com>
2020-04-17 18:05:27 +0900

    add minor code

--- web/src/components/lib/link_control.js
+++ web/src/components/lib/link_control.js
@@ -1,11 +1,11 @@
 export const saveUrlAndRedirect = targetLink => {
-  sessionStorage.setItem(targetLink, window.location.href)
+  window.sessionStorage.setItem(targetLink, window.location.href)
   window.location.href = targetLink
 }
 
 export const getUrlAndRedirect = () => {
-  const prevUrl = sessionStorage.getItem(location.pathname)
-  sessionStorage.removeItem(location.pathname)
+  const prevUrl = window.sessionStorage.getItem(location.pathname)
+  window.sessionStorage.removeItem(location.pathname)
 
   if (prevUrl) {
     window.location.href = prevUrl
