656043cc6f1da10a6353adfbb607c92c62e87dc6
yeshtml5 <yeshtml5@gmail.com>
2020-04-17 14:14:57 +0900

    detail

--- web/src/Interface.js
+++ web/src/Interface.js
@@ -46,10 +46,10 @@ native-push-foreground
             7 : 공지사항 페이지 [board_idx]
          */
       case 'native-push-background': //----------------------------native-push-foreground
-        alert('native-push-background')
+        //alert('native-push-background')
         let pushMsg = decodeURIComponent(event.detail)
         pushMsg = pushMsg.trim()
-        alert(pushMsg)
+        // alert(pushMsg)
         pushMsg = JSON.parse(pushMsg)
         const {push_type} = pushMsg
 
