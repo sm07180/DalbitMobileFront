201e7b8229c3bd5dc3d41040b60b5375cedc7d4b
황상한 <nmc2711@inforex.co.kr>
2020-04-23 15:04:55 +0900

    약관

---
+++ web/src/pages/agree/content/sess3.js
@@ -0,0 +1,18 @@
+/**
+ * @file 모바일/agree/index.js
+ * @brief 설정
+ * @todo
+ */
+
+import React, {useState, useEffect, useContext} from 'react'
+import styled from 'styled-components'
+
+export default props => {
+  //---------------------------------------------------------------------
+
+  return (
+    <>
+      <h1>s233</h1>
+    </>
+  )
+}
