8510612617ea266e62fcb955ce216d501206671a
yeshtml5 <yeshtml5@gmail.com>
2020-04-24 10:42:36 +0900

    windowLocation 정리

--- web/src/pages/login/index.js
+++ web/src/pages/login/index.js
@@ -122,8 +122,35 @@ export default (props) => {
     })
     console.log(res)
     if (res.status === 200) {
+      /**
+       * @title webview IOS 디바이스이동 이슈때문에적용
+       * @param {*} url
+       */
+      function windowLocation(url) {
+        var X = setTimeout(function () {
+          window.location.replace(url)
+          return true
+        }, 300)
+
+        if ((window.location = url)) {
+          clearTimeout(X)
+          return true
+        } else {
+          if ((window.location.href = url)) {
+            clearTimeout(X)
+            return true
+          } else {
+            clearTimeout(X)
+            window.location.replace(url)
+            return true
+          }
+        }
+        return false
+      }
+
       const redirectUrl = await res.text()
-      window.location = redirectUrl
+      windowLocation(redirectUrl)
+      //window.location = redirectUrl
     }
   }
 
