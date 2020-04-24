021a5df9e60168eb0729e54bd0d824998ffaf20e
Park Songwon <songwon.bak@gmail.com>
2020-04-24 11:11:52 +0900

    minor temp

--- web/src/pages/login/index.js
+++ web/src/pages/login/index.js
@@ -127,8 +127,11 @@ export default props => {
 
     if (res.status === 200) {
       const redirectUrl = await res.text()
+      setTimeout(function() {
+        document.location.href = redirectUrl
+      }, 250)
       // alert(redirectUrl)
-      window.location.href = redirectUrl
+      // window.location.href = redirectUrl
       // window.location.href = JSON.stringify(redirectUrl)
     }
   }
