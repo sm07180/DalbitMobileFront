999273ea9ee8750e78c7042d3d807065cc0e700f
황상한 <nmc2711@inforex.co.kr>
2020-04-27 18:04:00 +0900

    팬보드 및 내정보관리 수정

--- web/src/pages/mypage/setting.js
+++ web/src/pages/mypage/setting.js
@@ -52,32 +52,33 @@ export default props => {
     // reader.readAsArrayBuffer(file)
     reader.readAsDataURL(file)
 
-    function getOrientation(buffer) {
-      var view = new DataView(buffer)
-      if (view.getUint16(0, false) !== 0xffd8) return -2
-      var length = view.byteLength,
-        offset = 2
-      while (offset < length) {
-        var marker = view.getUint16(offset, false)
-        offset += 2
-        if (marker === 0xffe1) {
-          if (view.getUint32((offset += 2), false) !== 0x45786966) return -1
-          var little = view.getUint16((offset += 6), false) === 0x4949
-          offset += view.getUint32(offset + 4, little)
-          var tags = view.getUint16(offset, little)
-          offset += 2
-          for (var i = 0; i < tags; i++) {
-            if (view.getUint16(offset + i * 12, little) === 0x0112) return view.getUint16(offset + i * 12 + 8, little)
-          }
-        } else if ((marker & 0xff00) !== 0xff00) {
-          break
-        } else {
-          offset += view.getUint16(offset, false)
-        }
-      }
-      return -1
-    }
+    // function getOrientation(buffer) {
+    //   var view = new DataView(buffer)
+    //   if (view.getUint16(0, false) !== 0xffd8) return -2
+    //   var length = view.byteLength,
+    //     offset = 2
+    //   while (offset < length) {
+    //     var marker = view.getUint16(offset, false)
+    //     offset += 2
+    //     if (marker === 0xffe1) {
+    //       if (view.getUint32((offset += 2), false) !== 0x45786966) return -1
+    //       var little = view.getUint16((offset += 6), false) === 0x4949
+    //       offset += view.getUint32(offset + 4, little)
+    //       var tags = view.getUint16(offset, little)
+    //       offset += 2
+    //       for (var i = 0; i < tags; i++) {
+    //         if (view.getUint16(offset + i * 12, little) === 0x0112) return view.getUint16(offset + i * 12 + 8, little)
+    //       }
+    //     } else if ((marker & 0xff00) !== 0xff00) {
+    //       break
+    //     } else {
+    //       offset += view.getUint16(offset, false)
+    //     }
+    //   }
+    //   return -1
+    // }
 
+    //캔버스로 그려서 dataurl 로 뽑아내는 함수
     function drawAdjustImage(img) {
       const cnvs = document.createElement('canvas')
       var ctx = cnvs.getContext('2d')
