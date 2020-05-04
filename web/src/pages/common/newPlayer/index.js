e9ed76e9b0b41dc4ffa3e2dc61c4041cc22c2cef
황상한 <nmc2711@inforex.co.kr>
2020-05-04 14:29:07 +0900

    룸조인테스트

--- web/src/pages/common/newPlayer/index.js
+++ web/src/pages/common/newPlayer/index.js
@@ -28,6 +28,7 @@ export default props => {
         break
       case mode.playerNavigator !== undefined: //----------------------방송방으로 이동
         //
+        alert('데브테스트')
         if (__NODE_ENV === 'dev') {
           // const commonData = async obj => {
           //   const res = await Api.broad_list()
@@ -41,6 +42,7 @@ export default props => {
           setRoomNo(sessionStorage.getItem('room_no'))
           const res = await Api.broad_join({data: {roomNo: RoomNo}})
           const {code, result, data} = res
+          
           alert('1ㄴ:', res)
           alert('2:',res.data)
           
