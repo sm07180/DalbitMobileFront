46d4bb1e3ec3a6bd50a362695a2e59ab08e084af
leeeunbee <herbione@inforex.co.kr>
2020-05-12 09:46:37 +0900

    랭킹 수정

--- web/src/pages/ranking/content/index.js
+++ web/src/pages/ranking/content/index.js
@@ -44,6 +44,7 @@ export default props => {
   const [popup, setPopup] = useState(false)
   //const [moreState, setMoreState] = useState(false)
   const [myRank, setMyRank] = useState('-')
+  const [myProfile, setMyProfile] = useState(false)
 
   const popStateEvent = e => {
     if (e.state === null) {
@@ -102,26 +103,19 @@ export default props => {
   }
 
   const creatMyRank = () => {
-    const {profImg, level, nickNm, memNo} = context.profile
-    return (
-      <div className="my-rank">
-        <div className="figure-wrap">
-          <Figure url={profImg.thumb120x120} memNo={memNo} link={`/menu/profile`} />
-        </div>
-        <h3 className="flag">
-          <span>{myRank}</span>
-        </h3>
-        <div className="text">
-          <span>
-            Lv {level} {nickNm}
-          </span>
-        </div>
-        <button className="rank-info-btn" onClick={() => setPopup(popup ? false : true)}>
-          <img src={iconQ} alt="랭킹 산정 방법" />
-        </button>
-      </div>
-    )
+    if (context.token.isLogin) {
+      const settingProfileInfo = async memNo => {
+        const profileInfo = await Api.profile({params: {memNo: context.token.memNo}})
+        if (profileInfo.result === 'success') {
+          setMyProfile(profileInfo.data)
+        }
+      }
+      settingProfileInfo()
+    } else {
+      return null
+    }
   }
+
   //---------------------------------------------------------------------
   //fetch
   async function fetch(type, dateType, next) {
@@ -214,6 +208,8 @@ export default props => {
       fetch(rankType, dateType)
     }
 
+    creatMyRank()
+
     window.addEventListener('popstate', popStateEvent)
 
     return () => {
@@ -242,7 +238,24 @@ export default props => {
         <div className="rank-type">{createRankButton()}</div>
         <div className="date-type">{createDateButton()}</div>
       </div>
-      {context.profile && creatMyRank()}
+      {myProfile && (
+        <div className="my-rank">
+          <div className="figure-wrap">
+            <Figure url={myProfile.profImg.thumb120x120} memNo={myProfile.memNo} link={`/menu/profile`} />
+          </div>
+          <h3 className="flag">
+            <span>{myRank}</span>
+          </h3>
+          <div className="text">
+            <span>
+              Lv {myProfile.level} {myProfile.nickNm}
+            </span>
+          </div>
+          <button className="rank-info-btn" onClick={() => setPopup(popup ? false : true)}>
+            <img src={iconQ} alt="랭킹 산정 방법" />
+          </button>
+        </div>
+      )}
       {creatResult()}
 
       {popup && <LayerPopup setPopup={setPopup} />}
