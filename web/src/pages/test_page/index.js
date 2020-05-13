a2ffde55926b3b6ca2ca2440a1f6427b43791db5
unknown <EUNJI>
2020-05-13 18:22:51 +0900

    달충전하기 작업 중

--- web/src/pages/test_page/index.js
+++ web/src/pages/test_page/index.js
@@ -3,13 +3,11 @@ import React from 'react'
 import Seo from './seo.js'
 import Lee from './lee.js'
 
-import './test.scss'
-
 export default () => {
   return (
     <div className="test-page">
-      <Seo />
-      {false && <Lee />}
+      {false && <Seo />}
+      <Lee />
     </div>
   )
 }
--- web/src/pages/test_page/lee.js
+++ web/src/pages/test_page/lee.js
@@ -1,5 +1,15 @@
 import React from 'react'
 
+import './lee.scss'
+import BackBtn from './static/ic_back.svg'
+
 export default () => {
-  return <div></div>
+  return (
+    <>
+      <div className="header">
+        <img className="header__button--back" src={BackBtn} />
+        <h1 className="header__title">달 충전하기</h1>
+      </div>
+    </>
+  )
 }
