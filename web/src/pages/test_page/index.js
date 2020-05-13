ae71f400a201953af4a8da955addd34f6ec8c22b
unknown <EUNJI>
2020-05-13 16:37:11 +0900

    test page base

--- web/src/pages/test_page/index.js
+++ web/src/pages/test_page/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import Seo from './seo.js'
+import Lee from './lee.js'
+
 import './test.scss'
 
 export default () => {
-  return <div className="test-page">test page</div>
+  return (
+    <div className="test-page">
+      <Seo />
+      {false && <Lee />}
+    </div>
+  )
 }
