6732ecdfbb63087fd539c0aba46fc9da72d29d96
Jaeoh Kim <jaeohkim7@gmail.com>
2020-05-15 10:28:22 +0900

    2020-05-15 kjo

--- web/src/pages/test_page/index.js
+++ web/src/pages/test_page/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
 import Seo from './seo.js'
-import Lee from './lee.js'
-import Test from './test.js';
 
 export default () => {
   return (
     <div className="test-page">
-      {false && <Seo />}
+      <Seo />
       {false && <Lee />}
-      <Test />
     </div>
   )
 }
