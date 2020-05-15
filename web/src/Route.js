0693e3dc9a2cd9e187f93de871d61131702a2d2d
yeshtml5 <yeshtml5@gmail.com>
2020-05-15 14:54:04 +0900

    TestPage

--- web/src/Route.js
+++ web/src/Route.js
@@ -34,7 +34,7 @@ const ErrorPage = React.lazy(() => import('pages/common/error'))
 //Redirect
 const TempLogin = React.lazy(() => import('pages/common/redirect'))
 
-const TestPage = React.lazy(() => import('pages/test_page'))
+//const TestPage = React.lazy(() => import('pages/test_page'))
 //
 export default () => {
   return (
@@ -73,9 +73,9 @@ export default () => {
 
         <Route exact path="/agree" component={Agree} />
         <Route exact path="/agree/:title" component={Agree} />
-
+        {/* 
         <Route exact path="/temp_test" component={TestPage} />
-        <Route exact path="/temp_test/:path" component={TestPage} />
+        <Route exact path="/temp_test/:path" component={TestPage} /> */}
 
         <Route exact path="/error" component={ErrorPage} />
 
