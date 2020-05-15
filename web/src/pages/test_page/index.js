10e4a9a864da84caeaaf1d77a8d112bac36ba236
gaengkun <gaengkun@gmail.com>
2020-05-15 10:03:28 +0900

    환전페이지 커밋

--- web/src/pages/test_page/index.js
+++ web/src/pages/test_page/index.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
 import Seo from './seo.js'
-import Lee from './lee.js'
 
 export default () => {
   return (
     <div className="test-page">
-      {false && <Seo />}
-      <Lee />
+      <Seo />
+      {false && <Lee />}
     </div>
   )
 }
