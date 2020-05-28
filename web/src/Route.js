dd058aa17f222bfcad1e26f695e71dbbb4ae7644
Jaeoh Kim <jaeohkim7@gmail.com>
2020-05-28 09:09:25 +0900

    2020-05-28 환전하기 폴더 생성 및 파일 이동

--- web/src/Route.js
+++ web/src/Route.js
@@ -44,6 +44,8 @@ const TempPage = React.lazy(() => import('pages/temp'))
 
 const TestPage = React.lazy(() => import('pages/test_page'))
 
+const MoneyExchange = React.lazy(() => import('pages/money_exchange'))
+
 export default () => {
   return (
     <React.Suspense
@@ -85,6 +87,7 @@ export default () => {
         <Route exact path="/temp_test" component={TestPage} />
         <Route exact path="/temp_test/:path" component={TestPage} />
         <Route exact path="/temp_page" component={TempPage} />
+        <Route exact path="/money_exchange" component={MoneyExchange} />
         <Route exact path="/error" component={ErrorPage} />
         <Route exact path="/redirect" component={TempLogin} />
         <Redirect to="/error" />
