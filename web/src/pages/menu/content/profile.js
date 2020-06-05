3a70dd96f81f0bdcb2e987188f5cc89452e83eb1
이재은 <coreano81@inforex.co.kr>
2020-06-05 18:02:53 +0900

    모바일 관리자 오픈

--- web/src/pages/menu/content/profile.js
+++ web/src/pages/menu/content/profile.js
@@ -139,7 +139,7 @@ export default props => {
             <img src={Setting} />
           </a>
         )}
-        {__NODE_ENV === 'dev' && token && token.isLogin && showAdmin && (
+        {token && token.isLogin && showAdmin && (
           <a href="/admin/image">
             <img src={AdminIcon} />
           </a>
