8d01675ac323998cc2156c7f1eb6cba0d4f178ff
이재은 <coreano81@inforex.co.kr>
2020-06-10 14:00:17 +0900

    이벤트 좋아요 일때만 노출

--- web/src/pages/event_page/index.js
+++ web/src/pages/event_page/index.js
@@ -204,9 +204,11 @@ export default props => {
                             <div className="nick-name-wrap">
                               <span className="nick-name">{nickName}</span>
                               <span className="level">Lv{level}</span>
-                              {/* <div className="exp-box">
-                                {chageEventText()} <span>{gainPoint}</span>
-                              </div> */}
+                              { rankingType == 'like' &&
+                                <div className="exp-box">
+                                    {chageEventText()} <span>{gainPoint}</span>
+                                </div>
+                              }
                             </div>
                           </div>
                           {rankingType === 'exp' && fanRank1 && (
