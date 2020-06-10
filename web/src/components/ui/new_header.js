60eeba11862e7033267387b6e3a3b1bef1273c93
leeeunbee <herbione@inforex.co.kr>
2020-06-10 10:47:22 +0900

    2.5 서브 레이아웃

---
+++ web/src/components/ui/new_header.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import styled from 'styled-components'
+
+// static
+import closeBtn from './ic_back.svg'
+
+export default props => {
+  const goBack = () => {
+    window.history.back()
+  }
+
+  return (
+    <Header className="header-wrap">
+      <img className="close-btn" src={closeBtn} onClick={goBack} />
+      <h1>{props.title}</h1>
+    </Header>
+  )
+}
+
+const Header = styled.header`
+  display: flex;
+  width: 100%;
+  height: 40px;
+  background: #fff;
+  border-bottom: 1px solid #e0e0e0;
+  justify-content: center;
+
+  h1 {
+    display: inline-block;
+    font-size: 18px;
+    font-weight: 800;
+    color: #000;
+    line-height: 40px;
+  }
+  .close-btn {
+    display: block;
+    position: fixed;
+    left: 6px;
+    width: 36px;
+  }
+`
