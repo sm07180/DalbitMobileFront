c5ddcd317c454b7d0f2413f5264f09bfda9fed6c
leeeunbee <herbione@inforex.co.kr>
2020-06-11 15:20:53 +0900

    결제 테스트 수정

--- web/src/pages/charge/content/payment.js
+++ web/src/pages/charge/content/payment.js
@@ -42,9 +42,9 @@ export default props => {
 
   const {webview} = qs.parse(location.search)
 
-  let paymentName = '달 10'
-  let paymentPrice = '3000'
-  let payItemNo = 'CA0000001'
+  let paymentName = '달 50'
+  let paymentPrice = 5500
+  let payItemNo = 'A1865'
   let pageCode = '1'
 
   if (props.location.state) {
