6ea5f4284f41aed2ab05cba02c0a968649e85b6f
leeeunbee <herbione@inforex.co.kr>
2020-06-18 15:43:52 +0900

    라우트 수정

--- web/src/Route.js
+++ web/src/Route.js
@@ -27,7 +27,10 @@ let MySetting = React.lazy(() => import('pages/mypage/setting.js'))
 const Pay = React.lazy(() => import('pages/pay'))
 const PayResult = React.lazy(() => import('pages/pay_result'))
 const Store = React.lazy(() => import('pages/store'))
-const Charge = React.lazy(() => import('pages/charge'))
+let Charge = React.lazy(() => import('pages/charge'))
+if (__NODE_ENV !== 'real') {
+  Charge = React.lazy(() => import('pages/charge/index_test'))
+}
 const ChargeTest = React.lazy(() => import('pages/charge/index_test'))
 const Exchange = React.lazy(() => import('pages/exchange'))
 const Customer = React.lazy(() => import('pages/customer'))
