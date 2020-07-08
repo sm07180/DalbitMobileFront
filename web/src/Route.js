893887971dd4133c4ae8678a074f5d8f3d183235
이재은 <coreano81@inforex.co.kr>
2020-07-08 17:08:02 +0900

    라우트 정리

--- web/src/Route.js
+++ web/src/Route.js
@@ -21,7 +21,7 @@ const Store = React.lazy(() => import('pages/store'))
 const Charge = React.lazy(() => import('pages/charge'))
 
 const Exchange = React.lazy(() => import('pages/exchange'))
-const Customer = React.lazy(() => import('pages/customer_copy'))
+const Customer = React.lazy(() => import('pages/customer'))
 const LevelInfo = React.lazy(() => import('pages/level'))
 const Setting = React.lazy(() => import('pages/setting'))
 const Event = React.lazy(() => import('pages/event'))
