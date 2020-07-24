8a76ce4824d9728abeba145f1151b5510a800177
leeeunbee <herbione@inforex.co.kr>
2020-07-24 16:40:12 +0900

    본인인증 법정대리인 동의 나이 변경

--- web/src/pages/self_auth/legal_auth.js
+++ web/src/pages/self_auth/legal_auth.js
@@ -106,27 +106,10 @@ export default (props) => {
   }
 
   function calcAge(birth) {
-    var date = new Date()
+    const currentYear = new Date().getFullYear() + 1
+    const birthYear = birth.slice(0, 4)
 
-    var year = date.getFullYear()
-
-    var month = date.getMonth() + 1
-
-    var day = date.getDate()
-
-    if (month < 10) month = '0' + month
-
-    if (day < 10) day = '0' + day
-
-    var monthDay = month + day
-
-    var birthdayy = birth.substr(0, 4)
-
-    var birthdaymd = birth.substr(4, 4)
-
-    var age = monthDay < birthdaymd ? year - birthdayy - 1 : year - birthdayy
-
-    return age
+    return currentYear - birthYear
   }
 
   //인증 요청 버튼 벨리데이션
