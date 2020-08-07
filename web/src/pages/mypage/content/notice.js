6a041e4300cf78d9c1ecd2c05708b77becb50481
황상한 <nmc2711@inforex.co.kr>
2020-08-07 16:05:25 +0900

    공지사항 수정

--- web/src/pages/mypage/content/notice.js
+++ web/src/pages/mypage/content/notice.js
@@ -19,7 +19,7 @@ import pen from 'images/pen.svg'
 import WhitePen from '../component/images/WhitePen.svg'
 import {COLOR_MAIN, COLOR_POINT_Y, COLOR_POINT_P, PHOTO_SERVER} from 'context/color'
 import {IMG_SERVER, WIDTH_MOBILE} from 'context/config'
-
+import {DalbitTextArea} from '../content/textarea'
 // concat
 let currentPage = 1
 let timer
@@ -329,11 +329,13 @@ const Notice = (props) => {
           </div>
 
           <div className="contentWrite">
-            <textarea
+            <DalbitTextArea
+              state={comentContent}
+              setState={setCommentContent}
+              rows={10}
+              maxLength={200}
+              className="MsgText"
               placeholder="작성하고자 하는 글의 내용을 입력해주세요."
-              maxLength="189"
-              onChange={textChangeContent}
-              value={comentContent}
             />
           </div>
           <div className="checkbox-wrap">
