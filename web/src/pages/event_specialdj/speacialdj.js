1eecb9324ed0bb4552457dcff37b8723215dd570
gaengkun <gaengkun@gmail.com>
2020-08-25 20:30:34 +0900

    스페셜 dj 마크업 수정

--- web/src/pages/event_specialdj/speacialdj.js
+++ web/src/pages/event_specialdj/speacialdj.js
@@ -1,29 +1,30 @@
+import React, {useContext, useEffect, useState} from 'react'
+
 import {Context} from 'context'
+import {useHistory} from 'react-router-dom'
 import Api from 'context/api'
 import qs from 'query-string'
 
 import Layout from 'pages/common/layout'
-
-import React, {useContext, useEffect, useState} from 'react'
-import {useHistory} from 'react-router-dom'
-import './speacialdj.scss'
 import CloseBtn from './static/ic_close.svg'
 import speacialOn from './static/ic_success.svg'
 import speacialOff from './static/ic_unsuccess.svg'
 import titleImg from './static/title_img.jpg'
 import bottomImg from './static/bottom_img.jpg'
+import './speacialdj.scss'
 
 export default (props) => {
   const history = useHistory()
-  const [check, setCheck] = useState('')
+
+  const parameter = qs.parse(location.search)
+  const globalCtx = useContext(Context)
+  const {token} = globalCtx
+
   const [toggleCheck, setToggleCheck] = useState('')
   const [conditionData, setconditionData] = useState('')
   const [imageData, setImageData] = useState('')
-
   const [infoData, setInfoData] = useState('')
-  const globalCtx = useContext(Context)
-  const {token} = globalCtx
-  const parameter = qs.parse(location.search)
+
   async function specialdjCheck() {
     const res = await Api.event_specialdj({
       data: {
@@ -46,34 +47,6 @@ export default (props) => {
 
   const goBack = () => {
     history.goBack()
-  }
-
-  function imgItem() {
-    return (
-      imageData &&
-      imageData.map((item, index) => {
-        if (imageData[index].content_type === 'image') {
-          return (
-            <div key={index}>
-              <img src={imageData[index].image_mobile_url} />
-            </div>
-          )
-        } else if (imageData[index].content_type === 'button') {
-          return (
-            <div className="buttonBg" key={index}>
-              <button
-                style={{color: `${imageData[index].button_name_color}`, background: `${imageData[index].button_color}`}}
-                className="buttonLink"
-                onClick={() => {
-                  globalCtx.action.updatePopup('TERMS', 'specialdj-goods-detail')
-                }}>
-                {imageData[index].button_name}
-              </button>
-            </div>
-          )
-        }
-      })
-    )
   }
 
   let evetStart = String(infoData.req_start_date)
@@ -101,6 +74,29 @@ export default (props) => {
     }
   }
 
+  function imgItem() {
+    return (
+      imageData &&
+      imageData.map((item, index) => {
+        if (imageData[index].content_type === 'image') {
+          return <img src={imageData[index].image_mobile_url} key={index} alt={`이미지 컨텐츠 ${index}`} />
+        } else if (imageData[index].content_type === 'button') {
+          return (
+            <button
+              key={index}
+              style={{color: `${imageData[index].button_name_color}`, background: `${imageData[index].button_color}`}}
+              className="buttonLink"
+              onClick={() => {
+                history.push('/modal/specialdj_goods')
+              }}>
+              {imageData[index].button_name}
+            </button>
+          )
+        }
+      })
+    )
+  }
+
   useEffect(() => {
     specialdjCheck()
   }, [])
@@ -108,140 +104,130 @@ export default (props) => {
   return (
     <Layout {...props} status="no_gnb">
       <div className="speacialdj">
-        <div className="djTitle">
-          <div className="djTitleSub">{infoData.title}</div>
-          <img src={CloseBtn} className="djRefresh" onClick={goBack} />
+        <div className="title">
+          <h2>{infoData.title}</h2>
+          <button onClick={goBack}>닫기 버튼</button>
         </div>
-        <div className="img100">{imgItem()}</div>
+        <div className="contentImg">{imgItem()}</div>
 
         {token.isLogin === true ? (
           <>
-            <div className="checkList">
-              <div className="img100">
-                <img src="https://image.dalbitlive.com/event/specialdj/20200824/title.jpg" />
-              </div>
-              <b className="dayTitle">
-                {`${evetStartY}년 ${evetStartM}월 ${evetStartD}일 ~ ${endM}월 ${endD}일`}
-                <br />(<span className="dayTitle--orange">{infoData.condition_end_date && eventEnd()}</span>)
-              </b>
+            <img src="https://image.dalbitlive.com/event/specialdj/20200824/title.jpg" alt="선발 방식" className="imgResize" />
+            <div className="dayTitle">
+              {`${evetStartY}년 ${evetStartM}월 ${evetStartD}일 ~ ${endM}월 ${endD}일`}
+              <br />(<p>{infoData.condition_end_date && eventEnd()}</p>)
             </div>
-            <h3 className="img100">
-              <img src={titleImg} title="지원 요건" />
-            </h3>
+            <img src={titleImg} alt="지원 요건" className="imgResize" />
 
             <div className="checkList">
-              <div className="checkList__box">
-                <div className="checkList__table">
-                  <div className="checkList__list">
-                    <div className="checkList__talbeLeft ">
-                      {conditionData.conditionTitle1}
-                      <br />({conditionData.conditionValue1})
-                    </div>
-                    {conditionData.condition1 !== 1 ? (
-                      <div className={`checkList__talbeRight checkList__talbeRight--red`}>
-                        <p>
-                          최소 신청 조건이
-                          <br />
-                          충족하지 않습니다
-                        </p>
-
-                        <img src={speacialOff} />
-                      </div>
-                    ) : (
-                      <div className={`checkList__talbeRight checkList__talbeRight--purple`}>
-                        <p>
-                          스페셜 DJ에
-                          <br />
-                          도전해보세요!
-                        </p>
-                        <img src={speacialOn} />
-                      </div>
-                    )}
+              <div className="checkList__table">
+                <div className="checkList__list">
+                  <div className="checkList__talbeLeft">
+                    {conditionData.conditionTitle1}
+                    <br />({conditionData.conditionValue1})
                   </div>
-                  <div className="checkList__list">
-                    <div className="checkList__talbeLeft">
-                      {conditionData.conditionTitle2}
-                      <br />({conditionData.conditionValue2})
+                  {conditionData.condition1 !== 1 ? (
+                    <div className="checkList__talbeRight checkList__talbeRight--red">
+                      <p>
+                        최소 신청 조건이
+                        <br />
+                        충족하지 않습니다
+                      </p>
+                      <img src={speacialOff} alt="실패" />
                     </div>
-                    {conditionData.condition2 !== 1 ? (
-                      <div className={`checkList__talbeRight checkList__talbeRight--red`}>
-                        <p>
-                          최소 신청 조건이
-                          <br />
-                          충족하지 않습니다
-                        </p>
-                        <img src={speacialOff} />
-                      </div>
-                    ) : (
-                      <div className={`checkList__talbeRight checkList__talbeRight--purple`}>
-                        <p>
-                          스페셜 DJ에
-                          <br />
-                          도전해보세요!
-                        </p>
-                        <img src={speacialOn} />
-                      </div>
-                    )}
-                  </div>
-                  <div className="checkList__list">
-                    <div className="checkList__talbeLeft">
-                      {conditionData.conditionTitle3}
-                      <br />({conditionData.conditionValue3})
+                  ) : (
+                    <div className="checkList__talbeRight checkList__talbeRight--purple">
+                      <p>
+                        스페셜 DJ에
+                        <br />
+                        도전해보세요!
+                      </p>
+                      <img src={speacialOn} alt="도전" />
                     </div>
-                    {conditionData.condition3 !== 1 ? (
-                      <div className={`checkList__talbeRight checkList__talbeRight--red`}>
-                        <p>
-                          최소 신청 조건이
-                          <br />
-                          충족하지 않습니다
-                        </p>
-                        <img src={speacialOff} />
-                      </div>
-                    ) : (
-                      <div className={`checkList__talbeRight checkList__talbeRight--purple`}>
-                        <p>
-                          스페셜 DJ에
-                          <br />
-                          도전해보세요!
-                        </p>
-                        <img src={speacialOn} />
-                      </div>
-                    )}
-                  </div>
+                  )}
                 </div>
-
-                {conditionData.already === 0 ? (
-                  <>
-                    {conditionData.condition1 === 1 && conditionData.condition2 === 1 && conditionData.condition3 === 1 ? (
-                      <button
-                        className="button button--on"
-                        onClick={() => {
-                          history.push(
-                            '/event_specialdj/write?select_year=' +
-                              parameter.select_year +
-                              '&select_month=' +
-                              parameter.select_month
-                          )
-                        }}>
-                        스페셜DJ 신청서 작성
-                      </button>
-                    ) : (
-                      <div className="button"> 다음에 지원해주세요 </div>
-                    )}
-                  </>
-                ) : (
-                  <>
-                    <div className="button button--on">이미 지원하셨습니다.</div>
-                  </>
-                )}
+                <div className="checkList__list">
+                  <div className="checkList__talbeLeft">
+                    {conditionData.conditionTitle2}
+                    <br />({conditionData.conditionValue2})
+                  </div>
+                  {conditionData.condition2 !== 1 ? (
+                    <div className="checkList__talbeRight checkList__talbeRight--red">
+                      <p>
+                        최소 신청 조건이
+                        <br />
+                        충족하지 않습니다
+                      </p>
+                      <img src={speacialOff} alt="실패" />
+                    </div>
+                  ) : (
+                    <div className="checkList__talbeRight checkList__talbeRight--purple">
+                      <p>
+                        스페셜 DJ에
+                        <br />
+                        도전해보세요!
+                      </p>
+                      <img src={speacialOn} alt="도전" />
+                    </div>
+                  )}
+                </div>
+                <div className="checkList__list">
+                  <div className="checkList__talbeLeft">
+                    {conditionData.conditionTitle3}
+                    <br />({conditionData.conditionValue3})
+                  </div>
+                  {conditionData.condition3 !== 1 ? (
+                    <div className="checkList__talbeRight checkList__talbeRight--red">
+                      <p>
+                        최소 신청 조건이
+                        <br />
+                        충족하지 않습니다
+                      </p>
+                      <img src={speacialOff} alt="실패" />
+                    </div>
+                  ) : (
+                    <div className="checkList__talbeRight checkList__talbeRight--purple">
+                      <p>
+                        스페셜 DJ에
+                        <br />
+                        도전해보세요!
+                      </p>
+                      <img src={speacialOn} alt="도전" />
+                    </div>
+                  )}
+                </div>
               </div>
+              {conditionData.already === 0 ? (
+                <>
+                  {conditionData.condition1 === 1 && conditionData.condition2 === 1 && conditionData.condition3 === 1 ? (
+                    <button
+                      className="buttonOn"
+                      onClick={() => {
+                        history.push(
+                          '/event_specialdj/write?select_year=' +
+                            parameter.select_year +
+                            '&select_month=' +
+                            parameter.select_month
+                        )
+                      }}>
+                      스페셜DJ 신청서 작성
+                    </button>
+                  ) : (
+                    <div className="buttonOff"> 다음에 지원해주세요</div>
+                  )}
+                </>
+              ) : (
+                <>
+                  <div className="buttonOn">이미 지원하셨습니다.</div>
+                </>
+              )}
             </div>
           </>
         ) : (
           <>
-            <div className="buttonBox buttonBox--logout">
+            <div className="loginBox">
               <button
-                className="button button--on"
+                className="buttonOn"
                 onClick={() => {
                   history.push({
                     pathname: '/login',
@@ -255,13 +241,7 @@ export default (props) => {
             </div>
           </>
         )}
-        {token.isLogin === true ? (
-          <div className="img100">
-            <img src={bottomImg} />
-          </div>
-        ) : (
-          ''
-        )}
+        {token.isLogin === true ? <img src={bottomImg} className="imgResize" alt="하단 배경 이미지" /> : <></>}
       </div>
     </Layout>
   )
