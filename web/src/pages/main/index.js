be300d9c5548a1c60e981c08c386daf0ebebbab0
Boram Lim <lbr@inforex.co.kr>
2020-09-02 13:29:55 +0900

    my profile 오류 수정

--- web/src/pages/main/index.js
+++ web/src/pages/main/index.js
@@ -625,7 +625,7 @@ export default (props) => {
                   <NoResult />
                 )
               ) : (
-                <div style={{height: '315px'}}></div>
+                <div style={{height: '3600px'}}></div>
               )}
             </div>
           </div>
