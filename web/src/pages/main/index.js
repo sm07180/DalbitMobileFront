ead2de2335ea5e9ab4fe4118b4dacd0105285e57
gaengkun <gaengkun@gmail.com>
2020-09-09 16:55:41 +0900

    메뉴 수정

--- web/src/pages/main/index.js
+++ web/src/pages/main/index.js
@@ -556,26 +556,26 @@ export default (props) => {
                 스토어
               </Link>
             </div>
-            {globalCtx.isDevIp && (
-              <>
-                <div className="tab">
-                  <Link
-                    to={'/clip'}
-                    onClick={(event) => {
-                      event.preventDefault()
-                      history.push('/clip')
-                    }}>
-                    클립
-                  </Link>
-                </div>
-              </>
-            )}
-            {/* : (
+            {globalCtx.isDevIp ? (
               <>
                 <div className="tab tab--yellow">
                   <Link
                     className="newicon"
                     to={'/clip'}
+                    onClick={(event) => {
+                      event.preventDefault()
+                      history.push('/clip')
+                    }}>
+                    클립<i>NEW</i>
+                  </Link>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="tab tab--yellow">
+                  <Link
+                    className="newicon"
+                    to={'/clip_open'}
                     onClick={(event) => {
                       event.preventDefault()
                       history.push('/clip_open')
@@ -584,7 +584,7 @@ export default (props) => {
                   </Link>
                 </div>
               </>
-            ) */}
+            )}
           </div>
           <button
             className="broadBtn"
