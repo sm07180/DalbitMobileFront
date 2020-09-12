6044340244e8a1a1f16166030c40556a5c2aec3a
leeeunbee <herbione@inforex.co.kr>
2020-09-12 14:33:59 +0900

    클립 댓글 - 재입장 테스트

--- web/src/pages/common/clipPlayer/clip_func.js
+++ web/src/pages/common/clipPlayer/clip_func.js
@@ -6,11 +6,8 @@ import Utility from 'components/lib/utility'
 
 export const clipJoin = (data, context) => {
   if (Utility.getCookie('listen_room_no') === undefined || Utility.getCookie('listen_room_no') === 'null') {
-    alert('1')
     clipExit(context)
-    alert('6')
     Hybrid('ClipPlayerJoin', data)
-    alert('7')
   } else {
     context.action.confirm({
       msg: '현재 청취 중인 방송방이 있습니다.\n클립을 재생하시겠습니까?',
@@ -27,16 +24,12 @@ export const clipJoin = (data, context) => {
 }
 
 export const clipExit = (context) => {
-  alert('2')
   Hybrid('ClipPlayerEnd')
-  alert('3')
   Utility.setCookie('clip-player-info', '', -1)
   context.action.updateClipState(null)
   context.action.updateClipPlayerState(null)
   context.action.updateClipState(null)
   context.action.updatePlayer(false)
-  alert('4')
-  alert('5')
 }
 
 export const updateClipInfo = (data) => {
