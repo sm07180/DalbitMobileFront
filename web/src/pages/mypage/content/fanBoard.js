095f2b27a7219d44a3d4b2d6ecb57d4206c4968b
Boram Lim <lbr@inforex.co.kr>
2020-09-14 08:50:47 +0900

    board component update

--- web/src/pages/mypage/content/fanBoard.js
+++ web/src/pages/mypage/content/fanBoard.js
@@ -3,7 +3,7 @@
  * @brief 마이페이지 팬보드2.5v
  */
 import React, {useEffect, useState, useContext, useRef} from 'react'
-import {useParams} from 'react-router-dom'
+import {useLocation, useHistory} from 'react-router-dom'
 //modules
 import qs from 'query-string'
 // context
@@ -13,7 +13,8 @@ import Api from 'context/api'
 //components
 // import Header from '../component/header.js'
 import Header from 'components/ui/new_header.js'
-import BoardList from './board_list'
+// component
+import BoardItem from './board_item'
 import WriteBoard from './board_write'
 //svg
 import NoResult from 'components/ui/noResult'
@@ -23,6 +24,7 @@ let timer
 let total = 2
 //layout
 export default (props) => {
+  let location = useLocation()
   //context
   const ctx = useContext(Context)
   const context = useContext(Context)
@@ -41,6 +43,48 @@ export default (props) => {
   const [totalCount, setTotalCount] = useState(0)
   const [isOther, setIsOther] = useState(true)
   const [moreState, setMoreState] = useState(false)
+
+  // context && location
+  // let location = useLocation()
+  // const context = useContext(Context)
+  // var urlrStr = location.pathname.split('/')[2]
+  //전체 댓글리스트(props)
+  const TotalList = props.list
+  const TotalCount = props.totalCount
+  //state
+  const [replyWriteState, setReplyWriteState] = useState(false)
+  const [boardReplyList, setBoardReplyList] = useState([])
+
+  //정보 댓글로 전달
+  const ReplyInfoTransfer = (boardIdx, item) => {
+    context.action.updateReplyIdx(boardIdx)
+    setReplyWriteState(true)
+    fetchDataReplyList(boardIdx)
+    //setTitleReplyInfo(item)
+    if (context.fanboardReplyNum === boardIdx) {
+      context.action.updateFanboardReplyNum(-1)
+    } else {
+      context.action.updateFanboardReplyNum(boardIdx)
+    }
+    context.action.updateFanboardReply(item)
+    context.action.updateToggleAction(true)
+    // window.scrollTo({top: 0, left: 0, behavior: 'auto'})
+  }
+  // 팬보드 댓글 조회
+  async function fetchDataReplyList(boardIdx) {
+    const res = await Api.member_fanboard_reply({
+      params: {
+        memNo: urlrStr,
+        boardNo: boardIdx
+      }
+    })
+    if (res.result === 'success') {
+      setBoardReplyList(res.data.list)
+      // setFetching(false)
+    } else if (res.result === 'fail') {
+    }
+  }
+
   // 스크롤 이벤트
   const scrollEvtHdr = (event) => {
     if (timer) window.clearTimeout(timer)
@@ -83,16 +127,26 @@ export default (props) => {
       }
     }
   }
-  const setAction = (value) => {
+  const setAction = (value, writeType) => {
+    console.log(value, writeType)
     if (value === true) {
       currentPage = 1
       fetchData()
+      fetchDataReplyList(context.fanboardReplyNum)
     }
     console.log(props)
     if (props.set) {
       props.set(true)
     }
   }
+  // const setAction = (value, writeType) => {
+  //   console.log(value, writeType)
+
+  //   if (value === true) {
+  //     props.set(true)
+  //     fetchDataReplyList(context.fanboardReplyNum)
+  //   }
+  // }
   // 팬보드 글 조회
   async function fetchData(next) {
     currentPage = next ? ++currentPage : currentPage
@@ -174,7 +228,76 @@ export default (props) => {
       <WriteBoard {...props} set={setAction} />
 
       {/* 팬보드 리스트 영역 */}
-      {totalCount > 0 ? <BoardList list={boardList} totalCount={totalCount} set={setAction} /> : <NoResult />}
+      {/* {totalCount > 0 ? <BoardList list={boardList} totalCount={totalCount} set={setAction} /> : <NoResult />} */}
+      {totalCount > 0 ? (
+        <div className="listWrap">
+          <>
+            {TotalCount && (
+              <div className="list-count">
+                <span>게시글</span>
+                <span>{TotalCount}</span>
+              </div>
+            )}
+            {boardList &&
+              boardList !== false &&
+              boardList.map((item, index, self) => {
+                const {replyCnt, boardIdx} = item
+                return (
+                  <React.Fragment key={index}>
+                    <div className={`list-item ${boardIdx === context.fanboardReplyNum && 'on'}`}>
+                      {item && <BoardItem key={`board-${index}`} data={item} set={setAction} />}
+
+                      <div className="list-item__bottom">
+                        <button className="btn__reply" onClick={() => ReplyInfoTransfer(boardIdx, item)}>
+                          {replyCnt > 0 ? (
+                            <>
+                              답글 <em>{replyCnt}</em>
+                            </>
+                          ) : (
+                            <>답글쓰기</>
+                          )}
+                        </button>
+                      </div>
+                      {context.fanboardReplyNum &&
+                        context.toggleState &&
+                        boardIdx === context.fanboardReplyNum &&
+                        boardReplyList &&
+                        boardReplyList !== false &&
+                        boardReplyList.map((item1, index1) => {
+                          return (
+                            <div className="reply-list" key={index1}>
+                              <BoardItem
+                                key={`reply-${index1}`}
+                                data={item1}
+                                set={setAction}
+                                isViewOn={context.fanboardReply.viewOn}
+                                replyShowIdx={context.fanboardReplyNum}
+                                titleReplyInfo={context.fanboardReply}
+                              />
+                            </div>
+                          )
+                        })}
+                      {context.fanboardReplyNum &&
+                        context.toggleState &&
+                        boardIdx === context.fanboardReplyNum &&
+                        replyWriteState && (
+                          <WriteBoard
+                            isViewOn={context.fanboardReply.viewOn}
+                            replyWriteState={replyWriteState}
+                            set={setAction}
+                            type={'reply'}
+                            list={self}
+                          />
+                        )}
+                    </div>
+                  </React.Fragment>
+                )
+              })}
+          </>
+        </div>
+      ) : (
+        <NoResult />
+      )}
     </div>
   )
 }
