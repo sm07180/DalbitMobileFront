2fbcd3b9813eaafc4eecc9af7960444e6a92f08c
leeeunbee <herbione@inforex.co.kr>
2020-09-14 09:33:23 +0900

    클립테스트

--- web/src/pages/common/clipPlayer/clip_func.js
+++ web/src/pages/common/clipPlayer/clip_func.js
@@ -7,8 +7,8 @@ import Utility from 'components/lib/utility'
 export const clipJoin = (data, context) => {
   if (Utility.getCookie('listen_room_no') === undefined || Utility.getCookie('listen_room_no') === 'null') {
     // clipExit(context)
-    alert(context.player + context.clipState)
-    if (!context.player && context.clipState) {
+    alert(context.player)
+    if (!context.player) {
       return context.action.alert({
         msg: '현재 청취 중인 클립이 있습니다.'
       })
