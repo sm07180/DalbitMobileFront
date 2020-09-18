597710bd2a8fcd3ac124648c79ab7b97f59f3dbf
Boram Lim <lbr@inforex.co.kr>
2020-09-18 15:40:43 +0900

    랭킹 수정

--- web/src/pages/ranking_renewal/components/MyProfile.js
+++ web/src/pages/ranking_renewal/components/MyProfile.js
@@ -73,7 +73,7 @@ export default function MyProfile() {
     ) {
       return (
         <>
-          <p className="myRanking__left--now colorWhite">순위 없음</p>
+          <p className="myRanking__left--now colorWhite myRanking__left--text">순위 없음</p>
           <p className="rankingChange">
             <span></span>
           </p>
