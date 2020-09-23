6109601e8b791cd7382e5c18ef3371b0f35f29cf
gaengkun <gaengkun@gmail.com>
2020-09-23 09:36:29 +0900

    검색 noresult 센터 정렬 수정

--- web/src/pages/main/component/NoResult.js
+++ web/src/pages/main/component/NoResult.js
@@ -4,31 +4,32 @@ import styled from 'styled-components'
 // static
 import NoResultIcon from 'components/ui/ic_noResult.svg'
 
-export default props => {
+export default (props) => {
   useEffect(() => {}, [])
 
   return (
     <NoResult>
       <img src={NoResultIcon} />
-      <div className="text">조회 된 결과가 없습니다.</div>
+      <div className="text">
+        조회 된 결과가
+        <br /> 없습니다.
+      </div>
     </NoResult>
   )
 }
 
 const NoResult = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+  width: 100%;
   padding: 100px 0;
 
   img {
     display: block;
+    margin: auto;
   }
 
   .text {
+    width: 100%;
     margin-top: 9.7px;
-    width: 110px;
     text-align: center;
     color: #757575;
     word-break: keep-all;
