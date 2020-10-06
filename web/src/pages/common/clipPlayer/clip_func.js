874f8b370c71ff8a5fdb9007ac6c4c5796b251be
leeeunbee <herbione@inforex.co.kr>
2020-10-06 17:31:43 +0900

    클립 입장

--- web/src/pages/common/clipPlayer/clip_func.js
+++ web/src/pages/common/clipPlayer/clip_func.js
@@ -38,10 +38,12 @@ export const clipJoin = (data, context, webview) => {
       // clipExit(context)
       // context.action.alert({visible: false})
       // sessionStorage.setItem('clip_active', 'N')
-      let prevClipNo = JSON.parse(Utility.getCookie('clip-player-info'))
-      prevClipNo = prevClipNo.clipNo
-      if (prevClipNo === data.clipNo) {
-        return Hybrid('ClipPlayerJoin', data)
+      if (Utility.getCookie('clip-player-info') !== undefined) {
+        let prevClipNo = JSON.parse(Utility.getCookie('clip-player-info'))
+        prevClipNo = prevClipNo.clipNo
+        if (prevClipNo === data.clipNo) {
+          return Hybrid('ClipPlayerJoin', data)
+        }
       } else {
         clipExit(context)
         return Hybrid('ClipPlayerJoin', data)
