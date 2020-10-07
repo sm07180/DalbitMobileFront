6278b8793dd848fc39f08e5f0f6f6d7df61bb014
leeeunbee <herbione@inforex.co.kr>
2020-10-07 14:06:46 +0900

    클립입장 원복

--- web/src/pages/common/clipPlayer/clip_func.js
+++ web/src/pages/common/clipPlayer/clip_func.js
@@ -4,25 +4,9 @@ import {Hybrid, NewHybrid} from 'context/hybrid'
 // etc
 import Utility from 'components/lib/utility'
 import {OS_TYPE} from 'context/config.js'
-import Api from 'context/api'
 
 export const clipJoin = (data, context, webview) => {
-  if (
-    Utility.getCookie('listen_room_no') === undefined ||
-    Utility.getCookie('listen_room_no') === 'null' ||
-    Utility.getCookie('listen_room_no') === ''
-  ) {
-    console.log(sessionStorage.getItem('clip_active'))
-    if (sessionStorage.getItem('clip_active') === 'N') {
-      console.log('중복클릭')
-      context.action.alert({
-        msg: '클립 재생중입니다.\n 잠시만 기다려주세요.'
-      })
-      return false
-    } else {
-      sessionStorage.setItem('clip_active', 'N')
-    }
-
+  if (Utility.getCookie('listen_room_no') === undefined || Utility.getCookie('listen_room_no') === 'null') {
     if (webview === 'new') {
       let prevClipNo = JSON.parse(Utility.getCookie('clip-player-info'))
       prevClipNo = prevClipNo.clipNo
@@ -32,48 +16,25 @@ export const clipJoin = (data, context, webview) => {
         if (context.customHeader['os'] === OS_TYPE['IOS']) {
           return Hybrid('ClipPlayerJoin', data)
         } else {
+          // return NewHybrid('ClipPlay', webview, data)
           return Hybrid('ClipPlayerJoin', data)
         }
       }
     } else {
-      clipExit(context)
-      context.action.alert({visible: false})
       return Hybrid('ClipPlayerJoin', data)
-      // if (Utility.getCookie('clip-player-info') !== undefined) {
-      //   let prevClipNo = JSON.parse(Utility.getCookie('clip-player-info'))
-      //   prevClipNo = prevClipNo.clipNo
-      //   if (prevClipNo === data.clipNo) {
-      //     context.action.alert({visible: false})
-      //     return Hybrid('ClipPlayerJoin', data)
-      //   } else {
-      //     clipExit(context)
-      //     context.action.alert({visible: false})
-      //     return Hybrid('ClipPlayerJoin', data)
-      //   }
-      // } else {
-      //   clipExit(context)
-      //   context.action.alert({visible: false})
-      //   return Hybrid('ClipPlayerJoin', data)
-      // }
     }
   } else {
-    if (webview === 'new') {
-      return context.action.alert({
-        msg: '현재 방송 청취 중입니다.'
-      })
-    } else {
-      return context.action.confirm({
-        msg: '현재 청취 중인 방송방이 있습니다.\n클립을 재생하시겠습니까?',
-        callback: () => {
-          clipExit(context)
-          sessionStorage.removeItem('room_no')
-          Utility.setCookie('listen_room_no', null)
-          Hybrid('ExitRoom', '')
-          context.action.updatePlayer(false)
-          clipJoin(data, context)
-        }
-      })
-    }
+    return context.action.confirm({
+      msg: '현재 청취 중인 방송방이 있습니다.\n클립을 재생하시겠습니까?',
+      callback: () => {
+        clipExit(context)
+        sessionStorage.removeItem('room_no')
+        Utility.setCookie('listen_room_no', null)
+        Hybrid('ExitRoom', '')
+        context.action.updatePlayer(false)
+        clipJoin(data, context)
+      }
+    })
   }
 }
 
@@ -83,7 +44,6 @@ export const clipExit = (context) => {
   context.action.updateClipState(null)
   context.action.updateClipPlayerState(null)
   context.action.updatePlayer(false)
-  sessionStorage.removeItem('clip_active')
 }
 
 export const updateClipInfo = (data) => {
