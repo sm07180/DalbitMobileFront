3595685a89335756c01c439ac604c494c72b41c5
황상한 <nmc2711@inforex.co.kr>
2020-10-08 15:37:46 +0900

    방송방내 클립노출 분기 수정

--- web/src/pages/mypage/index.js
+++ web/src/pages/mypage/index.js
@@ -113,7 +113,12 @@ export default (props) => {
       {type: 'fanboard', txt: '팬보드', component: FanBoard, icon: MenuFanBoardeIcon}
     ]
   }
-  if (__NODE_ENV === 'dev' && Utility.getCookie('listen_room_no') === undefined && !Utility.getCookie('listen_room_no')) {
+  if (
+    __NODE_ENV === 'dev' ||
+    (Utility.getCookie('listen_room_no') === undefined &&
+      !Utility.getCookie('listen_room_no') &&
+      Utility.getCookie('listen_room_no') === 'null')
+  ) {
     mypageNavList = [
       {type: 'notice', txt: '방송공지', component: Notice, icon: MenuNoticeIcon},
       {type: 'fanboard', txt: '팬보드', component: FanBoard, icon: MenuFanBoardeIcon},
