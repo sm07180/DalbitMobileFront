5be77d213e1bdf60071f6227b65612add2e45d83
leeeunbee <herbione@inforex.co.kr>
2020-10-08 14:26:26 +0900

    방송방 라이브버튼 로직 수정

--- web/src/pages/mypage/content/profile_detail.js
+++ web/src/pages/mypage/content/profile_detail.js
@@ -376,14 +376,15 @@ export default (props) => {
             className="liveIcon"
             onClick={() => {
               if (__NODE_ENV === 'dev') {
+                alert(webview)
+                alert(Utility.getCookie('listen_room_no'))
+                alert(webview === 'new' && Utility.getCookie('listen_room_no'))
+                if (webview === 'new' && Utility.getCookie('listen_room_no')) return false
                 return RoomJoin({roomNo: profile.roomNo})
               }
               if (webview === 'new' && Utility.getCookie('clip-player-info') && context.customHeader['os'] === OS_TYPE['IOS']) {
                 return context.action.alert({msg: `클립 종료 후 청취 가능합니다.\n다시 시도해주세요.`})
               } else {
-                alert(webview)
-                alert(Utility.getCookie('listen_room_no'))
-                alert(webview === 'new' && Utility.getCookie('listen_room_no'))
                 if (webview === 'new' && Utility.getCookie('listen_room_no')) return false
                 RoomJoin({roomNo: profile.roomNo})
               }
