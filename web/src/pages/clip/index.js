d4d49d1d7414ccb8661092eb41f67118c03fdb34
황상한 <nmc2711@inforex.co.kr>
2020-10-08 19:09:09 +0900

    클립메인 푸쉬

--- web/src/pages/clip/index.js
+++ web/src/pages/clip/index.js
@@ -331,14 +331,12 @@ export default (props) => {
   // initial category
   const refreshCategory = (type) => {
     // setClipTypeActive('')
-    // if (scrollY !== 0) {
-    //   window.scrollTo(0, scrollY)
-    // }
-    // context.action.updateClipSort(0)
-    //context.action.updateClipGender('')
+    if (scrollY !== 0) {
+      window.scrollTo(0, scrollY)
+    }
+
     if (context.clipRefresh && type === 'category') {
       context.action.updatClipRefresh(false)
-      // context.action.updateClipGender(false)
     } else if (type === 'popular') {
       if (popularList.length > 6) {
         let newList = popularList.filter(function (x) {
