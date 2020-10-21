8ddc47e9942277f1090a18ca818b832982fa40ec
leeeunbee <herbione@inforex.co.kr>
2020-10-21 10:20:09 +0900

    플레이리스트 클립펑션수정

--- web/src/pages/common/clipPlayer/clip_func.js
+++ web/src/pages/common/clipPlayer/clip_func.js
@@ -24,66 +24,66 @@ export const clipJoin = (data, context, webview) => {
       })
       totalData = {...totalData, list: playListClipNo}
       console.log(totalData)
+
+      if (Utility.getCookie('listen_room_no') === undefined || Utility.getCookie('listen_room_no') === 'null') {
+        if (webview === 'new') {
+          let prevClipNo = JSON.parse(Utility.getCookie('clip-player-info'))
+          prevClipNo = prevClipNo.clipNo
+          if (prevClipNo === data.clipNo) {
+            return Hybrid('CloseLayerPopup')
+          } else {
+            if (context.customHeader['os'] === OS_TYPE['IOS']) {
+              return Hybrid('ClipPlayerJoin', totalData)
+            } else {
+              // return NewHybrid('ClipPlay', webview, totalData)
+              return Hybrid('ClipPlayerJoin', totalData)
+            }
+          }
+        } else {
+          if (sessionStorage.getItem('listening') === 'Y') {
+            return context.action.alert({msg: '클립 재생 중 입니다.'})
+          }
+          sessionStorage.setItem('listening', 'Y')
+          let prevClipNo
+          if (Utility.getCookie('clip-player-info')) {
+            prevClipNo = JSON.parse(Utility.getCookie('clip-player-info'))
+            prevClipNo = prevClipNo.clipNo
+            // console.log(prevClipNo, data.clipNo)
+            if (prevClipNo === data.clipNo) {
+              // console.log('같은곡')
+              sessionStorage.setItem('listening', 'N')
+            } else {
+              // console.log('틀린곡')
+              sessionStorage.setItem('listening', 'Y')
+            }
+            // console.log('2' + sessionStorage.getItem('listening'))
+          }
+
+          return Hybrid('ClipPlayerJoin', totalData)
+        }
+      } else {
+        if (webview === 'new') {
+          return context.action.alert({msg: '방송 종료 후 청취 가능합니다. \n다시 시도해주세요.'})
+        } else {
+          return context.action.confirm({
+            msg: '현재 청취 중인 방송방이 있습니다.\n클립을 재생하시겠습니까?',
+            callback: () => {
+              clipExit(context)
+              sessionStorage.removeItem('room_no')
+              Utility.setCookie('listen_room_no', null)
+              Hybrid('ExitRoom', '')
+              context.action.updatePlayer(false)
+              clipJoin(data, context)
+            }
+          })
+        }
+      }
     } else {
       context.action.alert({msg: message})
     }
   }
 
   fetchPlayList()
-
-  if (Utility.getCookie('listen_room_no') === undefined || Utility.getCookie('listen_room_no') === 'null') {
-    if (webview === 'new') {
-      let prevClipNo = JSON.parse(Utility.getCookie('clip-player-info'))
-      prevClipNo = prevClipNo.clipNo
-      if (prevClipNo === data.clipNo) {
-        return Hybrid('CloseLayerPopup')
-      } else {
-        if (context.customHeader['os'] === OS_TYPE['IOS']) {
-          return Hybrid('ClipPlayerJoin', data)
-        } else {
-          // return NewHybrid('ClipPlay', webview, data)
-          return Hybrid('ClipPlayerJoin', data)
-        }
-      }
-    } else {
-      if (sessionStorage.getItem('listening') === 'Y') {
-        return context.action.alert({msg: '클립 재생 중 입니다.'})
-      }
-      sessionStorage.setItem('listening', 'Y')
-      let prevClipNo
-      if (Utility.getCookie('clip-player-info')) {
-        prevClipNo = JSON.parse(Utility.getCookie('clip-player-info'))
-        prevClipNo = prevClipNo.clipNo
-        // console.log(prevClipNo, data.clipNo)
-        if (prevClipNo === data.clipNo) {
-          // console.log('같은곡')
-          sessionStorage.setItem('listening', 'N')
-        } else {
-          // console.log('틀린곡')
-          sessionStorage.setItem('listening', 'Y')
-        }
-        // console.log('2' + sessionStorage.getItem('listening'))
-      }
-
-      return Hybrid('ClipPlayerJoin', data)
-    }
-  } else {
-    if (webview === 'new') {
-      return context.action.alert({msg: '방송 종료 후 청취 가능합니다. \n다시 시도해주세요.'})
-    } else {
-      return context.action.confirm({
-        msg: '현재 청취 중인 방송방이 있습니다.\n클립을 재생하시겠습니까?',
-        callback: () => {
-          clipExit(context)
-          sessionStorage.removeItem('room_no')
-          Utility.setCookie('listen_room_no', null)
-          Hybrid('ExitRoom', '')
-          context.action.updatePlayer(false)
-          clipJoin(data, context)
-        }
-      })
-    }
-  }
 }
 
 export const clipExit = (context) => {
@@ -95,5 +95,5 @@ export const clipExit = (context) => {
 }
 
 export const updateClipInfo = (data) => {
-  Hybrid('ClipUpdateInfo', data)
+  Hybrid('ClipUpdateInfo', totalData)
 }
