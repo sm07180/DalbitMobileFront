b9de96557725cddcd02c5e71a5b468856c735b44
황상한 <nmc2711@inforex.co.kr>
2020-10-26 16:36:28 +0900

    클립메인 최신 클립 수정

--- web/src/pages/clip/index.js
+++ web/src/pages/clip/index.js
@@ -295,10 +295,10 @@ export default (props) => {
 
   const makeTop3List = () => {
     return Object.values(listTop3).map((item, idx) => {
-      if (item.length === 0) return null
+      if (item.length < 3) return <React.Fragment key={idx}></React.Fragment>
       let subjectMap = item[0].subjectType
       return (
-        <div className="slideWrap" key={idx} style={{display: item.length !== 3 ? 'none' : 'block'}}>
+        <div className="slideWrap" key={idx}>
           {clipType.map((item, idx) => {
             const {cdNm, value} = item
             if (subjectMap === value) {
