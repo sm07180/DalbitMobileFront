214827f9cb5a3f5d0c7a938a5675c135f02f437d
leeeunbee <herbione@inforex.co.kr>
2020-11-05 10:53:40 +0900

    클립 빌드분기수정

--- web/src/pages/common/clipPlayer/clip_func.js
+++ web/src/pages/common/clipPlayer/clip_func.js
@@ -44,8 +44,15 @@ export const clipJoin = (data, context, webview, isPush) => {
       playListData: {type: currentTyep, param: encodeURIComponent(url), isPush: isPush === 'push' ? true : false}
     }
   }
+
+  if (
+    (context.customHeader['os'] === OS_TYPE['IOS'] && context.customHeader['appBuild'] < 207) ||
+    (context.customHeader['os'] === OS_TYPE['Android'] && context.customHeader['appBuild'] < 39)
+  ) {
+    totalData = {...data}
+  }
+
   console.log('totalData', totalData)
-  // alert(JSON.stringify(totalData))
 
   if (Utility.getCookie('listen_room_no') === undefined || Utility.getCookie('listen_room_no') === 'null') {
     // alert(webview)
@@ -56,10 +63,10 @@ export const clipJoin = (data, context, webview, isPush) => {
         return Hybrid('CloseLayerPopup')
       } else {
         if (context.customHeader['os'] === OS_TYPE['IOS']) {
-          return Hybrid('ClipPlayerJoin', data)
+          return Hybrid('ClipPlayerJoin', totalData)
         } else {
           // return NewHybrid('ClipPlay', webview, data)
-          return Hybrid('ClipPlayerJoin', data)
+          return Hybrid('ClipPlayerJoin', totalData)
         }
       }
     } else {
@@ -82,7 +89,7 @@ export const clipJoin = (data, context, webview, isPush) => {
         // console.log('2' + sessionStorage.getItem('listening'))
       }
       if (context.customHeader['os'] === OS_TYPE['IOS']) {
-        Hybrid('ClipPlayerJoin', data)
+        Hybrid('ClipPlayerJoin', totalData)
         // if (timer) {
         //   clearTimeout(timer)
         // }
@@ -90,7 +97,7 @@ export const clipJoin = (data, context, webview, isPush) => {
         //   return Hybrid('ClipPlayerJoin', totalData)
         // }, 400)
       } else {
-        return Hybrid('ClipPlayerJoin', data)
+        return Hybrid('ClipPlayerJoin', totalData)
       }
     }
   } else {
