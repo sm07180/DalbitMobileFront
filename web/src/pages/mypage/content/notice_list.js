110939d06d1698f82eefc37ae142fc988ad8a58a
gaengkun <gaengkun@gmail.com>
2020-11-10 14:48:22 +0900

    달빛 크롭퍼 타이틀 진행중

--- web/src/pages/mypage/content/notice_list.js
+++ web/src/pages/mypage/content/notice_list.js
@@ -2,6 +2,8 @@ import React, {useContext, useMemo} from 'react'
 import {PHOTO_SERVER} from 'context/config'
 import {Context} from 'context'
 
+import styled from 'styled-components'
+
 //date format
 import Utility from 'components/lib/utility'
 
@@ -13,56 +15,66 @@ const NoticeList = (props) => {
 
   return (
     <>
-      {noticeList !== null &&
-        noticeList.map((item, index) => (
-          <>
-            {item.isTop === true ? (
-              <ul className="noticeList NoticeBookmark">
-                <li
-                  className={`noticeItme`}
-                  key={index}
-                  onClick={() => {
-                    setIsDetaile(true)
-                    setIsList(false)
-                    if (item.noticeIdx === detailIdx) {
-                      setDetailIdx(0)
-                    } else {
-                      setDetailIdx(item.noticeIdx)
-                    }
-                  }}>
-                  {item.imagePath ? <img src={`${PHOTO_SERVER}${item.imagePath}`} className="noticeItme__img" /> : ''}
-                  <div className="noticeItme__textWrap">
-                    <strong className="noticeItme__title">{item.title}</strong>
-                    <p className="noticeItme__date">{Utility.timeFormat(item.writeDt)}</p>
-                  </div>
-                  {(IntTime - item.writeTs) / 3600 < 7 && <i className="noticeItme__moreIcon">필독</i>}
-                </li>
-              </ul>
-            ) : (
-              <ul className="noticeList">
-                <li
-                  className={`noticeItme`}
-                  key={index}
-                  onClick={() => {
-                    setIsDetaile(true)
-                    setIsList(false)
-                    if (item.noticeIdx === detailIdx) {
-                      setDetailIdx(0)
-                    } else {
-                      setDetailIdx(item.noticeIdx)
-                    }
-                  }}>
-                  {item.imagePath ? <img src={`${PHOTO_SERVER}${item.imagePath}`} className="noticeItme__img" /> : ''}
-                  <div className="noticeItme__textWrap">
-                    <strong className="noticeItme__title">{item.title}</strong>
-                    <p className="noticeItme__date">{Utility.timeFormat(item.writeDt)}</p>
-                  </div>
-                  {(IntTime - item.writeTs) / 3600 < 7 && <i className="noticeItme__moreIcon">필독</i>}
-                </li>
-              </ul>
-            )}
-          </>
-        ))}
+      <ul className="noticeList bookmark">
+        {noticeList !== null &&
+          noticeList.map((item, index) => {
+            return (
+              <>
+                {item.isTop === true && (
+                  <li
+                    className="noticeItme"
+                    key={index}
+                    onClick={() => {
+                      setIsDetaile(true)
+                      setIsList(false)
+                      if (item.noticeIdx === detailIdx) {
+                        setDetailIdx(0)
+                      } else {
+                        setDetailIdx(item.noticeIdx)
+                      }
+                    }}>
+                    {item.imagePath ? <img src={`${PHOTO_SERVER}${item.imagePath}`} className="noticeItme__img" /> : ''}
+                    <div className="noticeItme__textWrap">
+                      <strong className="noticeItme__title">{item.title}</strong>
+                      <p className="noticeItme__date">{Utility.timeFormat(item.writeDt)}</p>
+                    </div>
+                    {(IntTime - item.writeTs) / 3600 < 7 && <i className="noticeItme__moreIcon">새글</i>}
+                  </li>
+                )}
+              </>
+            )
+          })}
+      </ul>
+      <ul className="noticeList">
+        {noticeList !== null &&
+          noticeList.map((item, index) => {
+            return (
+              <>
+                {item.isTop === false && (
+                  <li
+                    className="noticeItme"
+                    key={index}
+                    onClick={() => {
+                      setIsDetaile(true)
+                      setIsList(false)
+                      if (item.noticeIdx === detailIdx) {
+                        setDetailIdx(0)
+                      } else {
+                        setDetailIdx(item.noticeIdx)
+                      }
+                    }}>
+                    {item.imagePath ? <img src={`${PHOTO_SERVER}${item.imagePath}`} className="noticeItme__img" /> : ''}
+                    <div className="noticeItme__textWrap">
+                      <strong className="noticeItme__title">{item.title}</strong>
+                      <p className="noticeItme__date">{Utility.timeFormat(item.writeDt)}</p>
+                    </div>
+                    {(IntTime - item.writeTs) / 3600 < 7 && <i className="noticeItme__moreIcon">새글</i>}
+                  </li>
+                )}
+              </>
+            )
+          })}
+      </ul>
     </>
   )
 }
