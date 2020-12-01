bbbb7841825070f4b0372275111d429a4347cf70
Jaeoh Kim <jaeohkim7@gmail.com>
2020-12-01 16:30:11 +0900

    2020-12-01 kjo 방송사연 수정

--- web/src/pages/story/content/detail.js
+++ web/src/pages/story/content/detail.js
@@ -2,27 +2,69 @@ import React, {useEffect, useState} from 'react'
 import {useParams} from 'react-router-dom'
 import Api from 'context/api'
 import Utility from 'components/lib/utility'
+
+let timer
+let totalPage = 1
 export default () => {
   const {roomNo} = useParams()
   const [storyList, setStoryList] = useState([])
 
+  const [currentPage, setCurrentPage] = useState(1)
+  const [totalCnt, setTotalCnt] = useState(0)
+
   const fetchStory = async () => {
-    const {result, data} = await Api.getStory({roomNo})
+    const {result, data} = await Api.getStory({roomNo, page: currentPage, records: 10})
     if (result === 'success') {
-      const {list} = data
-      setStoryList(list)
+      const {list, paging} = data
+      if (currentPage > 1) {
+        setStoryList(storyList.concat(list))
+      } else {
+        setStoryList(list)
+      }
+
+      if (paging) {
+        totalPage = paging.totalPage
+        setTotalCnt(paging.total)
+      }
     }
   }
 
   useEffect(() => {
     fetchStory()
-  }, [])
+  }, [currentPage])
+
+  useEffect(() => {
+    let fetching = false
+    const scrollEvtHdr = () => {
+      if (timer) window.clearTimeout(timer)
+      timer = window.setTimeout(function () {
+        //스크롤
+        const windowHeight = 'innerHeight' in window ? window.innerHeight : document.documentElement.offsetHeight
+        const body = document.body
+        const html = document.documentElement
+        const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight)
+        const windowBottom = windowHeight + window.pageYOffset
+        const diff = 400
+        if (totalPage > currentPage && windowBottom >= docHeight - diff) {
+          if (!fetching) {
+            setCurrentPage(currentPage + 1)
+          }
+        }
+      }, 50)
+    }
+
+    window.addEventListener('scroll', scrollEvtHdr)
+    return () => {
+      window.removeEventListener('scroll', scrollEvtHdr)
+      fetching = true
+    }
+  }, [currentPage])
 
   return (
     <div className="detail-wrap">
       <div className="main-txt">
         <span className="label">사연</span>
-        <span className="number">{storyList.length > 0 ? storyList.length : '0'}</span>
+        <span className="number">{totalCnt}</span>
       </div>
 
       {storyList.map((story, idx) => {
