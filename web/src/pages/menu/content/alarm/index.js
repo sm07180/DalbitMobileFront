f2182c5b90811b909b083d4036afa5a48287b92f
Boram Lim <lbr@inforex.co.kr>
2021-01-22 16:28:46 +0900

    alarm push link update

--- web/src/pages/menu/content/alarm/index.js
+++ web/src/pages/menu/content/alarm/index.js
@@ -98,11 +98,7 @@ export default function Alert() {
         history.push(`/customer/notice/${roomNo}`)
         break
       case 31:
-        if (context.profile.memNo === memNo) {
-          history.push(`/mypage/${context.profile.memNo}/fanboard`)
-        } else {
-          history.push(`/mypage/${memNo}?tab=1`)
-        }
+        history.push(`/mypage/${context.profile.memNo}/fanboard`)
         break
       case 32:
         history.push(`/mypage/${context.profile.memNo}/wallet`)
