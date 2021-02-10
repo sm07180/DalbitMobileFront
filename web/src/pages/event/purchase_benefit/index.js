8b5683bf6a5347bae67f9e091e4f47244fc5fe7e
Boram Lim <lbr@inforex.co.kr>
2021-02-10 14:11:30 +0900

    달 구매혜택 이벤트 이미지 수정 및 구매버튼 추가

--- web/src/pages/event/purchase_benefit/index.js
+++ web/src/pages/event/purchase_benefit/index.js
@@ -1,7 +1,9 @@
 import React, {useState, useContext, useEffect} from 'react'
 import {useHistory} from 'react-router-dom'
-import {IMG_SERVER} from 'context/config'
+import Api from 'context/api'
+import {OS_TYPE} from 'context/config'
 import {Context} from 'context'
+import {StoreLink} from 'context/link'
 import styled from 'styled-components'
 // 이벤트 적용후 모바일에서 테스트! (방송방,클립 플레이어 적용 꼭확인)
 export default () => {
@@ -12,13 +14,26 @@ export default () => {
     if (!context.token.isLogin) history.push('/login?redirect=/event/purchaseBenefit')
   }, [context.token.isLogin])
 
+  const customHeader = JSON.parse(Api.customHeader)
+  const eventBtn = () => {
+    if (customHeader['os'] === OS_TYPE['IOS']) {
+      StoreLink(context, history)
+    } else {
+      history.push(`/pay/store`)
+    }
+  }
+
   return (
     <Content id="purchaseBenefit">
       <button className="btnBack" onClick={() => history.goBack()}>
         <img src="https://image.dalbitlive.com/svg/close_w_l.svg" alt="close" />
       </button>
-      <img src="https://image.dalbitlive.com/event/purchase_benefit/radio_view_open.png" alt="별, 달 추가 이벤트" />
+      <img src="https://image.dalbitlive.com/event/purchase_benefit/210210/radio_view_open.png" alt="별, 달 추가 이벤트" />
+      <button className="buy_btn" onClick={() => eventBtn()}>
+        <img src="https://image.dalbitlive.com/event/purchase_benefit/210210/buy_button.png" alt="스토어 가기" />
+      </button>
       <div className="notice">
+        <img src="https://image.dalbitlive.com/event/purchase_benefit/210210/radio_view_notice.png" alt="이벤트 유의사항" />
         <ul className="text">
           <li>본 이벤트는 예고없이 종료될 수 있습니다.</li>
           <li>추가로 드리는 달과 별은 내 지갑에서 확인할 수 있습니다.</li>
@@ -62,6 +77,10 @@ const Content = styled.div`
       right: 4px;
       width: 40px;
       height: 40px;
+    }
+    .buy_btn {
+      width: 100%;
+      padding: 5% 10%;
     }
   }
 `
