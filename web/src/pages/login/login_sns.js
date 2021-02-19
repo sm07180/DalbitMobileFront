41c0e5f51cad7fbda437239d5dcf941afe26c52a
Boram Lim <lbr@inforex.co.kr>
2021-02-19 12:59:55 +0900

    login 변경

--- web/src/pages/login/login_sns.js
+++ web/src/pages/login/login_sns.js
@@ -12,6 +12,7 @@ import facebookLogo from './static/facebook_logo.svg'
 import googleLogo from './static/google_logo.svg'
 import kakaoLogo from './static/kakao_logo.svg'
 import naverLogo from './static/naver_logo.svg'
+import phoneLogo from './static/phone_logo.svg'
 import logoW from './static/logo_w_no_symbol.svg'
 
 export default function login_sns({props}) {
@@ -22,6 +23,8 @@ export default function login_sns({props}) {
   const [loginPop, setLoginPop] = useState(false)
 
   const [appleAlert, setAppleAlert] = useState(false)
+  const [moreSection, setMoreSection] = useState(false)
+
   const customHeader = JSON.parse(Api.customHeader)
 
   const fetchSocialData = async (vendor) => {
@@ -55,65 +58,107 @@ export default function login_sns({props}) {
   }
 
   return (
-    <div className="loginForm">
-      <h1
-        onClick={() => {
-          if (!webview) {
-            window.location.href = '/'
-          }
-        }}>
-        {/* <img className="logo" src="https://image.dalbitlive.com/images/login/login_img.png" /> */}
-        <img src={logoW} alt="달빛라이브" />
-      </h1>
-
-      <button className="loginForm__btnHome" onClick={() => history.push('/')}>
+    <>
+      <button className="btnHome" onClick={() => history.push('/')}>
         둘러보기
       </button>
+      <div className="loginForm">
+        <h1
+          onClick={() => {
+            if (!webview) {
+              window.location.href = '/'
+            }
+          }}>
+          {/* <img className="logo" src="https://image.dalbitlive.com/images/login/login_img.png" /> */}
+          <img src={logoW} alt="달빛라이브" />
+        </h1>
+        <p className="loginForm__msg">
+          레벨업하면 <span>최대 10,000원</span>
+          <br />
+          상당의 달 지급
+        </p>
 
-      <p className="loginForm__msg">
-        레벨업하면 <span>최대 10,000원</span>
-        <br />
-        상당의 달 지급
-      </p>
+        {(globalCtx.nativeTid == '' || globalCtx.nativeTid == 'init') && (
+          <div className="socialLogin">
+            {moreSection === false &&
+              (customHeader['os'] === OS_TYPE['IOS'] ? (
+                <>
+                  <button className="social-kakao-btn" onClick={() => fetchSocialData('kakao')}>
+                    <img className="icon" src={kakaoLogo} /> <span>카카오로 계속하기</span>
+                  </button>
+                  <button className="social-apple-btn" onClick={() => fetchSocialData('apple')}>
+                    <img className="icon" src={appleLogo} /> <span>Apple로 계속하기</span>
+                  </button>
+                </>
+              ) : (
+                <>
+                  <button className="social-kakao-btn" onClick={() => fetchSocialData('kakao')}>
+                    <img className="icon" src={kakaoLogo} /> <span>카카오로 계속하기</span>
+                  </button>
+                  <button className="social-google-btn" onClick={() => fetchSocialData('google')}>
+                    <img className="icon" src={googleLogo} /> <span>구글로 계속하기</span>
+                  </button>
+                </>
+              ))}
+            {moreSection === true && (
+              <div className={`moreAnimation ${customHeader['os'] === OS_TYPE.IOS && 'ios'}`}>
+                <button className="social-kakao-btn" onClick={() => fetchSocialData('kakao')}>
+                  <img className="icon" src={kakaoLogo} /> <span>카카오로 계속하기</span>
+                </button>
+                {customHeader['os'] === OS_TYPE.IOS ? (
+                  <>
+                    <button className="social-apple-btn" onClick={() => fetchSocialData('apple')}>
+                      <img className="icon" src={appleLogo} /> <span>Apple로 계속하기</span>
+                    </button>
+                    <button className="social-google-btn" onClick={() => fetchSocialData('google')}>
+                      <img className="icon" src={googleLogo} /> <span>구글로 계속하기</span>
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <button className="social-google-btn" onClick={() => fetchSocialData('google')}>
+                      <img className="icon" src={googleLogo} /> <span>구글로 계속하기</span>
+                    </button>
+                    <button className="social-apple-btn" onClick={() => fetchSocialData('apple')}>
+                      <img className="icon" src={appleLogo} /> <span>Apple로 계속하기</span>
+                    </button>
+                  </>
+                )}
 
-      {(globalCtx.nativeTid == '' || globalCtx.nativeTid == 'init') && (
-        <div className="socialLogin">
-          <button className="social-kakao-btn" onClick={() => fetchSocialData('kakao')}>
-            <img className="icon" src={kakaoLogo} /> <span>카카오로 계속하기</span>
-          </button>
-          <button className="social-naver-btn" onClick={() => fetchSocialData('naver')}>
-            <img className="icon" src={naverLogo} /> <span>네이버로 계속하기</span>
-          </button>
-          <button className="social-google-btn" onClick={() => fetchSocialData('google')}>
-            <img className="icon" src={googleLogo} /> <span>구글로 계속하기</span>
-          </button>
-          <button className="social-facebook-btn" onClick={() => fetchSocialData('facebook')}>
-            <img className="icon" src={facebookLogo} /> <span>페이스북으로 계속하기</span>
-          </button>
+                <button className="social-naver-btn" onClick={() => fetchSocialData('naver')}>
+                  <img className="icon" src={naverLogo} /> <span>네이버로 계속하기</span>
+                </button>
+                <button className="social-facebook-btn" onClick={() => fetchSocialData('facebook')}>
+                  <img className="icon" src={facebookLogo} /> <span>페이스북으로 계속하기</span>
+                </button>
 
-          <button className="social-apple-btn" onClick={() => fetchSocialData('apple')}>
-            <img className="icon" src={appleLogo} /> <span>Apple로 계속하기</span>
-          </button>
+                <button className="social-phone-btn" onClick={() => setLoginPop(true)}>
+                  <img className="icon" src={phoneLogo} /> <span>전화번호로 계속하기</span>
+                </button>
+              </div>
+            )}
+            {moreSection === false && (
+              <button className="social-more-btn" onClick={() => setMoreSection(true)}>
+                <span>다른 방법으로 계속하기</span>
+              </button>
+            )}
 
-          {appleAlert && <div className="apple-alert">OS를 최신 버전으로 설치해주세요.</div>}
+            {appleAlert && <div className="apple-alert">OS를 최신 버전으로 설치해주세요.</div>}
+          </div>
+        )}
 
-          <button className="social-phone-btn" onClick={() => history.push('/signup')}>
-            <span>전화번호로 가입하기</span>
-          </button>
+        <div className="link-wrap">
+          <span onClick={() => history.push('/signup')}>
+            <div className="link-text">회원가입</div>
+          </span>
+          <div className="bar" />
+          <a href="/service">
+            <div className="link-text yellow">고객센터</div>
+          </a>
         </div>
-      )}
 
-      <div className="link-wrap">
-        <span onClick={() => setLoginPop(true)}>
-          <div className="link-text">전화번호 로그인</div>
-        </span>
-        <div className="bar" />
-        <a href="/service">
-          <div className="link-text yellow">고객센터</div>
-        </a>
+        {loginPop && <LoginForm setLoginPop={setLoginPop} props={props} />}
       </div>
-
-      {loginPop && <LoginForm setLoginPop={setLoginPop} props={props} />}
-    </div>
+    </>
   )
 }
