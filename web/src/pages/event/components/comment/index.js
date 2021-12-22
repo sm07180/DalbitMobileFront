e8dcecf636f86fef5d4464638a292625c2082067
문형진 <772chosun@gmail.com>
2021-12-22 14:10:17 +0900

    트리 이벤트 수정

--- web/src/pages/event/components/comment/index.js
+++ web/src/pages/event/components/comment/index.js
@@ -9,18 +9,27 @@ import Utility from 'components/lib/utility'
 // static
 import NoResult from 'components/ui/new_noResult'
 import './comment.scss'
-import moment from "moment";
-
+import moment from 'moment'
 
 const EventComment = (props) => {
-  const { commentList, totalCommentCnt, commentAdd, commentRpt, commentDel, resetStoryList, contPlaceHolder, noResultMsg, maxLength } = props;
-  const globalCtx = useContext(Context);
-  const {token} = globalCtx;
-  const history = useHistory();
-  const contRef = useRef();
+  const {
+    commentList,
+    totalCommentCnt,
+    commentAdd,
+    commentRpt,
+    commentDel,
+    resetStoryList,
+    contPlaceHolder,
+    noResultMsg,
+    maxLength
+  } = props
+  const globalCtx = useContext(Context)
+  const {token} = globalCtx
+  const history = useHistory()
+  const contRef = useRef()
 
-  const [moreState, setMoreState] = useState(-1);
-  const [writeState, setWriteState] = useState(false);
+  const [moreState, setMoreState] = useState(-1)
+  const [writeState, setWriteState] = useState(false)
 
   const moreToggle = (boardIdx) => {
     if (boardIdx !== moreState) {
@@ -30,111 +39,119 @@ const EventComment = (props) => {
     }
   }
 
-
   // 사연 내용 validation
   const inputValueCheck = (e) => {
-    const value = e.currentTarget.value;
+    const value = e.currentTarget.value
 
     if (value.length > 0) {
       setWriteState(true)
     } else {
-      setWriteState(false);
+      setWriteState(false)
     }
 
     if (value.length >= maxLength) {
       globalCtx.action.toast({msg: `최대 ${maxLength}자 이내 입력 가능합니다.`})
-      return;
+      return
     }
-  };
+  }
 
   // 프로필 이동 이벤트
   const goProfile = (e) => {
-    const { targetMemNo } = e.currentTarget.dataset;
+    const {targetMemNo} = e.currentTarget.dataset
 
     if (targetMemNo !== undefined && targetMemNo > 0) {
       history.push(`/mypage/${targetMemNo}`)
     }
-  };
-  
+  }
+
   // 댓글 쓰기 이벤트
   const contAddEvent = () => {
     if (contRef !== undefined && contRef.current.value.length > 0) {
-      setMoreState(-1);
-      commentAdd(contRef.current.value);
-      contRef.current.value = '';
+      setMoreState(-1)
+      commentAdd(contRef.current.value)
+      contRef.current.value = ''
     } else {
-      globalCtx.action.toast({msg: '사연을 입력해주세요.'});
+      globalCtx.action.toast({msg: '사연을 입력해주세요.'})
     }
-  };
+  }
 
   // 댓글 삭제 이벤트
   const contDelEvent = (e) => {
-    const { targetNum } = e.currentTarget.dataset;
+    const {targetNum} = e.currentTarget.dataset
 
     if (targetNum !== undefined) {
       globalCtx.action.confirm({
         callback: () => {
-          setMoreState(-1);
-          commentDel(targetNum);
+          setMoreState(-1)
+          commentDel(targetNum)
         },
         msg: '해당 사연을 삭제하시겠습니까?\n'
-      });
+      })
     }
-  };
+  }
 
   // 댓글 신고 이벤트
   const contRptEvent = (e) => {
-    const { targetNum } = e.currentTarget.dataset;
+    const {targetNum} = e.currentTarget.dataset
 
     if (targetNum !== undefined) {
       globalCtx.action.confirm({
         callback: () => {
-          setMoreState(-1);
-          commentRpt(targetNum);
+          setMoreState(-1)
+          commentRpt(targetNum)
         },
         msg: '해당 사연을 신고하시겠습니까?\n'
-      });
+      })
     }
   }
 
   // 새로고침 이벤트
   const refreshList = (e) => {
-    const target = e.currentTarget;
+    const target = e.currentTarget
     if (target !== undefined) {
-      resetStoryList();
+      resetStoryList()
       target.classList.remove('on')
-      setTimeout(() => { target.classList.add('on') }, 0);
+      setTimeout(() => {
+        target.classList.add('on')
+      }, 0)
     }
   }
 
   // 시간값 계산 함수
   const getFormatting = (data) => {
-    const targetTime = moment(data);
-    const duration = moment.duration(moment().diff(targetTime));
+    const targetTime = moment(data)
+    const duration = moment.duration(moment().diff(targetTime))
 
     if (duration.days() > 0) {
-      return targetTime.format('YYYY-MM-DD');
+      return targetTime.format('YYYY-MM-DD')
     } else if (duration.hours() > 0) {
-      return `${duration.hours()} 시간 전`;
+      return `${duration.hours()} 시간 전`
     } else if (duration.minutes() > 0) {
-      return `${duration.minutes()} 분 전`;
+      return `${duration.minutes()} 분 전`
     } else {
-      return `${duration.seconds()} 초 전`;
+      return `${duration.seconds()} 초 전`
     }
-  };
+  }
 
   return (
     <div className="commentEventWrap">
-      {globalCtx.token.isLogin &&
+      {globalCtx.token.isLogin && (
         <div className="addInputBox">
           <div className="userBox">
-            <div className="photo"><img src={globalCtx.profile.profImg.thumb62x62} alt={globalCtx.profile.nickNm}/></div>
+            <div className="photo">
+              <img src={globalCtx.profile.profImg.thumb62x62} alt={globalCtx.profile.nickNm} />
+            </div>
             <div className="userNick">{globalCtx.profile.nickNm}</div>
           </div>
-          <textarea placeholder={contPlaceHolder} ref={contRef} onChange={inputValueCheck} maxLength={100}/>
-          <button className={`writeBtn ${writeState ? 'on' : ''}`} onClick={contAddEvent}>등록</button>
+          <textarea placeholder={contPlaceHolder} ref={contRef} onChange={inputValueCheck} maxLength={100} />
+          <div className="textCount">
+            <strong>50</strong>/100
+          </div>
+          <button className={`writeBtn ${writeState ? 'on' : ''}`} onClick={contAddEvent}>
+            등록하기
+          </button>
         </div>
-      }
+      )}
       <div className="commentBox">
         <div className="totalBox">
           댓글 <span>{`${totalCommentCnt}`}</span>개
@@ -152,7 +169,12 @@ const EventComment = (props) => {
                 return (
                   <div className="listItem" key={`comment-${idx}`}>
                     <div className="thumb" onClick={goProfile} data-target-mem-no={tail_mem_no}>
-                      <img src={`${PHOTO_SERVER + (image_profile !== '' ? `${image_profile}?120x120` : '/profile_3/profile_m_200327.jpg')}`} alt={mem_nick}/>
+                      <img
+                        src={`${
+                          PHOTO_SERVER + (image_profile !== '' ? `${image_profile}?120x120` : '/profile_3/profile_m_200327.jpg')
+                        }`}
+                        alt={mem_nick}
+                      />
                     </div>
                     <div className="textBox">
                       <div className="nick">
@@ -161,20 +183,31 @@ const EventComment = (props) => {
                       </div>
                       <p className="msg">{tail_conts}</p>
                     </div>
-                      <button className="btnMore" onClick={() => { moreToggle(idx) }}/>
-                      {moreState === idx && (
-                        <div className="moreList">
-                          {(globalCtx.adminChecker === true || parseInt(token.memNo) == tail_mem_no) ?
-                            <button data-target-num={tail_no} onClick={contDelEvent}>삭제</button>
-                            : <button data-target-num={tail_no} onClick={contRptEvent}>신고</button> }
-                        </div>
-                      )}
+                    <button
+                      className="btnMore"
+                      onClick={() => {
+                        moreToggle(idx)
+                      }}
+                    />
+                    {moreState === idx && (
+                      <div className="moreList">
+                        {globalCtx.adminChecker === true || parseInt(token.memNo) == tail_mem_no ? (
+                          <button data-target-num={tail_no} onClick={contDelEvent}>
+                            삭제하기
+                          </button>
+                        ) : (
+                          <button data-target-num={tail_no} onClick={contRptEvent}>
+                            신고하기
+                          </button>
+                        )}
+                      </div>
+                    )}
                   </div>
                 )
               })}
             </>
           ) : (
-            <NoResult type="default" text={noResultMsg}/>
+            <NoResult type="default" text={noResultMsg} />
           )}
         </div>
       </div>
@@ -186,7 +219,7 @@ EventComment.defaultProps = {
   contPlaceHolder: '댓글을 입력해주세요. (최대 300자)',
   noResultMsg: '아직 작성된 댓글이 없습니다.',
   commentList: [],
-  maxLength: 300,
-};
+  maxLength: 300
+}
 
-export default EventComment;
+export default EventComment
--- web/src/pages/event/tree/content/Lover.js
+++ web/src/pages/event/tree/content/Lover.js
@@ -1,36 +1,43 @@
-import React, {useEffect, useState, useCallback, useContext} from 'react';
-import {useHistory} from 'react-router-dom';
-import Utility, {isHitBottom, addComma} from 'components/lib/utility';
-import Api from 'context/api';
-import {Context} from 'context';
-import {IMG_SERVER, PHOTO_SERVER} from 'context/config';
-import moment from 'moment';
+import React, {useEffect, useState, useCallback, useContext} from 'react'
+import {useHistory} from 'react-router-dom'
+import Utility, {isHitBottom, addComma} from 'components/lib/utility'
+import Api from 'context/api'
+import {Context} from 'context'
+import {IMG_SERVER, PHOTO_SERVER} from 'context/config'
+import BadgeList from 'common/badge_list'
+import moment from 'moment'
 
 // 사랑꾼 선발대회 Content Component
 const Lover = (props) => {
   const context = useContext(Context)
-  const history = useHistory();
+  const history = useHistory()
   const [myRankInfo, setMyRankInfo] = useState() // 자신의 랭킹 정보
-  const [rankListInfo, setRankListInfo] = useState({cnt: 0, list: [], breakNo: 30, title: '실시간', subTitle: `(${moment().format('A HH:mm')} 기준)`}) // 랭킹 리스트 정보
-  const [pageInfo, setPageInfo] = useState({ seqNo: props.seqNo, pageNo: 1, pagePerCnt: 500 });
+  const [rankListInfo, setRankListInfo] = useState({
+    cnt: 0,
+    list: [],
+    breakNo: 30,
+    title: '실시간',
+    subTitle: `(${moment().format('A HH:mm')} 기준)`
+  }) // 랭킹 리스트 정보
+  const [pageInfo, setPageInfo] = useState({seqNo: props.seqNo, pageNo: 1, pagePerCnt: 500})
 
   // 사랑꾼 랭킹 리스트 가져오기
   const getRankListInfo = () => {
     Api.getLikeTreeRankList(pageInfo)
       .then((res) => {
         if (res.code === '00000') {
-          const current = moment();
-          let title = '실시간';
-          let subTitle = `(${current.format('A HH:mm')} 기준)`;
+          const current = moment()
+          let title = '실시간'
+          let subTitle = `(${current.format('A HH:mm')} 기준)`
 
           if (current.isBefore('2022-01-07') && current.isAfter('2021-12-30')) {
             if (pageInfo.seqNo === 1) {
-              title = '1회차';
-              subTitle = '';
+              title = '1회차'
+              subTitle = ''
             }
           }
 
-          setRankListInfo({ ...rankListInfo, ...res.data, title, subTitle });
+          setRankListInfo({...rankListInfo, ...res.data, title, subTitle})
         } else {
           console.log(res.code)
         }
@@ -43,7 +50,7 @@ const Lover = (props) => {
     Api.getLikeTreeRankUserInfo({memNo: context.profile.memNo, seqNo: pageInfo.seqNo})
       .then((res) => {
         if (res.code === '00000') {
-          setMyRankInfo(res.data);
+          setMyRankInfo(res.data)
         } else {
           console.log(res)
         }
@@ -53,70 +60,62 @@ const Lover = (props) => {
 
   // 랭킹 스크롤 이벤트
   const scrollAddList = () => {
-    const windowHeight =
-      "innerHeight" in window
-        ? window.innerHeight
-        : document.documentElement.offsetHeight;
-    const body = document.body;
-    const html = document.documentElement;
-    const docHeight = Math.max(
-      body.scrollHeight,
-      body.offsetHeight,
-      html.clientHeight,
-      html.scrollHeight,
-      html.offsetHeight
-    );
-    const windowBottom = windowHeight + window.pageYOffset;
+    const windowHeight = 'innerHeight' in window ? window.innerHeight : document.documentElement.offsetHeight
+    const body = document.body
+    const html = document.documentElement
+    const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight)
+    const windowBottom = windowHeight + window.pageYOffset
 
-    if (rankListInfo.list.length > rankListInfo.breakNo && (windowBottom >= docHeight - 300)) {;
-      setRankListInfo({...rankListInfo, breakNo: rankListInfo.breakNo + 1});
-      window.removeEventListener("scroll", scrollAddList);
+    if (rankListInfo.list.length > rankListInfo.breakNo && windowBottom >= docHeight - 300) {
+      setRankListInfo({...rankListInfo, breakNo: rankListInfo.breakNo + 1})
+      window.removeEventListener('scroll', scrollAddList)
     } else if (rankListInfo.breakNo >= rankListInfo.list.length) {
-      window.removeEventListener("scroll", scrollAddList);
+      window.removeEventListener('scroll', scrollAddList)
     }
-  };
+  }
 
   // 다음 실기간 랭킹
   const nextEvent = () => {
-    const current = moment();
-    if (pageInfo.seqNo !== 2) { // && current.isAfter('2021-12-30')
-      setPageInfo({ ...pageInfo, seqNo: 2 });
+    const current = moment()
+    if (pageInfo.seqNo !== 2) {
+      // && current.isAfter('2021-12-30')
+      setPageInfo({...pageInfo, seqNo: 2})
     }
-  };
+  }
 
   // 이전 실시간 랭킹
   const prevEvent = () => {
     if (pageInfo.seqNo !== 1) {
-      setPageInfo({ ...pageInfo, seqNo: 1 });
+      setPageInfo({...pageInfo, seqNo: 1})
     }
-  };
+  }
 
   // 프로필 이동 이벤트
   const goProfile = (e) => {
-    const { targetMemNo } = e.currentTarget.dataset;
+    const {targetMemNo} = e.currentTarget.dataset
 
     if (targetMemNo !== undefined && targetMemNo > 0) {
       history.push(`/mypage/${targetMemNo}`)
     }
-  };
+  }
 
   useEffect(() => {
     if (context.token.isLogin) {
       getRankUserInfo()
     }
-  }, [pageInfo.seqNo]);
+  }, [pageInfo.seqNo])
 
   useEffect(() => {
-    window.addEventListener("scroll", scrollAddList);
+    window.addEventListener('scroll', scrollAddList)
 
     return () => {
-      window.removeEventListener("scroll", scrollAddList);
+      window.removeEventListener('scroll', scrollAddList)
     }
-  }, [rankListInfo]);
+  }, [rankListInfo])
 
   useEffect(() => {
-    getRankListInfo();
-  }, [pageInfo]);
+    getRankListInfo()
+  }, [pageInfo])
 
   return (
     <>
@@ -133,9 +132,11 @@ const Lover = (props) => {
             <div className="titleWrap">{rankListInfo.title}</div>
             <span>{rankListInfo.subTitle}</span>
           </div>
-          <button className={`next ${(pageInfo.seqNo !== 2 && moment().isAfter('2021-12-30')) ? 'active' : 'noActive'}`} onClick={nextEvent}>
+          <button
+            className={`next ${pageInfo.seqNo !== 2 && moment().isAfter('2021-12-30') ? 'active' : 'noActive'}`}
+            onClick={nextEvent}>
             다음
-            <img src={`${IMG_SERVER}/event/tree/arrow.png`}/>
+            <img src={`${IMG_SERVER}/event/tree/arrow.png`} />
           </button>
         </div>
         {myRankInfo && (
@@ -159,7 +160,7 @@ const Lover = (props) => {
         )}
         <div className="rankUl">
           {rankListInfo.list.map((row, index) => {
-            if ( rankListInfo.breakNo <= index ) return;
+            if (rankListInfo.breakNo <= index) return
 
             return (
               <div className="rankList" key={index}>
@@ -174,7 +175,7 @@ const Lover = (props) => {
                   )}
                 </div>
                 <div className="listBox">
-                  <em className={`icon_wrap ${row.memSex === 'm' ? 'icon_male' : 'icon_female'}`} >
+                  <em className={`icon_wrap ${row.memSex === 'm' ? 'icon_male' : 'icon_female'}`}>
                     <span className="blind">성별</span>
                   </em>
                   <span className="userNick">{row.memNick}</span>
@@ -194,6 +195,6 @@ const Lover = (props) => {
 
 Lover.defaultProps = {
   seqNo: 1
-};
+}
 
 export default Lover
