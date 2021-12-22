a2623e140cb60fcf546ca430dc2facf9c5e217f7
문형진 <772chosun@gmail.com>
2021-12-22 11:18:18 +0900

    임시저장

--- web/src/pages/event/welcome/index.js
+++ web/src/pages/event/welcome/index.js
@@ -46,51 +46,103 @@ export default () => {
     <div id="welcome">
       <Header title="이벤트" />
       <img src={`${IMG_SERVER}/event/welcome/welcomeTop.png`} className="bgImg" />
-      <section className="tabContainer"></section>
-      <section className="step">
-        <div className="containerBox">
-          <div className="title">
-            <img src={`${IMG_SERVER}/event/welcome/stepTitleBj-1.png`} alt="step 1" />
-          </div>
-          <div className="giftUl">
-            <div className="giftList">
-              <img src={`${IMG_SERVER}/event/welcome/giftSample.png`} alt="" />
-              <p>10달</p>
+      <div className="tabContainer"></div>
+      <div className="step">
+        <section>
+          <div className="containerBox">
+            <div className="title">
+              <img src={`${IMG_SERVER}/event/welcome/stepTitleBj-1.png`} alt="step 1" />
             </div>
-            <div className="giftList">
-              <img src={`${IMG_SERVER}/event/welcome/giftSample.png`} alt="" />
-              <p>10달</p>
-            </div>
-            <div className="giftList">
-              <img src={`${IMG_SERVER}/event/welcome/giftSample.png`} alt="" />
-              <p>10달</p>
-            </div>
-          </div>
-          <div className="questUl">
-            <div className="questList">
-              <p className="questText">방송 진행 1시간</p>
-              <div className="gaugeOuter">
-                <div className="gaugeInner"></div>
+            <div className="giftUl">
+              <div className="giftList">
+                <img src={`${IMG_SERVER}/event/welcome/giftSample.png`} alt="" />
+                <p>10달</p>
               </div>
-              <p className="questCount">(30/60)</p>
-            </div>
-            <div className="questList">
-              <p className="questText">좋아요 받기 10회</p>
-              <div className="gaugeOuter">
-                <div className="gaugeInner"></div>
+              <div className="giftList">
+                <img src={`${IMG_SERVER}/event/welcome/giftSample.png`} alt="" />
+                <p>10달</p>
               </div>
-              <p className="questCount">(120/120)</p>
-            </div>
-            <div className="questList">
-              <p className="questText">방송 진행 1시간</p>
-              <div className="gaugeOuter">
-                <div className="gaugeInner"></div>
+              <div className="giftList">
+                <img src={`${IMG_SERVER}/event/welcome/giftSample.png`} alt="" />
+                <p>10달</p>
               </div>
-              <p className="questCount">(30/60)</p>
             </div>
+            <div className="questUl">
+              <div className="questList">
+                <p className="questText">방송 진행 1시간</p>
+                <div className="gaugeOuter">
+                  <div className="gaugeInner"></div>
+                </div>
+                <p className="questCount">(1/60)</p>
+              </div>
+              <div className="questList">
+                <p className="questText">좋아요 받기 10회</p>
+                <div className="gaugeOuter">
+                  <div className="gaugeInner"></div>
+                </div>
+                <p className="questCount complete">(120/120)</p>
+              </div>
+              <div className="questList">
+                <p className="questText">방송 진행 1시간</p>
+                <div className="gaugeOuter">
+                  <div className="gaugeInner"></div>
+                </div>
+                <p className="questCount">(59/60)</p>
+              </div>
+            </div>
+            <button className="giftBtn on">
+              <img src={`${IMG_SERVER}/event/welcome/giftBtn-on.png`} alt="선물 받기" />
+            </button>
           </div>
-        </div>
-      </section>
+        </section>
+        <section>
+          <div className="containerBox">
+            <div className="title">
+              <img src={`${IMG_SERVER}/event/welcome/stepTitleBj-2.png`} alt="step 1" />
+            </div>
+            <div className="giftUl">
+              <div className="giftList">
+                <img src={`${IMG_SERVER}/event/welcome/giftSample.png`} alt="" />
+                <p>10달</p>
+              </div>
+              <div className="giftList">
+                <img src={`${IMG_SERVER}/event/welcome/giftSample.png`} alt="" />
+                <p>10달</p>
+              </div>
+              <div className="giftList">
+                <img src={`${IMG_SERVER}/event/welcome/giftSample.png`} alt="" />
+                <p>10달</p>
+              </div>
+            </div>
+            <div className="questUl">
+              <div className="questList">
+                <p className="questText">방송 진행 1시간</p>
+                <div className="gaugeOuter">
+                  <div className="gaugeInner"></div>
+                </div>
+                <p className="questCount">(1/60)</p>
+              </div>
+              <div className="questList">
+                <p className="questText">좋아요 받기 10회</p>
+                <div className="gaugeOuter">
+                  <div className="gaugeInner"></div>
+                </div>
+                <p className="questCount complete">(120/120)</p>
+              </div>
+              <div className="questList">
+                <p className="questText">방송 진행 1시간</p>
+                <div className="gaugeOuter">
+                  <div className="gaugeInner"></div>
+                </div>
+                <p className="questCount">(59/60)</p>
+              </div>
+            </div>
+            <button className="giftBtn on">
+              <img src={`${IMG_SERVER}/event/welcome/giftBtn-on.png`} alt="선물 받기" />
+            </button>
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
