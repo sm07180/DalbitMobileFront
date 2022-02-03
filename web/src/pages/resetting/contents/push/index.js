a1a7ee51a1cf63853bdc7056cdffc027ed2e973f
박지호 <ghost1565@naver.com>
2022-02-03 09:36:13 +0900

    #설정리뉴얼 임시저장

--- web/src/pages/resetting/contents/push/index.js
+++ web/src/pages/resetting/contents/push/index.js
@@ -13,6 +13,11 @@ const SettingPush = () => {
   const tabList = ['무음','소리','진동'];
   const [tabType, setTabType] = useState(tabList[0])
 
+  const [alarmAll, setAlarmAll] = useState(false)
+  const [alarmMyStarBroadcast, setAlarmMyStarBroadcast] = useState(false)
+  const [alarmMyStarClip, setAlarmMyStarClip] = useState(false)
+  const [alarmMailbox, setAlarmMailbox] = useState(false)
+
   return (
     <>
       <Header position={'sticky'} title={'Push 알림 설정'} type={'back'}/>
@@ -33,7 +38,17 @@ const SettingPush = () => {
           </ul>
         </div>
         <div className='switchWrap'>
-            <SwitchList title={"전체 알림 수신"} mark={true} on={true}></SwitchList>
+            <SwitchList title={"전체 알림 수신"} on={alarmAll}></SwitchList>
+            <SwitchList title={"마이스타 방송 시작 알림"} mark={true} on={alarmMyStarBroadcast}></SwitchList>
+            <SwitchList title={"마이스타 클립 등록 알림"} mark={true} on={alarmMyStarClip}></SwitchList>
+            <SwitchList title={"우체통 알림"} mark={true} on={alarmMailbox}></SwitchList>
+            <SwitchList title={"내 클립 알림"} mark={true} on={false}></SwitchList>
+            <SwitchList title={"신규 팬 추가 알림"} mark={true} on={false}></SwitchList>
+            <SwitchList title={"팬보드 신규 글 등록 알림"} mark={true} on={false}></SwitchList>
+            <SwitchList title={"팬보드 댓글 등록 알림"} mark={true} on={false}></SwitchList>
+            <SwitchList title={"선물 도착 알림"} mark={true} on={false}></SwitchList>
+            <SwitchList title={"1:1 문의 답변 도착 알림"} mark={true} on={false}></SwitchList>
+            <SwitchList title={"알림받기 방송시작 알림"} mark={true} on={false}></SwitchList>
         </div>
       </div>
     </>
