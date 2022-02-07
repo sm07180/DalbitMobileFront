dbc7c2ca9b0d59db3fb0fbe4ee9a219c17a29c8e
박지호 <ghost1565@naver.com>
2022-02-07 18:29:20 +0900

    #임시저장

---
+++ web/src/pages/rehonor/contents/SpecialDj.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import DetailView from '../components/DetailView'
+// global components
+
+const SpecialDj = (props) => {
+
+  return (
+    <>
+      <DetailView/>
+      
+    </>
+  )
+}
+
+export default SpecialDj
