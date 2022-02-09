89608c7b8cabb37897983b5477d929590b0550b8
박지송 <jisong0292@gamil.com>
2022-02-09 16:42:02 +0900

    스토어페이지 디자인 수정

--- web/src/pages/store/contents/bankTransfer/bankTransfer.js
+++ web/src/pages/store/contents/bankTransfer/bankTransfer.js
@@ -130,8 +130,10 @@ const BankTransfer = () => {
             <li onClick={tabClick} className={userInfo.receiptCode === "b"  ? "active": ""} data-tab-target="b">지출증빙용</li>
           </ul>
         </div>
-        {userInfo.receiptCode === "i" && <InputItems><input type="number" name="receiptPhone" onChange={onChange} maxLength="20" placeholder="주민번호 또는 핸대폰번호를 입력해주세요."/></InputItems>}
-        {userInfo.receiptCode === "b" && <InputItems><input type="number" name="receiptBiz" onChange={onChange} maxLength="20" placeholder="사업자번호를 입력해주세요."/></InputItems>}
+        <div className="cashReceipt">
+          {userInfo.receiptCode === "i" && <InputItems><input type="number" name="receiptPhone" onChange={onChange} maxLength="20" placeholder="주민번호 또는 핸대폰번호를 입력해주세요."/></InputItems>}
+          {userInfo.receiptCode === "b" && <InputItems><input type="number" name="receiptBiz" onChange={onChange} maxLength="20" placeholder="사업자번호를 입력해주세요."/></InputItems>}
+        </div>
         <SubmitBtn text="입금계좌 받기" onClick={()=>clickDeposit()} />
       </section>
       <section className="noticeInfo">
