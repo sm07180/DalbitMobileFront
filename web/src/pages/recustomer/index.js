c5ffb48819443e4a157a90223c80d414c5594c09
박지호 <ghost1565@naver.com>
2022-02-09 12:09:50 +0900

    #임시저장

---
+++ web/src/pages/recustomer/index.js
@@ -0,0 +1,73 @@
+/**
+ * @file 모바일/customer/index.js
+ * @brief 고객센터
+ * @todo
+ */
+
+import React, {useState, useEffect, useContext} from 'react'
+import {useHistory, useParams} from 'react-router-dom'
+import {Context} from 'context'
+
+import Header from 'components/ui/header/Header'
+//Content
+import Faq from './contents/faq/Faq'
+import Question from './contents/question/Question'
+
+import './style.scss'
+
+const Customer = () => {
+  let history = useHistory()
+  const params = useParams();
+  const categoty = params.type;
+  const globalCtx = useContext(Context);
+  const {token, profile} = globalCtx;
+
+  const golink = (path) => {
+    history.push("/customer/" + path);
+  }
+
+  return (
+    <div id='customer'>
+      {
+        !categoty ?
+          <>
+            <Header position={'sticky'} type={'back'}/>
+            <div className='content'>
+              <div className='mainText'>
+                {!token.isLogin ? <span>달라에게</span> : <span><strong>{profile.nickNm}</strong>님,</span>}
+                <span>궁금한게 있으시다구요?</span>
+              </div>
+              <div className='subText'>
+                고객센터 <strong>전화문의</strong> 또는 <strong>1:1문의</strong>로 접수해주세요.<br/>
+                최대한 빨리 답변 드릴게요!
+              </div>
+              <div className='categotyWrap'>
+                <div className='categotyList'>
+                  <div className='categotyImg'></div>
+                  <div className='categotyName'>FAQ</div>
+                </div>
+                <div className='categotyList'>
+                  <div className='categotyImg'></div>
+                  <div className='categotyName'>운영정책</div>
+                </div>
+                <div className='categotyList'>
+                  <div className='categotyImg'></div>
+                  <div className='categotyName'>1:1문의</div>
+                </div>
+              </div>
+            </div>
+          </>
+        :
+        categoty === "faq" ?
+          <Faq/>
+        :
+        categoty === "question" ?
+          <Question/>
+        :
+          <></>
+      }
+    </div>
+  )
+}
+
+export default Customer
