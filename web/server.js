d0a3fd7b77437c79e8d17545a2d5817325cf8ea9
홍광표 <gonetpower@gmail.com>
2022-02-11 12:55:18 +0900

    server

--- web/server.js
+++ web/server.js
@@ -4,7 +4,7 @@ const https = require('https');
 const path = require('path');
 const bodyParser = require('body-parser')
 const compression = require('compression')
-
+const urlencode = require('urlencode');
 const ssl = {
     key: fs.readFileSync('./key/key.pem'),
     cert: fs.readFileSync('./key/cert.pem'),
@@ -15,7 +15,20 @@ app.use(compression());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use("/dist",express.static(path.join(__dirname, './dist')));
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, './dist/index.html'));
+    let indexHtml = fs.readFileSync('./dist/index.html','utf-8');
+    const customHeader = req.headers['custom-header'];
+    const redirecturl = req.headers['redirecturl'];
+    if(customHeader){
+        indexHtml = indexHtml.replace("#customHeader", urlencode.decode(customHeader))
+    }else{
+        indexHtml = indexHtml.replace("#customHeader", "")
+    }
+    if(redirecturl){
+        indexHtml = indexHtml.replace("#redirectUrl", urlencode.decode(redirecturl))
+    }else{
+        indexHtml = indexHtml.replace("#redirectUrl", "")
+    }
+    res.send(indexHtml);
 })
 https.createServer(ssl, app).listen(443, err => {
     if (err) throw err;
