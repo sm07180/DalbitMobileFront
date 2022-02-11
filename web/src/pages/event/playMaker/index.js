929926da4e14b0bca1458440c0b7959f65c92346
박지송 <jisong0292@gamil.com>
2022-02-12 08:55:59 +0900

    #7583 플레이메이커(구글폼추가)

--- web/src/pages/event/playMaker/index.js
+++ web/src/pages/event/playMaker/index.js
@@ -9,9 +9,9 @@ const PlayMaker = () =>{
     <div id="playMaker">
       <Header title={'이벤트'} type={'back'}/>
       <img src="https://image.dalbitlive.com/event/playMaker/main.png"/>
-      <button className="applyBtn">
+      <a className="applyBtn" href="https://docs.google.com/forms/d/e/1FAIpQLSecaCjYX_wmMEcikiB49WuHLIWCcmdHUjhyQ4AiF0_MI5ZDXA/viewform">
         <img src="https://image.dalbitlive.com/event/playMaker/applyBtn.png" alt="플레이메이커 지원하러 가기" />
-      </button>
+      </a>
     </div>
   )
 }
