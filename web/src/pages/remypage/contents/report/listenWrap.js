d94b4dccb6b25bb4cb67b4a8a54cbb448ca9e941
박지송 <jisong0292@gamil.com>
2022-02-16 09:29:15 +0900

    디자인 수정

--- web/src/pages/remypage/contents/report/listenWrap.js
+++ web/src/pages/remypage/contents/report/listenWrap.js
@@ -170,7 +170,7 @@ const listenWrap = () =>{
       </section>
 
       {bottomSlide &&
-      <PopSlide title="기간 설절" setPopSlide={setBottomSlide}>
+      <PopSlide title="기간 설정" setPopSlide={setBottomSlide}>
         <ReportTabMenu data={tabmenu} tab={tabType} setTab={setTabType} pickerPrev={pickerPrev} allDate={allDate} changeActive={changeActive}/>
         <InputItems>
           <DatePicker name="pickdata" value={dt.pickdataPrev} change={pickerPrev} changeActive={changeActive}/>
