fac30e647352ee96bf6ca66a71f3332145c200d8
박지호 <ghost1565@naver.com>
2022-02-16 10:00:45 +0900

    #회원탈퇴 페이지 추가

---
+++ web/src/pages/rerule/components/Checkbox.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+import {IMG_SERVER, WIDTH_PC, WIDTH_PC_S, WIDTH_TABLET, WIDTH_TABLET_S, WIDTH_MOBILE, WIDTH_MOBILE_S} from 'context/config'
+import Utility from 'components/lib/utility'
+
+import './checkbox.scss'
+
+const Checkbox = ({fnClick, fnChange, title = '', checked = false}) => (
+    <label>
+      <input
+        onClick={(e) => {
+          if (fnClick !== undefined) fnClick(e.target.checked)
+        }}
+        onChange={(e) => {
+          if (fnChange !== undefined) fnChange(e.target.checked)
+        }}
+        type="checkbox"
+        checked={checked}
+        className={checked ? 'on' : ''}
+      />
+      <div className="titlewrap" dangerouslySetInnerHTML={{__html: Utility.nl2br(title)}}></div>
+    </label>
+)
+export default Checkbox
