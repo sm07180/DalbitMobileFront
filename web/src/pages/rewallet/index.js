37c7160e64c92d321d8dfd9f761938954fa98230
박용훈 <ot9400@naver.com>
2022-02-18 14:01:05 +0900

    내지갑

--- web/src/pages/rewallet/index.js
+++ web/src/pages/rewallet/index.js
@@ -69,12 +69,19 @@ const WalletPage = (props) => {
           const byeolAndDal = walletType === walletTabMenu[0]?
             {dalTotCnt : listRes.data?.dalTotCnt}: {byeolTotCnt : listRes.data?.byeolTotCnt};
           setLastPage(Math.ceil(listRes.data.paging.total / pagePerCnt));
+
+          let popHistoryCnt = 0;
+          popRes.data?.list.map((v) => {
+            popHistoryCnt += v?.cnt;
+          })
+
           context.globalAction.dispatchWalletData({
             type: 'ADD_DATA',
             data: {
               ...byeolAndDal,
               listHistory: pageNo === 1 ? listRes.data?.list : walletData.listHistory.concat(listRes.data?.list),
               popHistory: popRes.data?.list,
+              popHistoryCnt
             }
           });
           setIsLoading(false);
@@ -83,7 +90,8 @@ const WalletPage = (props) => {
             type: 'ADD_HISTORY',
             data: {
               listHistory: [],
-              popHistory: popRes.data?.list
+              popHistory: popRes.data?.list,
+              popHistoryCnt: 0
             }
           });
         }
@@ -92,17 +100,17 @@ const WalletPage = (props) => {
   }
 
   //별 수치가 안맞음.
-  const getNewWallet = async () => {
-    const {result, data, message} = await Api.getMyPageNewWallet();
-
-    if(result === 'success'){
-      context.globalAction.dispatchWalletData({type:'ADD_DATA',
-        data:{
-          byeolTotCnt : data?.byeol,
-          dalTotCnt : data?.dal,
-      }});
-    }
-  };
+  // const getNewWallet = async () => {
+  //   const {result, data, message} = await Api.getMyPageNewWallet();
+  //
+  //   if(result === 'success'){
+  //     context.globalAction.dispatchWalletData({type:'ADD_DATA',
+  //       data:{
+  //         byeolTotCnt : data?.byeol,
+  //         dalTotCnt : data?.dal,
+  //     }});
+  //   }
+  // };
 
   //tab 이동
   const setTabType = (walletType) => {
@@ -135,19 +143,23 @@ const WalletPage = (props) => {
     if(toExchangeTab){
       setTabType(walletTabMenu[2]);
     }
+
+    return () => {
+      context.globalAction.dispatchWalletData({type:'INIT'});
+    };
   },[]);
 
   // 환전취소
-  async function cancelExchangeFetch() {
+  async function cancelExchangeFetch(exchangeIdx) {
     const {result, data, message} = await Api.postExchangeCancel({
-      exchangeIdx: 0 // 환전취소 글번호
+      exchangeIdx, // 환전취소 글번호
     })
     if (result === 'success') {
-      context.action.alert({
+      context.action.confirm({
         title: '환전 취소가 완료되었습니다.',
-        className: 'mobile',
         msg: message,
         callback: () => {
+          getWalletHistory(1, walletType === walletTabMenu[0]? 1 : 0);
         }
       })
     } else {
@@ -157,45 +169,6 @@ const WalletPage = (props) => {
         }
       })
     }
-  }
-
-  //ios 본인인증 체크
-  const checkSelfAuth = async () => {
-    //2020_10_12 환전눌렀을때 본인인증 나이 제한 없이 모두 가능
-    let myBirth
-    const baseYear = new Date().getFullYear() - 11
-    const myInfoRes = await Api.mypage()
-    if (myInfoRes.result === 'success') {
-      myBirth = myInfoRes.data.birth.slice(0, 4)
-    }
-
-    async function fetchSelfAuth() {
-      const res = await Api.self_auth_check({})
-      if (res.result === 'success') {
-        if (res.data.company === '기타') {
-          return context.action.alert({
-            msg: `휴대폰 본인인증을 받지 않은 경우\n환전이 제한되는 점 양해부탁드립니다`
-          })
-        }
-        const {parentsAgreeYn, adultYn} = res.data
-        if (parentsAgreeYn === 'n' && adultYn === 'n') return history.push('/selfauth_result')
-        if (myBirth > baseYear) {
-          return context.action.alert({
-            msg: `만 14세 미만 미성년자 회원은\n서비스 이용을 제한합니다.`
-          })
-        } else {
-          history.push('/money_exchange')
-        }
-      } else if (res.result === 'fail' && res.code === '0') {
-        history.push('/selfauth')
-      } else {
-        context.action.alert({
-          msg: res.message
-        })
-      }
-    }
-    fetchSelfAuth()
-    // history.push('/money_exchange')
   }
 
   return (
