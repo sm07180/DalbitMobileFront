f9c2333d6932bd23111f72a6dece6f4bb38dfcc8
문형진 <772chosun@gmail.com>
2022-02-21 10:28:40 +0900

    달라져스

---
+++ web/src/pages/event/rebranding/contents/round_1.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from 'react'
+import {IMG_SERVER} from 'context/config'
+import Utility from "components/lib/utility";
+// global components
+import BadgeItems from 'components/ui/badgeItems/BadgeItems'
+
+import '../style.scss'
+
+const Round_1 = (props) => {
+  // 
+
+  return (
+    <>
+      <section>
+        <img src={`${IMG_SERVER}/event/rebranding/gift-1.png`} alt="이벤트 상품 이미지" />
+      </section>
+      <section>
+        <img src={`${IMG_SERVER}/event/rebranding/detail.png`} alt="이벤트 유의사항 이미지" />
+      </section>
+    </>
+  )
+}
+
+export default Round_1
---
+++ web/src/pages/event/rebranding/index.js
@@ -0,0 +1,73 @@
+import React, {useState, useEffect} from 'react'
+import {IMG_SERVER} from 'context/config'
+import Utility from "components/lib/utility";
+// global components
+import Header from 'components/ui/header/Header'
+import BadgeItems from 'components/ui/badgeItems/BadgeItems'
+// contents
+import Round_1 from './contents/Round_1'
+// import Round_2 from './contents/Round_2'
+// import Round_3 from './contents/Round_3'
+
+import './style.scss'
+
+const Rebranding = (props) => {
+  // 
+
+  return (
+    <div id="rebranding">
+      <Header title="이벤트" type="back" />
+      <section>
+        <img src={`${IMG_SERVER}/event/rebranding/bg-1.png`} alt="이벤트 이미지" />
+      </section>
+      <section>
+        <img src={`${IMG_SERVER}/event/rebranding/bg-2.png`} alt="이벤트 이미지" />
+        <button>
+          <img src={`${IMG_SERVER}/event/rebranding/btn-1.png`} alt="버튼 이미지" />
+        </button>
+      </section>
+      <section>
+        <img src={`${IMG_SERVER}/event/rebranding/bg-3.png`} alt="이벤트 이미지" />
+        <button>
+          <img src={`${IMG_SERVER}/event/rebranding/btn-1.png`} alt="버튼 이미지" />
+        </button>
+      </section>
+      <section>
+        <img src={`${IMG_SERVER}/event/rebranding/bg-4.png`} alt="이벤트 이미지" />
+        <div className="stoneWrap">
+          <button>
+            <img src={`${IMG_SERVER}/event/rebranding/stoneBtn-1.png`} alt="이벤트 이미지" />
+          </button>
+          <button>
+            <img src={`${IMG_SERVER}/event/rebranding/stoneBtn-1.png`} alt="이벤트 이미지" />
+          </button>
+          <img src={`${IMG_SERVER}/event/rebranding/sign.png`} />
+          <button>
+            <img src={`${IMG_SERVER}/event/rebranding/stoneBtn-2.png`} alt="이벤트 이미지" />
+          </button>
+        </div>
+      </section>
+      <div className="tabmenuWrap">
+        <div className="tabmenu">
+          <li className="active">
+            <img src={`${IMG_SERVER}/event/rebranding/tabmenu-1.png`} alt="" />
+          </li>
+          <li>
+            <img src={`${IMG_SERVER}/event/rebranding/tabmenu-2.png`} alt="" />
+          </li>
+          <li>
+            <img src={`${IMG_SERVER}/event/rebranding/tabmenu-3.png`} alt="" />
+          </li>
+        </div>
+      </div>
+      <section>
+        
+      </section>
+      <Round_1 />
+      {/* <Round_2 />
+      <Round_3 /> */}
+    </div>
+  )
+}
+
+export default Rebranding
