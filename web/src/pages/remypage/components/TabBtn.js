b087b4171913f5002e9dffdca9abcdefb69449cf
박지호 <ghost1565@naver.com>
2022-02-22 13:15:52 +0900

    #공지사항 임시저장

---
+++ web/src/pages/remypage/components/TabBtn.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+const TabBtn = (props) => {
+  const {param, tabChangeAction, defaultTab} = props
+
+  const tabClick = (e) => {
+    const {tabTarget} = e.currentTarget.dataset
+    if (tabTarget === param.item) {
+      param.setTab(tabTarget)
+      if (param.setPage) {
+        param.setPage(defaultTab ? defaultTab : 0)
+      }
+    }
+    if(typeof tabChangeAction === 'function') tabChangeAction(param.item);
+  }
+
+  return (
+    <li className={param.tab === param.item ? 'active' : ''} data-tab-target={param.item} onClick={tabClick}>{param.item}</li>
+  )
+}
+
+export default TabBtn
--- web/src/pages/remypage/contents/notice/Notice.js
+++ web/src/pages/remypage/contents/notice/Notice.js
@@ -2,9 +2,11 @@ import React, {useEffect, useState, useContext, useRef} from 'react'
 import {useHistory} from 'react-router-dom'
 import {Context} from 'context'
 
+import Api from 'context/api'
+
 // global components
 import Header from 'components/ui/header/Header'
-import TabBtn from 'components/ui/tabBtn/TabBtn'
+import TabBtn from '../../components/TabBtn'
 // components
 
 // contents
@@ -17,17 +19,51 @@ import {setNoticeTab} from "redux/actions/notice";
 
 
 const NoticePage = () => {
-  const noticeTabmenu = ['알림','공지사항']
+  const noticeTabmenu = ['알림','공지사항'];
+  const [allimNew, setAllimNew] = useState(0);
   const {tab} = useSelector((state) => state.notice);
   const dispatch = useDispatch();
   const history = useHistory()
   const context = useContext(Context)
+
+  const fetchAllimData = () => {
+    let params = {page: 1, records: 1000};
+    Api.my_notification(params).then((res) => {
+      if(res.result === "success") {
+        setAllimNew(res.data.newCnt);
+      }
+    });
+  };
+
+  // const fetchPostData = () => {
+  //   let params = {page: 1, records: 1000};
+  //   Api.noticeList(params).then((res) => {
+  //     if(res.result === "success") {
+  //       if(postPageInfo.page !== 1) {
+  //         let temp = []
+  //         res.data.list.forEach((value) => {
+  //           if(postListInfo.list.findIndex((target) => target.noticeIdx == value.noticeIdx) === -1) { //list의 인덱스가 현재 noticeIdx-1일경우 그 값을 temp에 담아줌
+  //             temp.push(value);
+  //           }
+  //         })
+  //         //cnt: noticeIdx, list: 스크롤시 출력되는 list, totalPage: 전체 페이지
+  //         setPostListInfo({cnt: res.data.list.noticeIdx, list: postListInfo.list.concat(temp), totalPage: res.data.paging.totalPage});
+  //       } else {
+  //         setPostListInfo({cnt: res.data.list.noticeIdx, list: res.data.list, totalPage: res.data.paging.totalPage});
+  //       }
+  //     } else {
+  //       setPostListInfo({cnt: 0, list: [], totalPage: 0});
+  //       context.action.alert({msg: res.message});
+  //     }
+  //   }).catch((e) => console.log(e));
+  // };
 
   // 로그인 토큰값 확인
   useEffect(() => {
     if(!(context.token.isLogin)) {
       history.push("/login")
     }
+    fetchAllimData();
   }, []);
 
   return (
