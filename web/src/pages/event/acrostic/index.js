6d03b4e80348faa570620d3d85b5f65499113c40
kimhyunjin <khjzzm@gmail.com>
2022-02-24 14:10:56 +0900

    댓글이벤트 본인인증 추가

--- web/src/pages/event/acrostic/index.js
+++ web/src/pages/event/acrostic/index.js
@@ -10,9 +10,11 @@ import Api from "context/api";
 import {Context} from "context";
 import {useSelector} from "react-redux";
 import _ from "lodash";
+import {useHistory} from "react-router-dom";
 
 const Acrostic = () => {
   const context = useContext(Context)
+  const history = useHistory();
   const [popup, setPopup] = useState(false);
   const [poem, setPoem] = useState({
     tailMemNo: context.profile && context.profile.memNo,
@@ -51,23 +53,37 @@ const Acrostic = () => {
 
   //글 작성
   const savePoem = (value) => {
-    Api.poem({
-      reqBody: true,
-      data: {
-        tailMemNo: poem.tailMemNo,
-        tailMemId: poem.tailMemId,
-        tailMemSex: "",
-        tailMemIp: "",
-        tailConts: value,
-        tailLoginMedia: poem.tailLoginMedia,
-      },
-      method: 'POST'
-    }).then((response)=>{
-      if(response.code === "C003"){
-        context.action.alert({msg: '댓글이 등록되었습니다.'});
-        getPoemList()
+    selfAuthCheck().then((res) => {
+      if (res.result === 'success'){
+        Api.poem({
+          reqBody: true,
+          data: {
+            tailMemNo: poem.tailMemNo,
+            tailMemId: poem.tailMemId,
+            tailMemSex: "",
+            tailMemIp: "",
+            tailConts: value,
+            tailLoginMedia: poem.tailLoginMedia,
+          },
+          method: 'POST'
+        }).then((response)=>{
+          if(response.code === "C003"){
+            context.action.alert({msg: '댓글이 등록되었습니다.'});
+            getPoemList()
+          }
+        })
       }
     })
+  }
+
+  //본인인증
+  const selfAuthCheck = async () =>{
+    const {result, data} = await Api.self_auth_check();
+    if(result === 'success'){
+      return {result : result, phoneNo : data.phoneNo};
+    }else{
+      history.push(`/selfauth?event=/event`)
+    }
   }
 
   //글 삭제
@@ -134,7 +150,6 @@ const Acrostic = () => {
         <div className="subContent">
           <p>한 회원 당 한 계정으로만 참여할 수 있습니다.</p>
           <p>상금은 제세공과금 없이 환전 가능한 “별”로 지급해드립니다.</p>
-          <p>다수의 계정으로 중복 참여는 불가하지만 한 계정으로 중복 참여는 가능합니다.</p>
           <p>상품은 이벤트 종료 후 일주일 안에 본인인증에 사용 하신 번호로 발송해드립니다.</p>
           <p>도배 등의 행위는 재제 사유입니다.</p>
           <p>상품이 품절될 경우 동일 가격대의 다른 상품으로 대체됩니다.</p>
