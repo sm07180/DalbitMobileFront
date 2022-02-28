f99b665d1e2ea437150ab2dfc60ebd0b9dabb8bb
박지호 <ghost1565@naver.com>
2022-02-28 11:05:12 +0900

    #7649 댓글이벤트 유의사항 항목 제거

--- web/src/pages/event/acrostic/index.js
+++ web/src/pages/event/acrostic/index.js
@@ -148,7 +148,6 @@ const Acrostic = () => {
       {popup &&
       <LayerPopup title="유의사항" setPopup={setPopup}>
         <div className="subContent">
-          <p>한 회원 당 한 계정으로만 참여할 수 있습니다.</p>
           <p>상금은 제세공과금 없이 환전 가능한 “별”로 지급해드립니다.</p>
           <p>상품은 이벤트 종료 후 일주일 안에 본인인증에 사용 하신 번호로 발송해드립니다.</p>
           <p>도배 등의 행위는 재제 사유입니다.</p>
