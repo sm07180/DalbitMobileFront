560beb3c161947e69e9cbdd198babe574346792f
박지호 <ghost1565@naver.com>
2022-03-10 11:28:30 +0900

    #관리자 UI 변경 임시저장

--- web/src/pages/common/popup/AdminLayerPopup.tsx
+++ web/src/pages/common/popup/AdminLayerPopup.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState, useContext } from "react";
 import { useHistory, useLocation } from "react-router-dom";
-import styled from "styled-components";
 import { GlobalContext } from "context";
+
+import './adminLayerPopup.scss'
 
 const AdminLayerPopup = (props: any)=> {
   const { globalState, globalAction } = useContext(GlobalContext);
   const history = useHistory();
-  const closePopup = () => {
+  const popupClose = () => {
     globalAction.setBroadcastAdminLayer!((prevState) => ({
       ...prevState,
       status: false,
@@ -17,15 +18,15 @@ const AdminLayerPopup = (props: any)=> {
 
   const closePopupDim = (e) => {
     const target = e.target;
-    if (target.id === "layerPopup") {
-      closePopup();
+    if (target.id === "layerPop") {
+      popupClose();
     }
   };
   //clip Link
   const broadCastLink = (type: string) => {
     if (type === "admin") {
       globalAction.setShadowAdmin!(1);
-      closePopup();
+      popupClose();
       if (globalState.inBroadcast) {
         window.location.href = `/broadcast/${globalState.broadcastAdminLayer.roomNo}`;
         setTimeout(() => {
@@ -36,7 +37,7 @@ const AdminLayerPopup = (props: any)=> {
       }
     } else {
       globalAction.setShadowAdmin!(0);
-      closePopup();
+      popupClose();
       if (globalState.inBroadcast) {
         window.location.href = `/broadcast/${globalState.broadcastAdminLayer.roomNo}`;
         setTimeout(() => {
@@ -54,53 +55,28 @@ const AdminLayerPopup = (props: any)=> {
     };
   }, []);
   return (
-    <Content>
-      <div id="layerPopup" onClick={closePopupDim}>
-        <div className="layerContainer">
-          <div className="layerContent">
-            <div className="adminContent">
-              {globalState.broadcastAdminLayer.nickNm === "" ? (
-                <h3>관리자로 입장하시겠습니까?</h3>
-              ) : (
-                <h3>
-                  <b>{globalState.broadcastAdminLayer.nickNm}</b> 님의 <br /> 방송방에 입장하시겠습니까?
-                </h3>
-              )}
-            </div>
-            <div className="btnWrap">
-              <button className="btn btn_cancel" onClick={() => broadCastLink("normal")}>
-                시청자 모드
-              </button>
-              <button className="btn btn_ok" onClick={() => broadCastLink("admin")}>
-                관리자 모드
-              </button>
-            </div>
+    <div id="layerPop" onClick={closePopupDim}>
+      <div className="popLayer">
+        <div className="popContainer">
+          <div className="adminContent">
+            {globalState.broadcastAdminLayer.nickNm === "" ? (
+              <h3>관리자로 입장하시겠습니까?</h3>
+            ) : (
+              <h3>
+                <b>{globalState.broadcastAdminLayer.nickNm}</b> 님의 <br /> 방송방에 입장하시겠습니까?
+              </h3>
+            )}
           </div>
-          <button className="btnClose" onClick={closePopup}>
-            닫기
-          </button>
+          <div className="btnWrap">
+            <button className="enterBtn normal" onClick={() => broadCastLink("normal")}>시청자 모드</button>
+            <button className="enterBtn admin" onClick={() => broadCastLink("admin")}>관리자 모드</button>
+          </div>
+        </div>
+        <div className='closeWrap'>
+          <button className='close'onClick={popupClose}>닫기</button>
         </div>
       </div>
-    </Content>
+    </div>      
   );
 };
 export default  AdminLayerPopup
-
-const Content = styled.div`
-  .adminContent {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-bottom: 20px;
-    text-align: center;
-  }
-  .btnWrap {
-    .darkgray {
-      background-color: #757575;
-      color:#fff;
-    }
-    .mainColor {
-      background-color: #FF3C7B;
-    }
-  }
-`;
