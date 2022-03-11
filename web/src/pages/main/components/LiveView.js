f560b857e1d6ae27906ac594b068db11a5e3ac73
박지호 <ghost1565@naver.com>
2022-03-11 13:46:05 +0900

    #임시저장

--- web/src/pages/main/components/LiveView.js
+++ web/src/pages/main/components/LiveView.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect} from 'react'
 import {useHistory} from 'react-router-dom'
 
 // global components
@@ -14,6 +14,50 @@ const LiveView = (props) => {
   let locationStateHistory = useHistory();
   const context = useContext(Context);
 
+  const scrollEvent = () => {
+    const timeRank1 = document.getElementById('timeRankwer1st');
+    const timeRank2 = document.getElementById('timeRankwer2nd');
+    const timeRank3 = document.getElementById('timeRankwer3rd');
+    
+    if(timeRank1){
+      const timeRank1Top = timeRank1?.getBoundingClientRect().top;
+      if(timeRank1Top < 700) {
+        timeRank1.classList.add('show');
+        timeRank1.classList.remove('hide');
+      } else {
+        timeRank1.classList.add('hide');
+        timeRank1.classList.remove('show');
+      }
+    }
+
+    if(timeRank2){
+      const timeRank2Top = timeRank2?.getBoundingClientRect().top;
+      if(timeRank2Top < 700) {
+        timeRank2.classList.add('show');
+        timeRank2.classList.remove('hide');
+      } else {
+        timeRank2.classList.add('hide');
+        timeRank2.classList.remove('show');
+      }
+    }
+
+    if(timeRank3){
+      const timeRank3Top = timeRank3?.getBoundingClientRect().top;
+      if(timeRank3Top < 700) {
+        timeRank3.classList.add('show');
+        timeRank3.classList.remove('hide');
+      } else {
+        timeRank3.classList.add('hide');
+        timeRank3.classList.remove('show');
+      }
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', scrollEvent)
+    return () => window.removeEventListener('scroll', scrollEvent)
+  }, [])
+
   return (
     <div className="liveListWrap">
       {data && data.length > 0 ?
@@ -21,13 +65,16 @@ const LiveView = (props) => {
           {data.map((list,index) => {
             //타임뱃지만 출력
             const timeBadge = list.liveBadgeList.filter((data)=> data?.text.indexOf('타임')>-1 );
-
-            console.log(timeBadge[0]?.text);
-
+            const timeRank = timeBadge[0]?.text;
+            const cupidData = list.goodMem;
             return (
-              <div className={`listRow ${timeBadge.length > 0 ? "timeRankwer" : ""}`} key={index} onClick={() => {
-                RoomValidateFromClip(list.roomNo, context, locationStateHistory, list.bjNickNm);
-              }}>
+              <div className={`listRow`}
+                id={`${timeRank === "타임 1위" ? "timeRankwer1st" : timeRank === "타임 2위" ? "timeRankwer2nd" : timeRank === "타임 3위" ? "timeRankwer3rd" : ""}`}
+                key={index}
+                onClick={() => {
+                  RoomValidateFromClip(list.roomNo, context, locationStateHistory, list.bjNickNm);
+                }}
+              >
                 <div className="photo">
                   <img src={list.bjProfImg.thumb292x292} alt="" />
                   { list.gstMemNo && <img src={list.gstProfImg.thumb292x292} className="guest" alt="" /> }
@@ -37,6 +84,13 @@ const LiveView = (props) => {
                   <div className="listItem">
                     <BadgeItems data={list} type={'isNew'} />
                     <BadgeItems data={list} type={'isBadge'} />
+                    {
+                      cupidData.map((cupid, index) => {
+                        return (
+                          <span className={`cupidRanker cupidRanker-${cupid}`} key={index}></span>
+                        )
+                      })
+                    }
                     {/* <BadgeItems data={timeBadge} type={'liveBadgeList'} /> */}
                   </div>
                   <div className="listItem">
