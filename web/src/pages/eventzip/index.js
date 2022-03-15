c7de86b321336b08f5b31df8686f6bce733cd912
박지호 <ghost1565@naver.com>
2022-03-15 18:26:09 +0900

    #이벤트모음.zip 페이지 이벤트 기간 종료 시 자동정렬 추가

--- web/src/pages/eventzip/index.js
+++ web/src/pages/eventzip/index.js
@@ -10,15 +10,71 @@ import {Context} from "context";
 const EventZip = () => {
   let history = useHistory()
   const context = useContext(Context)
-  const platformWarEventEnd = new moment().isAfter('20220312');
-  const shareEventEnd = new moment().isAfter('20220325');
-  const poemEventEnd = new moment().isAfter('20220323');
-  const inviteEventEnd = new moment().isAfter('20220309');
-  const wiseEventEnd = new moment().isAfter('20220328');
+  const [eventInfo, setEventInfo] = useState(
+    [
+      {
+        eventTitle : "달라져스 : 달라 스톤 모으기",
+        startDay : moment('20220308').format('MM.DD'),
+        endDay : moment('20220327').format('MM.DD'),
+        bannerImg : "https://image.dalbitlive.com/eventzip/eventZip_7634.png",
+        path : "/event/rebranding",
+        endState :  moment().isAfter('20220328')
+      },
+      {
+        eventTitle : "슬기로운 달라생활",
+        startDay : moment('20220307').format('MM.DD'),
+        endDay : moment('20220327').format('MM.DD'),
+        bannerImg : "https://image.dalbitlive.com/eventzip/eventZip_7733.png",
+        path : "/notice",
+        noticeNum : 617,
+        endState :  moment().isAfter('20220328')
+      },
+      {
+        eventTitle : "달라를 소개해 달라",
+        startDay : moment('20220303').format('MM.DD'),
+        endDay : moment('20220324').format('MM.DD'),
+        bannerImg : "https://image.dalbitlive.com/eventzip/eventZip_7650.png",
+        path : "/event/share",
+        endState :  moment().isAfter('20220325')
+      },
+      {
+        eventTitle : "달라를 축하해 달라",
+        startDay : moment('20220228').format('MM.DD'),
+        endDay : moment('20220321').format('MM.DD'),
+        bannerImg : "https://image.dalbitlive.com/eventzip/eventZip_7649.png",
+        path : "/event/acrostic",
+        endState :  moment().isAfter('20220322')
+      },
+      {
+        eventTitle : "달라에 놀러온 핵인싸 주목!",
+        startDay : moment('20220310').format('MM.DD'),
+        endDay : "",
+        bannerImg : "https://image.dalbitlive.com/eventzip/eventZip_7583.png",
+        path : "/event/playmaker",
+        endState : false
+      },
+      {
+        eventTitle : "3사 플랫폼 노래대전",
+        startDay : moment('20220303').format('MM.DD'),
+        endDay : moment('20220311').format('MM.DD'),
+        bannerImg : "https://image.dalbitlive.com/eventzip/eventZip_7677.png",
+        path : "/event/platformWar",
+        endState :  moment().isAfter('20220312')
+      },
+      {
+        eventTitle : "친구 초대, 초대왕 도전!",
+        startDay : moment('20220227').format('MM.DD'),
+        endDay : moment('20220307').format('MM.DD'),
+        bannerImg : "https://image.dalbitlive.com/eventzip/eventZip_7590.png",
+        path : "/event/invite",
+        endState :  moment().isAfter('20220308')
+      },
+    ]
+  );
 
-  const playMakerEventEnd = new moment().isAfter('20301231');
-  
-  const dalragersEvenetEnd = new moment().isAfter('20220328');
+  useEffect(() => {
+
+  }, [eventInfo]);
 
   const golink = (path, endDay, num) => {
     if(endDay){
@@ -38,71 +94,49 @@ const EventZip = () => {
    <div id='eventZip'>
     <Header position={'sticky'} title="리브랜딩 이벤트 모음.zip" type={'back'}/>
     <div className='content'>
-      <div className='eventWrap'>
-
-        <div className={`eventList ${dalragersEvenetEnd ? 'end' : ''}`} onClick={() => {golink("/event/rebranding", dalragersEvenetEnd)}}>
-          <div className='thumbNail' style={{backgroundImage: `url(https://image.dalbitlive.com/eventzip/eventZip_7634.png)`}}/>
-          <div className='eventInfo'>
-            <div className='eventTitle'>달라져스 : 달라 스톤 모으기</div>
-            <div className='eventDate'>03.08 - 03.27</div>
+      <div className='eventWrap'>        
+        <div id='ingWrap'>
+          {
+            eventInfo.map((list, index) => {
+              if(!list.endState){
+                return (
+                  <div key={index} className={`eventList ${list.endState ? 'end' : ''}`} onClick={() => {golink(`${list.path}`, list.endState, `${list.noticeNum && list.noticeNum}`)}}>
+                    <div className='thumbNail' style={{backgroundImage: `url(${list.bannerImg})`}}/>
+                    <div className='eventInfo'>
+                      <div className='eventTitle'>{list.eventTitle}</div>
+                      <div className='eventDate'>{list.endDay ? `${list.startDay} - ${list.endDay}` : "상시모집"}</div>
+                    </div>
+                  </div>
+                )
+              }                   
+            })
+          }
+        </div>
+        <div id='comingSoonWrap'>
+          <div className='eventList comingSoon'>
+            <div className='thumbNail'/>
+            <div className='eventInfo'>
+              <div className='eventTitle'>이벤트가 시작되면 배너를 통해 알려드려요.</div>
+            </div>
           </div>
         </div>
-
-        <div className={`eventList ${wiseEventEnd ? 'end' : ''}`} onClick={() => {golink("/notice", wiseEventEnd, 617)}}>
-          <div className='thumbNail' style={{backgroundImage: `url(https://image.dalbitlive.com/eventzip/eventZip_7733.png)`, backgroundSize:'cover'}}/>
-          <div className='eventInfo'>
-            <div className='eventTitle'>슬기로운 달라생활</div>
-            <div className='eventDate'>03.07 - 03.27</div>
-          </div>
-        </div>
-
-        <div className={`eventList ${shareEventEnd ? 'end' : ''}`} onClick={() => {golink("/event/share", shareEventEnd)}}>
-          <div className='thumbNail' style={{backgroundImage: `url(https://image.dalbitlive.com/eventzip/eventZip_7650.png)`}}/>
-          <div className='eventInfo'>
-            <div className='eventTitle'>달라를 소개해 달라</div>
-            <div className='eventDate'>03.03 - 03.24</div>
-          </div>
-        </div>
-
-        <div className={`eventList ${poemEventEnd ? 'end' : ''}`} onClick={() => {golink("/event/acrostic", poemEventEnd)}}>
-          <div className='thumbNail' style={{backgroundImage: `url(https://image.dalbitlive.com/eventzip/eventZip_7649.png)`}}/>
-          <div className='eventInfo'>
-            <div className='eventTitle'>달라를 축하해 달라</div>
-            <div className='eventDate'>02.28 - 03.21</div>
-          </div>
-        </div>
-
-        <div className={`eventList ${playMakerEventEnd ? 'end' : ''}`} onClick={() => {golink("/event/playmaker", playMakerEventEnd)}}>
-          <div className='thumbNail' style={{backgroundImage: `url(https://image.dalbitlive.com/eventzip/eventZip_7583.png)`}}/>
-          <div className='eventInfo'>
-            <div className='eventTitle'>달라에 놀러온 핵인싸 주목!</div>
-            <div className='eventDate'>상시 모집</div>
-          </div>
-        </div>
-
-        <div className='eventList comingSoon'>
-          <div className='thumbNail'/>
-          <div className='eventInfo'>
-            <div className='eventTitle'>이벤트가 시작되면 배너를 통해 알려드려요.</div>
-          </div>
-        </div>
-        
-        <div className={`eventList ${platformWarEventEnd ? 'end' : ''}`} onClick={() => {golink("/event/platformWar", platformWarEventEnd)}}>
-        {/*  <div className={`eventList ${platformWarEventEnd ? 'end' : ''}`} onClick={() => {golink("/notice", platformWarEventEnd, 612)}}>*/}
-          <div className='thumbNail' style={{backgroundImage: `url(https://image.dalbitlive.com/eventzip/eventZip_7677.png)`}}/>
-          <div className='eventInfo'>
-            <div className='eventTitle'>3사 플랫폼 노래대전</div>
-            <div className='eventDate'>03.03 - 03.11</div>
-          </div>
-        </div>
-
-        <div className={`eventList ${inviteEventEnd ? 'end' : ''}`} onClick={() => {golink("/event/invite", inviteEventEnd)}}>
-          <div className='thumbNail' style={{backgroundImage: `url(https://image.dalbitlive.com/eventzip/eventZip_7590.png)`}}/>
-          <div className='eventInfo'>
-            <div className='eventTitle'>친구 초대, 초대왕 도전!</div>
-            <div className='eventDate'>02.21 - 03.07</div>
-          </div>
-        </div>
+        <div id='endWrap'>
+          {
+            eventInfo.map((list, index) => {
+              if(list.endState){
+                return (
+                  <div key={index} className={`eventList ${list.endState ? 'end' : ''}`} onClick={() => {golink(`${list.path}`, list.endState)}}>
+                    <div className='thumbNail' style={{backgroundImage: `url(${list.bannerImg})`}}/>
+                    <div className='eventInfo'>
+                      <div className='eventTitle'>{list.eventTitle}</div>
+                      <div className='eventDate'>{list.startDay} - {list.endDay}</div>
+                    </div>
+                  </div>
+                )
+              }                   
+            })
+          }
+        </div>  
       </div>
     </div>
    </div>
