b7971cb6a786d39605b2a539ad9284f73eac6d97
박지호 <ghost1565@naver.com>
2022-03-16 09:42:44 +0900

    #이벤트zip 페이지 수정

--- web/src/pages/eventzip/index.js
+++ web/src/pages/eventzip/index.js
@@ -10,44 +10,45 @@ import {Context} from "context";
 const EventZip = () => {
   let history = useHistory()
   const context = useContext(Context)
+  const nowDay = moment().format('YYYYMMDD');
   const [eventInfo, setEventInfo] = useState(
     [
       {
         eventTitle : "달라져스 : 달라 스톤 모으기",
-        startDay : moment('20220308').format('MM.DD'),
-        endDay : moment('20220327').format('MM.DD'),
+        startDay : "20220308",
+        endDay : "20220327",
         bannerImg : "https://image.dalbitlive.com/eventzip/eventZip_7634.png",
         path : "/event/rebranding",
-        endState :  moment().isAfter('20220328')
+        endState : false
       },
       {
         eventTitle : "슬기로운 달라생활",
-        startDay : moment('20220307').format('MM.DD'),
-        endDay : moment('20220327').format('MM.DD'),
+        startDay : "20220307",
+        endDay : "20220327",
         bannerImg : "https://image.dalbitlive.com/eventzip/eventZip_7733.png",
         path : "/notice",
         noticeNum : 617,
-        endState :  moment().isAfter('20220328')
+        endState : false
       },
       {
         eventTitle : "달라를 소개해 달라",
-        startDay : moment('20220303').format('MM.DD'),
-        endDay : moment('20220324').format('MM.DD'),
+        startDay : "20220303",
+        endDay : "20220324",
         bannerImg : "https://image.dalbitlive.com/eventzip/eventZip_7650.png",
         path : "/event/share",
-        endState :  moment().isAfter('20220325')
+        endState : false
       },
       {
         eventTitle : "달라를 축하해 달라",
-        startDay : moment('20220228').format('MM.DD'),
-        endDay : moment('20220321').format('MM.DD'),
+        startDay : "20220228",
+        endDay : "20220321",
         bannerImg : "https://image.dalbitlive.com/eventzip/eventZip_7649.png",
         path : "/event/acrostic",
-        endState :  moment().isAfter('20220322')
+        endState : false
       },
       {
         eventTitle : "달라에 놀러온 핵인싸 주목!",
-        startDay : moment('20220310').format('MM.DD'),
+        startDay :"20220310",
         endDay : "",
         bannerImg : "https://image.dalbitlive.com/eventzip/eventZip_7583.png",
         path : "/event/playmaker",
@@ -55,26 +56,30 @@ const EventZip = () => {
       },
       {
         eventTitle : "3사 플랫폼 노래대전",
-        startDay : moment('20220303').format('MM.DD'),
-        endDay : moment('20220311').format('MM.DD'),
+        startDay : "20220303",
+        endDay : "20220311",
         bannerImg : "https://image.dalbitlive.com/eventzip/eventZip_7677.png",
         path : "/event/platformWar",
-        endState :  moment().isAfter('20220312')
+        endState : false
       },
       {
         eventTitle : "친구 초대, 초대왕 도전!",
-        startDay : moment('20220227').format('MM.DD'),
-        endDay : moment('20220307').format('MM.DD'),
+        startDay : "20220227",
+        endDay : "20220307",
         bannerImg : "https://image.dalbitlive.com/eventzip/eventZip_7590.png",
         path : "/event/invite",
-        endState :  moment().isAfter('20220308')
+        endState :false
       },
     ]
   );
 
   useEffect(() => {
-
-  }, [eventInfo]);
+    for(let i = 0; i < eventInfo.length; i++){
+      if(moment(nowDay).isAfter(moment(eventInfo[i].endDay).add(1, 'days'))){
+        eventInfo[i].endState = true
+      }
+    }
+  }, [nowDay]);
 
   const golink = (path, endDay, num) => {
     if(endDay){
@@ -104,7 +109,9 @@ const EventZip = () => {
                     <div className='thumbNail' style={{backgroundImage: `url(${list.bannerImg})`}}/>
                     <div className='eventInfo'>
                       <div className='eventTitle'>{list.eventTitle}</div>
-                      <div className='eventDate'>{list.endDay ? `${list.startDay} - ${list.endDay}` : "상시모집"}</div>
+                      <div className='eventDate'>
+                        {list.endDay ? `${moment(list.startDay).format('MM.DD')} - ${moment(list.endDay).format('MM.DD')}` : "상시모집"}
+                      </div>
                     </div>
                   </div>
                 )
@@ -129,7 +136,7 @@ const EventZip = () => {
                     <div className='thumbNail' style={{backgroundImage: `url(${list.bannerImg})`}}/>
                     <div className='eventInfo'>
                       <div className='eventTitle'>{list.eventTitle}</div>
-                      <div className='eventDate'>{list.startDay} - {list.endDay}</div>
+                      <div className='eventDate'>{moment(list.startDay).format('MM.DD')} - {moment(list.endDay).format('MM.DD')}</div>
                     </div>
                   </div>
                 )
