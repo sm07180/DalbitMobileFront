8eddabe54756e85e1c6a18af3363b4d77fd827f7
이정혁 <habetree@gmail.com>
2022-04-20 14:57:33 +0900

    #7991 프로필 가입 신청 기능 추가 및 히든 처리

--- web/src/pages/profile/components/totalInfo.js
+++ web/src/pages/profile/components/totalInfo.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useRef} from 'react'
+import React, {useEffect, useState, useRef, useContext} from 'react'
 import {IMG_SERVER} from 'context/config'
 import Swiper from 'react-id-swiper'
 // global components
@@ -10,6 +10,9 @@ import {goProfileDetailPage} from "pages/profile/contents/profileDetail/profileD
 import {useHistory} from "react-router-dom";
 import FeedLike from "pages/profile/components/FeedLike";
 import {useSelector} from "react-redux";
+import Api from "context/api";
+import {Context} from "context";
+import Utility from 'components/lib/utility';
 
 const TotalInfo = (props) => {
   const history = useHistory();
@@ -17,6 +20,7 @@ const TotalInfo = (props) => {
   const [openBadge,setOpenBadge] = useState(false);
   const [badgeTotalCnt,setBadgeTotalCnt] = useState(0);
   const profileData = useSelector(state => state.profile);
+  const context = useContext(Context);
   const swiperRef = useRef();
   const defaultNotice = [{
     contents: "방송 공지를 등록해주세요.",
@@ -64,14 +68,49 @@ const TotalInfo = (props) => {
     }
   },[data])
 
-  // 팀 상세 페이지 이동
+  // 팀 가입신청 버튼 출력 여부
+  const getTeamJoinBtnVisibleYn = () => {
+    let result = false;
+
+    if (data.teamSymbolvo !== undefined &&
+        data.teamSymbolvo.team_no != 0 &&
+        data.teamSymbolvo.bg_cnt < 5 &&
+        data.teamJoinCheck === 1) {
+      //result = true;
+    }
+
+    return result;
+  }
+
+  // 팀 가입신청 요청
   const reqTeamJoin = (e) => {
+    const { teamNo } = e.currentTarget.dataset;
+
+    const param ={
+      teamNo: teamNo,
+      memNo: context.token.memNo,
+      reqSlct: 'r' //신청구분 [r:가입신청, i:초대]
+    };
+
+    Api.getTeamMemReqIns(param).then((res)=>{
+      if(res.code === "00000"){
+        context.action.toast({
+          msg: '팁 가입신청이 완료 되었습니다.'
+        });
+      } else {
+        context.action.toast({ msg: res.message });
+      }
+    })
+  }
+
+  // 팀 상세 페이지 이동
+  const goTeamDetailPage = (e) => {
     const { teamNo } = e.currentTarget.dataset;
 
     if (teamNo !== undefined) {
       history.push(`/team/detail/${teamNo}`);
-    }
-  }
+    };
+  };
 
   /* 피드 삭제시 스와이퍼 업데이트용 */
   useEffect(() => {
@@ -137,7 +176,7 @@ const TotalInfo = (props) => {
       </div>
       {(data.teamSymbolvo !== undefined && data.teamSymbolvo.team_no !== 0) &&
         <div className="teamInfo">
-          <div className="wrapBox" data-team-no={data.teamSymbolvo.team_no} onClick={reqTeamJoin}>
+          <div className="wrapBox" data-team-no={data.teamSymbolvo.team_no} onClick={goTeamDetailPage}>
             <img src={`${IMG_SERVER}/profile/teamInfo-title.png`} alt="team" className="title" />
             <div className="teamSymbol">
               <img src={`${IMG_SERVER}/team/parts/E/${data.teamSymbolvo.team_bg_code}.png`} />
@@ -147,7 +186,7 @@ const TotalInfo = (props) => {
             <div className="teamName">{data.teamSymbolvo.team_name}</div>
           </div>
           {/* 자신이 가입된 팀이 없고, 상대방 팀과 같지 않다면 가입 신청 버튼 출력 */}
-          {(profileData.teamSymbolvo !== undefined && profileData.teamSymbolvo.team_no !== 0 && data.teamSymbolvo.team_no !== profileData.teamSymbolvo.team_no && profileData.teamSymbolvo.bg_cnt < 5) && <button>가입신청</button>}
+          {getTeamJoinBtnVisibleYn() && <button data-team-no={data.teamSymbolvo.team_no} onClick={reqTeamJoin}>가입신청</button> }
         </div>
       }
       {data.profMsg &&
