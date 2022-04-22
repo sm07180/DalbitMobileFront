795f606eacfddef7999c7db1c3302108958de5aa
박지호 <ghost1565@naver.com>
2022-04-22 11:19:44 +0900

    #임시저장

--- web/src/pages/story/index.js
+++ web/src/pages/story/index.js
@@ -21,19 +21,19 @@ export default () => {
       contents: "BJ님! 일반 라디오방송 DJ보다 훨씬 잘하십니다. 이번에 저 방탄소년단 콘서트에 가는데요! 저 곡하나 신청해도 될까요? ㅎㅎ 방탄소년단 최신곡으로 하나 부탁드려요! ",
       nickNm: "일이삼사오육칠팔꾸십",
       profImg: "https://photo.dalbitlive.com/profile_0/20754115200/20200804125251259035.jpeg",
-      writeDt : "20210310172024"
+      writeDt : 20210310172024
     },
     {
       contents: "일반 라디오방송 DJ보다 훨씬 잘하시는 것 같아요! 이번에 저 아이유 콘서트에 가는데요! 저 곡하나 신청해도 될까요? ㅎㅎ 아이유 블루밍 최신곡으로 하나 부탁드려요! 매번 좋은노래 틀어주셔서 감사합니다 :) 아이유 짱! 아이유 짱! 아이유 짱!  BJ 라디오 짱!",
       nickNm: "달라둥이👑",
       profImg: "https://photo.dalbitlive.com/profile_0/20754115200/20200804125251259035.jpeg",
-      writeDt : "20210310172024"
+      writeDt : 20210310172024
     },
     {
       contents: "안녕하세요! 방송 항상 잘 듣고 있어요 ㅎㅎ ",
       nickNm: "💸맹치",
       profImg: "https://photo.dalbitlive.com/profile_0/20754115200/20200804125251259035.jpeg",
-      writeDt : "20210310172024"
+      writeDt : 20210310172024
     },
   ]
 
