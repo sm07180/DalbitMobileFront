ccafb54b2f39de73e1c4796de2428b561a2be205
이승재 <nonp2@inforex.com>
2022-04-25 10:11:46 +0900

    팀제도 수정사항 커밋 / 시간 관련 변환 로직 추가

--- web/src/pages/team/components/popup/BadgeInfo.js
+++ web/src/pages/team/components/popup/BadgeInfo.js
@@ -7,6 +7,15 @@ import '../../scss/badgeInfo.scss';
 const BadgeInfoPop = (props) => {
   const {statChk,badgeData} = props;
 
+  const secFormatHH = (data,code) => {
+    let hour = 0;
+    if(code === "a008" || code === "a002"){
+      hour = Math.floor(data / 3600);
+      return hour;
+    }else{
+      return data
+    }
+  }
   // 페이지 시작
   return (
     <>
@@ -28,7 +37,7 @@ const BadgeInfoPop = (props) => {
         {(statChk ==='m' || statChk==='t') &&
         <div className="textWrap">
           <div className="text2">진행도</div>
-          <div className="text2"><span>{badgeData.bg_achieve} /{badgeData.bg_objective}</span></div>
+          <div className="text2"><span>{secFormatHH(badgeData.bg_achieve,badgeData.bg_code)} /{secFormatHH(badgeData.bg_objective,badgeData.bg_code)}</span></div>
         </div>
         }
       </div>
