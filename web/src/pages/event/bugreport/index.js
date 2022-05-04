6048567c9563bd549eb199fd2a9e100b6453b72a
박지송 <jisong0292@gamil.com>
2022-05-04 14:36:47 +0900

    #8003 버그리포트 추가 임시저장

---
+++ web/src/pages/event/bugreport/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {useSelector} from "react-redux";
+
+import {IMG_SERVER} from 'context/config'
+import {Hybrid} from 'context/hybrid'
+
+import Header from 'components/ui/header/Header'
+
+
+import './style.scss'
+
+const bugReport = () => {
+  const isDesktop = useSelector((state)=> state.common.isDesktop)
+  const form = () =>{
+    // if(isDesktop){
+    //   window.open('https://docs.google.com/forms/d/e/1FAIpQLSeStgFZwGDjf9NEV373bGK65s5teMEmM3j6UZvBtpQVPS3YxA/viewform')
+    // }else{
+    //   Hybrid('openUrl', `https://docs.google.com/forms/d/e/1FAIpQLSeStgFZwGDjf9NEV373bGK65s5teMEmM3j6UZvBtpQVPS3YxA/viewform`)
+    // }
+  }
+
+  return (
+    <div id="bugReport">
+      <Header title="버그리포트" type="back"/>
+      <section className="mainWrap">
+        <img src={`${IMG_SERVER}/event/bugreport/main.png`} alt="버그리포트" />
+        <button onClick={()=>{form()}}>
+          <img src={`${IMG_SERVER}/event/bugreport/buttonReport.png`} alt="리포트 작성하기" />
+        </button>
+      </section>
+    </div>
+  );
+};
+
+export default bugReport;
