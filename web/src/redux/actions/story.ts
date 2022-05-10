d561fc0b296212e5629e1a0aa06a46cdb4c0a146
박용훈 <ot9400@naver.com>
2022-05-10 17:08:05 +0900

    #7932

--- web/src/redux/actions/story.ts
+++ web/src/redux/actions/story.ts
@@ -1,9 +1,7 @@
 import {createAction} from "typesafe-actions";
 
 export const SET_INIT = "story/SET_INIT";
-export const SET_LIST = "story/SET_LIST";
-export const SET_PAGE_INFO = "story/SET_PAGE_INFO";
+export const SET_DATA = "story/SET_DATA";
 
 export const setInit = createAction(SET_INIT)<any>();
-export const setList = createAction(SET_LIST)<Array<any>>();
-export const setPageInfo = createAction(SET_PAGE_INFO)<{pageNo: number, pagePerCnt: number}>();
+export const setData = createAction(SET_DATA)<any>();
